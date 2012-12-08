954c817d63bd4c14584860bc4d1c24e4fab40c15
Remigijus Kiminas <remdex@gmail.com>
2012-12-08 18:51:08 +0200

    WEB client and compiled windows client

---
+++ translations/lhc_en.ts
@@ -0,0 +1,283 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="lt">
+<defaultcodec></defaultcodec>
+<context>
+    <name>LHQTableWidget</name>
+     <message>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+	<message>
+        <source>Nick</source>
+        <translation>Nick</translation>
+    </message>
+	<message>
+        <source>Time</source>
+        <translation>Time</translation>
+    </message>
+	<message>
+        <source>Referrer</source>
+        <translation>Referrer</translation>
+    </message>
+	<message>
+        <source>IP</source>
+        <translation>IP</translation>
+    </message>
+	<message>
+        <source>E-mail</source>
+        <translation>E-mail</translation>
+    </message>
+	<message>
+        <source>Department</source>
+        <translation>Department</translation>
+    </message>	
+</context>
+<context>
+    <name>ChatWidget</name>
+     <message>
+        <source>Live helper chat - chat</source>
+        <translation>Live helper chat - chat</translation>
+    </message>	
+</context>
+<context>
+    <name>ChatRoomsWidget</name>
+     <message>
+        <source>Live helper chat - chat rooms</source>
+        <translation>Live helper chat - chat rooms</translation>
+    </message>	
+</context>
+<context>
+    <name>LoginDialogBase</name>
+     <message>
+        <source>Please login</source>
+        <translation>Please login</translation>
+    </message>
+	<message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+	<message>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+	<message>
+        <source>Username</source>
+        <translation>Username</translation>
+    </message>
+	<message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+	<message>
+        <source>Host</source>
+        <translation>Host</translation>
+    </message>
+	<message>
+        <source>Autologin</source>
+        <translation>Autologin</translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialog</name>
+     <message>
+        <source>Authentification failed</source>
+        <translation>Authentification failed</translation>
+    </message>
+	<message>
+        <source>Warning</source>
+        <translation>Warning</translation>
+    </message>
+	<message>
+        <source>Host field is empty!</source>
+        <translation>Host field is empty!</translation>
+    </message>
+	<message>
+        <source>Password field is empty!</source>
+        <translation>Password field is empty!</translation>
+    </message>
+	<message>
+        <source>User name field is empty!</source>
+        <translation>User name field is empty!</translation>
+    </message>
+</context>
+<context>
+    <name>LhcTransferDialog</name>
+     <message>
+        <source>Chat transfer</source>
+        <translation>Chat transfer</translation>
+    </message>
+	<message>
+        <source>Online users</source>
+        <translation>Online users</translation>
+    </message>
+	<message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+	<message>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message> 
+</context>	
+<context>
+    <name>ChatRoomsWindow</name>
+     <message>
+        <source>Add chat</source>
+        <translation>Add chat</translation>
+    </message> 
+	<message>
+        <source>Open in new a window</source>
+        <translation>Open in new a window</translation>
+    </message>
+	<message>
+        <source>Open in new a window</source>
+        <translation>Open in new a window</translation>
+    </message>
+	<message>
+        <source>Open in new a separate window</source>
+        <translation>Open in new a separate window</translation>
+    </message>
+	<message>
+        <source>Loading...</source>
+        <translation>Loading...</translation>
+    </message> 
+	<message>
+        <source>Close chat</source>
+        <translation>Close chat</translation>
+    </message> 
+	<message>
+        <source>Delete chat</source>
+        <translation>Delete chat</translation>
+    </message>
+	<message>
+        <source>Reject chat</source>
+        <translation>Reject chat</translation>
+    </message>
+	<message>
+        <source>Pending chats</source>
+        <translation>Pending chats</translation>
+    </message>
+	<message>
+        <source>Transfered chats</source>
+        <translation>Transfered chats</translation>
+    </message>
+	<message>
+        <source>Active chats</source>
+        <translation>Active chats</translation>
+    </message>
+	<message>
+        <source>Closed chats</source>
+        <translation>Closed chats</translation>
+    </message> 
+</context>
+<context>
+    <name>ChatWindow</name>
+     <message>
+        <source>User left chat</source>
+        <translation>User left chat</translation>
+    </message>
+	<message>
+        <source>User joined chat</source>
+        <translation>User joined chat</translation>
+    </message>
+	<message>
+        <source>Actions</source>
+        <translation>Actions</translation>
+    </message> 
+	<message>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+	<message>
+        <source>Loading...</source>
+        <translation>Loading...</translation>
+    </message>
+	<message>
+        <source>Owner</source>
+        <translation>Owner</translation>
+    </message>
+	<message>
+        <source>responsable</source>
+        <translation>responsable</translation>
+    </message>
+	<message>
+        <source>Come from</source>
+        <translation>Come from</translation>
+    </message>
+	<message>
+        <source>E-mail</source>
+        <translation>E-mail</translation>
+    </message>
+	<message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message> 
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Live helper chat</source>
+        <translation>Live helper chat</translation>
+    </message>  
+	<message>
+        <source>New request</source>
+        <translation>New request</translation>
+    </message>
+	<message>
+        <source>You have new chat pending. To start chat click me.</source>
+        <translation>You have new chat pending. To start chat click me.</translation>
+    </message>
+	<message>
+        <source>To you have been transfered new chat, start chat click me.</source>
+        <translation>To you have been transfered new chat, start chat click me.</translation>
+    </message> 
+	<message>
+        <source>Waiting for action...</source>
+        <translation>Waiting for action...</translation>
+    </message> 
+	<message>
+        <source>&amp;Restore</source>
+        <translation>&amp;Restore</translation>
+    </message>
+	<message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quit</translation>
+    </message>
+	<message>
+        <source>E&amp;xit</source>
+        <translation>E&amp;xit</translation>
+    </message> 
+	<message>
+        <source>Login as</source>
+        <translation>Login as</translation>
+    </message>
+	<message>
+        <source>Chat rooms</source>
+        <translation>Chat rooms</translation>
+    </message>
+	<message>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+	<message>
+        <source>About program</source>
+        <translation>About program</translation>
+    </message>
+	<message>
+        <source>&amp;Actions</source>
+        <translation>&amp;Actions</translation>
+    </message>
+	<message>
+        <source>&amp;Chats</source>
+        <translation>&amp;Chats</translation>
+    </message>
+	<message>
+        <source>&amp;Management</source>
+        <translation>&amp;Management</translation>
+    </message>
+	<message>
+        <source>&amp;Help</source>
+        <translation>&amp;Help</translation>
+    </message>    
+</context>
+</TS>
