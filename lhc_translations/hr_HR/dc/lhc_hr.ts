d729b2861c5497bc43f4d98d93b42f1ffd7faa0a
Remigijus Kiminas <remdex@gmail.com>
2012-12-08 19:06:25 +0200

    Translations

---
+++ lhc_translations/hr_HR/dc/lhc_hr.ts
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="hr">
+<defaultcodec></defaultcodec>
+<context>
+    <name>ChatRoomsWidget</name>
+    <message>
+        <source>Live helper chat - chat rooms</source>
+        <translation>ONLINE pomoć - sobe za razgovor</translation>
+    </message>
+</context>
+<context>
+    <name>ChatRoomsWindow</name>
+    <message>
+        <source>Add chat</source>
+        <translation>Dodaj razgovor</translation>
+    </message>
+    <message>
+        <source>Open in new a window</source>
+        <translation>Otvori u novom prozoru</translation>
+    </message>
+    <message>
+        <source>Open in new a separate window</source>
+        <translation>Otvori u novom samostalnom prozoru</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation>Učitavam...</translation>
+    </message>
+    <message>
+        <source>Close chat</source>
+        <translation>Završi razgovor</translation>
+    </message>
+    <message>
+        <source>Delete chat</source>
+        <translation>Obriši razgovor</translation>
+    </message>
+    <message>
+        <source>Reject chat</source>
+        <translation>Odbaci razgovor</translation>
+    </message>
+    <message>
+        <source>Pending chats</source>
+        <translation>Razgovori na čekanju</translation>
+    </message>
+    <message>
+        <source>Transfered chats</source>
+        <translation>Preseljeni razgovori</translation>
+    </message>
+    <message>
+        <source>Active chats</source>
+        <translation>Aktivni razgovori</translation>
+    </message>
+    <message>
+        <source>Closed chats</source>
+        <translation>Završeni razgovori</translation>
+    </message>
+</context>
+<context>
+    <name>ChatWidget</name>
+    <message>
+        <source>Live helper chat - chat</source>
+        <translation>ONLINE pomoć</translation>
+    </message>
+</context>
+<context>
+    <name>ChatWindow</name>
+    <message>
+        <source>User left chat</source>
+        <translation>Korisnik završio razgovor</translation>
+    </message>
+    <message>
+        <source>User joined chat</source>
+        <translation>Korisnik započeo razgovor</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Aktivnosti</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation>Informacija</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation>Učitavam...</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>Vlasnik</translation>
+    </message>
+    <message>
+        <source>responsable</source>
+        <translation>odgovoran</translation>
+    </message>
+    <message>
+        <source>Come from</source>
+        <translation>Stigao od</translation>
+    </message>
+    <message>
+        <source>E-mail</source>
+        <translation>E-mail</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Greška</translation>
+    </message>
+</context>
+<context>
+    <name>LHQTableWidget</name>
+    <message>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Nick</source>
+        <translation>Nadimak</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>Vrijeme</translation>
+    </message>
+    <message>
+        <source>Referrer</source>
+        <translation>Odnosi se na</translation>
+    </message>
+    <message>
+        <source>IP</source>
+        <translation>IP</translation>
+    </message>
+    <message>
+        <source>E-mail</source>
+        <translation>E-mail</translation>
+    </message>
+    <message>
+        <source>Department</source>
+        <translation>OJ</translation>
+    </message>
+</context>
+<context>
+    <name>LhcTransferDialog</name>
+    <message>
+        <source>Chat transfer</source>
+        <translation>Prijenos razgovora</translation>
+    </message>
+    <message>
+        <source>Online users</source>
+        <translation>Online korisnici</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <source>Authentification failed</source>
+        <translation>Autentifikacija nije prošla</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Upozorenje</translation>
+    </message>
+    <message>
+        <source>Host field is empty!</source>
+        <translation>Host polje je prazno!</translation>
+    </message>
+    <message>
+        <source>Password field is empty!</source>
+        <translation>Polje lozinke je prazno!</translation>
+    </message>
+    <message>
+        <source>User name field is empty!</source>
+        <translation>Polje korisničko ime je prazno!</translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialogBase</name>
+    <message>
+        <source>Please login</source>
+        <translation>Molim prijavite se</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Korisničko ime</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Lozinka</translation>
+    </message>
+    <message>
+        <source>Host</source>
+        <translation>Host</translation>
+    </message>
+    <message>
+        <source>Autologin</source>
+        <translation>Automatska prijava</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Live helper chat</source>
+        <translation>ONLINE pomoć</translation>
+    </message>
+    <message>
+        <source>New request</source>
+        <translation>Novi zahtjev</translation>
+    </message>
+    <message>
+        <source>You have new chat pending. To start chat click me.</source>
+        <translation>Imate nove razgovore na čekanju. Za početak razgovora kliknite ovdje.</translation>
+    </message>
+    <message>
+        <source>To you have been transfered new chat, start chat click me.</source>
+        <translation>Prabačen vam je novi razgovor, pokrenite razgovor klikom ovdje.</translation>
+    </message>
+    <message>
+        <source>Waiting for action...</source>
+        <translation>Čekam izvršenje akcije...</translation>
+    </message>
+    <message>
+        <source>&amp;Restore</source>
+        <translation>&amp;Povrati</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Završi</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>I&amp;zađi</translation>
+    </message>
+    <message>
+        <source>Login as</source>
+        <translation>Prijavi se kao</translation>
+    </message>
+    <message>
+        <source>Chat rooms</source>
+        <translation>Sobe za razgovor</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <source>About program</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <source>&amp;Actions</source>
+        <translation>&amp;Akcije</translation>
+    </message>
+    <message>
+        <source>&amp;Chats</source>
+        <translation>&amp;Razgovori</translation>
+    </message>
+    <message>
+        <source>&amp;Management</source>
+        <translation>&amp;Upravljanje</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Pomoć</translation>
+    </message>
+</context>
+</TS>
