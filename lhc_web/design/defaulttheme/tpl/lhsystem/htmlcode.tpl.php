88c9e15be32998478715ac4357ad599f683d7203
Remdex <remdex@gmail.com>
2013-01-24 07:03:03 +0100

    Embed generation update

--- lhc_web/design/defaulttheme/tpl/lhsystem/htmlcode.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsystem/htmlcode.tpl.php
@@ -2,20 +2,26 @@
 <p>
 <select id="LocaleID" class="default-select">
     <?php foreach ($locales as $locale ) : ?>
-    <option value="<?php echo $locale?>"><?php echo $locale?></option>
+    <option value="<?php echo $locale?>/"><?php echo $locale?></option>
     <?php endforeach; ?>    
 </select>
 </p>
 
 <p class="explain"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/htmlcode','Copy code from textarea to page where you want your status to appear');?></p>
-<textarea style="width:100%;height:50px;font-size:11px;" id="HMLTContent" onclick="$(this).select()"><?php echo htmlspecialchars('<script type="text/javascript" src="http://'.$_SERVER['HTTP_HOST'].erLhcoreClassDesign::baseurl('/chat/getstatus').'"></script>')?></textarea>
+<textarea style="width:100%;height:50px;font-size:11px;" id="HMLTContent" onclick="$(this).select()"><?php echo htmlspecialchars('<script type="text/javascript" src="http://'.$_SERVER['HTTP_HOST'].erLhcoreClassDesign::baseurl('chat/getstatus').'"></script>')?></textarea>
 
 
 <script type="text/javascript">
 
+var default_site_access = '<?php echo erConfigClassLhConfig::getInstance()->getSetting( 'site', 'default_site_access' ); ?>/';
+
+
 $('#LocaleID').change(function(){    
-    $('#HMLTContent').html('&lt;script type=&quot;text/javascript&quot; src=&quot;http://<?php echo $_SERVER['HTTP_HOST']?><?php echo erLhcoreClassDesign::baseurldirect()?>'+$(this).val()+'/chat/getstatus'+'&quot;&gt;&lt;/script&gt;');
+    var siteAccess = $('#LocaleID').val() == default_site_access ? '' : $('#LocaleID').val();
+    $('#HMLTContent').html('&lt;script type=&quot;text/javascript&quot; src=&quot;http://<?php echo $_SERVER['HTTP_HOST']?><?php echo erLhcoreClassDesign::baseurldirect()?>'+siteAccess+'chat/getstatus'+'&quot;&gt;&lt;/script&gt;');
 });
 
-$('#HMLTContent').html('&lt;script type=&quot;text/javascript&quot; src=&quot;http://<?php echo $_SERVER['HTTP_HOST']?><?php echo erLhcoreClassDesign::baseurldirect()?>'+$('#LocaleID').val()+'/chat/getstatus'+'&quot;&gt;&lt;/script&gt;');
+
+var siteAccess = $('#LocaleID').val() == default_site_access ? '' : $('#LocaleID').val();
+$('#HMLTContent').html('&lt;script type=&quot;text/javascript&quot; src=&quot;http://<?php echo $_SERVER['HTTP_HOST']?><?php echo erLhcoreClassDesign::baseurldirect()?>'+siteAccess+'chat/getstatus'+'&quot;&gt;&lt;/script&gt;');
 </script>
