78004fe500de4c67d55355d0d540717ca707aeb4
Remdex <remdex@gmail.com>
2013-01-31 20:39:59 +0100

    Show last activity ago

--- lhc_web/design/defaulttheme/tpl/lhuser/userlist.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhuser/userlist.tpl.php
@@ -6,6 +6,7 @@
     <th width="1%">ID</th>
     <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/userlist','Username');?></th>
     <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/userlist','E-mail');?></th>
+    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/userlist','Last activity');?></th>
     <th width="1%">&nbsp;</th>
     <th width="1%">&nbsp;</th>
 </tr>
@@ -15,6 +16,7 @@
         <td><?php echo $user->id?></td>
         <td><?php echo $user->username?></td>
         <td><?php echo $user->email?></td>
+        <td><?php echo $user->lastactivity_ago?> ago</td>
         <td><a class="tiny button round" href="<?php echo erLhcoreClassDesign::baseurl('user/edit')?>/<?php echo $user->id?>">Edit</a></td>
         <td><a class="tiny alert button round" onclick="return confirm('<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('gallery/album_list_admin','Are you sure?');?>')" href="<?php echo erLhcoreClassDesign::baseurl('user/delete')?>/<?php echo $user->id?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/userlist','Delete');?></a></td>
     </tr>
--- lhc_web/lib/models/lhuser/erlhcoreclassmodeluser.php
+++ lhc_web/lib/models/lhuser/erlhcoreclassmodeluser.php
@@ -84,6 +84,38 @@ class erLhcoreClassModelUser {
        		 
        		   return $this->user_groups_id;
        		break;
+       				
+       	case 'lastactivity_front':
+       		   $this->lastactivity_front = '';
+       		   
+       		   if ( $this->lastactivity > 0 ) {
+       		       $this->lastactivity_front = date('Y-m-d H:i:s');
+       		   };
+       		   
+       		   return $this->lastactivity_front;
+       		break;
+       		
+       	case 'lastactivity_ago':
+       		   $this->lastactivity_ago = '';
+       		   
+       		   if ( $this->lastactivity > 0 ) {
+       		               		       
+                    $periods         = array("s.", "m.", "h.", "d.", "w.", "m.", "y.", "dec.");
+                    $lengths         = array("60","60","24","7","4.35","12","10");
+                                                                                                 
+                    $difference     = time() - $this->lastactivity;
+                                         
+                    for ($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+                        $difference /= $lengths[$j];
+                    }
+                 
+                    $difference = round($difference);
+                    
+                   $this->lastactivity_ago = "$difference $periods[$j]";       		       
+       		   };
+       		   
+       		   return $this->lastactivity_ago;
+       		break;
               
        	default:
        		break;
