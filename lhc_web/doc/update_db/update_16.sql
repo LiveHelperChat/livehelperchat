591026f6db500b7c97a2c411278186e693f90093
Remdex <remdex@gmail.com>
2013-03-30 08:36:07 +0100

    Update for SQL

--- lhc_web/doc/update_db/update_16.sql
+++ lhc_web/doc/update_db/update_16.sql
@@ -24,31 +24,22 @@ VALUES ('enable_close_list', '', '');
 INSERT INTO `lh_users_setting_option` (`identifier`, `class`, `attribute`)
 VALUES ('enable_unread_list', '', '');
 
-ALTER TABLE `lh_transfer`
-CHANGE `user_id` `dep_id` int(11) NOT NULL AFTER `chat_id`,
-COMMENT='';
-
-DELETE FROM `lh_transfer`;
-
-ALTER TABLE `lh_transfer`
-ADD `transfer_user_id` int(11) NOT NULL,
-ADD `from_dep_id` int(11) NOT NULL AFTER `transfer_user_id`,
-COMMENT='';
-
-ALTER TABLE `lh_transfer`
-ADD `transfer_to_user_id` int(11) NOT NULL,
-COMMENT='';
-
-ALTER TABLE `lh_transfer`
-ADD INDEX `transfer_user_id` (`transfer_user_id`),
-ADD INDEX `dep_id` (`dep_id`);
-
-ALTER TABLE `lh_transfer`
-ADD INDEX `transfer_user_id_dep_id` (`transfer_user_id`, `dep_id`),
-DROP INDEX `transfer_user_id`;
-
-ALTER TABLE `lh_transfer`
-ADD INDEX `transfer_to_user_id` (`transfer_to_user_id`);
-
 ALTER TABLE `lh_msg`
 ADD INDEX `chat_id_user_id_status` (`chat_id`, `user_id`, `status`);
+
+DROP TABLE `lh_transfer`;
+
+CREATE TABLE `lh_transfer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `chat_id` int(11) NOT NULL,
+  `dep_id` int(11) NOT NULL,
+  `transfer_user_id` int(11) NOT NULL,
+  `from_dep_id` int(11) NOT NULL,
+  `transfer_to_user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dep_id` (`dep_id`),
+  KEY `transfer_user_id_dep_id` (`transfer_user_id`,`dep_id`),
+  KEY `transfer_to_user_id` (`transfer_to_user_id`)
+);
+
+
