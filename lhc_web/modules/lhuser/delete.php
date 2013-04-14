094b740783aaab21a02c0244f2b34aebee11250a
Remdex <remdex@gmail.com>
2013-04-14 15:02:09 +0200

    Remove short tags

--- lhc_web/modules/lhuser/delete.php
+++ lhc_web/modules/lhuser/delete.php
@@ -3,24 +3,26 @@
 $departament = erLhcoreClassUser::getSession()->load( 'erLhcoreClassModelUser', $Params['user_parameters']['user_id']);
 erLhcoreClassUser::getSession()->delete($departament);
 
-$q = ezcDbInstance::get()->createDeleteQuery();
-
 // Transfered chats to user
-$q->deleteFrom( 'lh_transfer' )->where( $q->expr->eq( 'user_id', $Params['user_parameters']['user_id'] ) );
+$q = ezcDbInstance::get()->createDeleteQuery();
+$q->deleteFrom( 'lh_transfer' )->where( $q->expr->eq( 'transfer_user_id', $Params['user_parameters']['user_id'] ) );
 $stmt = $q->prepare();
 $stmt->execute();
 
 // User departaments
+$q = ezcDbInstance::get()->createDeleteQuery();
 $q->deleteFrom( 'lh_userdep' )->where( $q->expr->eq( 'user_id', $Params['user_parameters']['user_id'] ) );
 $stmt = $q->prepare();
 $stmt->execute();
 
 // User groups
+$q = ezcDbInstance::get()->createDeleteQuery();
 $q->deleteFrom( 'lh_groupuser' )->where( $q->expr->eq( 'user_id', $Params['user_parameters']['user_id'] ) );
 $stmt = $q->prepare();
 $stmt->execute();
 
 // User remember
+$q = ezcDbInstance::get()->createDeleteQuery();
 $q->deleteFrom( 'lh_users_remember' )->where( $q->expr->eq( 'user_id', $Params['user_parameters']['user_id'] ) );
 $stmt = $q->prepare();
 $stmt->execute();
