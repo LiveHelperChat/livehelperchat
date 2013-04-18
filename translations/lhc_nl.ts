ea2c8d69a7f34afda13ca4ee7b137db086e05cd7
Robin Stuivenberg <robin@abctravel.nl>
2013-04-18 12:36:45 +0200

    Added Dutch Translation An completed the LT cont
    Dutch translation files added, with completed contexts.
    Also added the Lithuanian translate files where i have completed the
    contexts.
    The comlpeted contexts are now translated with a space, as lithuanian is
    not a language i know;-)

---
+++ translations/lhc_nl.ts
@@ -0,0 +1,314 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL">
+<context>
+    <name>LHQTableWidget</name>
+    <message>
+        <source>ID</source>
+        <translatorcomment>ID</translatorcomment>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Nick</source>
+        <translatorcomment>Alias</translatorcomment>
+        <translation type="unfinished">Naam</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translatorcomment>Tijd</translatorcomment>
+        <translation type="unfinished">Tijd</translation>
+    </message>
+    <message>
+        <source>Referrer</source>
+        <translation type="unfinished">Verwijzende pagina</translation>
+    </message>
+    <message>
+        <source>IP</source>
+        <translation type="unfinished">IP-adres</translation>
+    </message>
+    <message>
+        <source>E-mail</source>
+        <translation type="unfinished">E-mail</translation>
+    </message>
+    <message>
+        <source>Department</source>
+        <translation type="unfinished">Afdeling</translation>
+    </message>
+	<message>
+        <source>Country</source>
+        <translation type="unfinished">Land</translation>
+    </message>
+	<message>
+        <source>Phone</source>
+        <translation type="unfinished">Telefoon</translation>
+    </message>
+		<message>
+        <source>Browser</source>
+        <translation type="unfinished">Browser</translation>
+    </message>
+		<message>
+        <source>Last visit</source>
+        <translation type="unfinished">Laatste bezoek</translation>
+    </message>
+		<message>
+        <source>First visit</source>
+        <translation type="unfinished">Eerste bezoek</translation>
+    </message>
+		<message>
+        <source>Operator message</source>
+        <translation type="unfinished">Bericht van mederker</translation>
+    </message>
+		<message>
+        <source>Pages viewed</source>
+        <translation type="unfinished">Aantal pagina's bezocht</translation>
+    </message>
+</context>
+<context>
+    <name>ChatWidget</name>
+    <message>
+        <source>Live helper chat - chat</source>
+        <translation type="unfinished">Live helper chat - chat</translation>
+    </message>
+</context>
+<context>
+    <name>ChatRoomsWidget</name>
+    <message>
+        <source>Live helper chat - chat rooms</source>
+        <translation type="unfinished">Live helper chat - chat overzicht</translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialogBase</name>
+    <message>
+        <source>Please login</source>
+        <translation type="unfinished">Login alstublieft</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation type="unfinished">Ok</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished">Annuleren</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation type="unfinished">Gebruikersnaam</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="unfinished">Wachtwoord</translation>
+    </message>
+    <message>
+        <source>Host</source>
+        <translation type="unfinished">Host</translation>
+    </message>
+    <message>
+        <source>Autologin</source>
+        <translation type="unfinished">Automatisch inloggen</translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <source>Authentification failed</source>
+        <translation type="unfinished">Inloggen mislukt</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished">Waarschuwing</translation>
+    </message>
+    <message>
+        <source>Host field is empty!</source>
+        <translation type="unfinished">Host adres is leeg!</translation>
+    </message>
+    <message>
+        <source>Password field is empty!</source>
+        <translation type="unfinished">Wachtwoord niet ingevuld!</translation>
+    </message>
+    <message>
+        <source>User name field is empty!</source>
+        <translation type="unfinished">Gebruikersnaam niet ingevuld!</translation>
+    </message>
+</context>
+<context>
+    <name>LhcTransferDialog</name>
+    <message>
+        <source>Chat transfer</source>
+        <translation type="unfinished">Chat doorgeven</translation>
+    </message>
+    <message>
+        <source>Online users</source>
+        <translation type="unfinished">Gebruikers online</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation type="unfinished">Ok</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished">Annuleren</translation>
+    </message>
+</context>
+<context>
+    <name>ChatRoomsWindow</name>
+    <message>
+        <source>Add chat</source>
+        <translation type="unfinished">Chatgesprek toevoegen</translation>
+    </message>
+    <message>
+        <source>Open in new a window</source>
+        <translation type="unfinished">Open in nieuw venster</translation>
+    </message>
+    <message>
+        <source>Open in new a separate window</source>
+        <translation type="unfinished">Open in een apart venster</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation type="unfinished">Laden...</translation>
+    </message>
+    <message>
+        <source>Close chat</source>
+        <translation type="unfinished">Chatgesprek sluiten</translation>
+    </message>
+    <message>
+        <source>Delete chat</source>
+        <translation type="unfinished">Chatgesprek verwijderen</translation>
+    </message>
+    <message>
+        <source>Reject chat</source>
+        <translation type="unfinished">Chatgesprek weigeren</translation>
+    </message>
+    <message>
+        <source>Pending chats</source>
+        <translation type="unfinished">Chatgesprekken in wachtrij</translation>
+    </message>
+    <message>
+        <source>Transfered chats</source>
+        <translation type="unfinished">Doorgegeven chatgesprekken</translation>
+    </message>
+    <message>
+        <source>Active chats</source>
+        <translation type="unfinished">Actieve chatgesprekken</translation>
+    </message>
+    <message>
+        <source>Closed chats</source>
+        <translation type="unfinished">Gesloten chatgesprekken</translation>
+    </message>
+	  <message>
+        <source>Online Users</source>
+        <translation type="unfinished">Online Gebruikers</translation>
+    </message>
+</context>
+<context>
+    <name>ChatWindow</name>
+    <message>
+        <source>User left chat</source>
+        <translation type="unfinished">Gebruiker heeft chatgesprek verlaten</translation>
+    </message>
+    <message>
+        <source>User joined chat</source>
+        <translation type="unfinished">Gebruiker aangemeld bij chat</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation type="unfinished">Acties</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation type="unfinished">Informatie</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation type="unfinished">Laden...</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation type="unfinished">Eigenaar</translation>
+    </message>
+    <message>
+        <source>responsable</source>
+        <translation type="unfinished">verantwoordelijk</translation>
+    </message>
+    <message>
+        <source>Come from</source>
+        <translation type="unfinished">Komt van</translation>
+    </message>
+    <message>
+        <source>E-mail</source>
+        <translation type="unfinished">E-mail</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished">Fout</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Live helper chat</source>
+        <translation type="unfinished">Live chat helpdesk</translation>
+    </message>
+    <message>
+        <source>New request</source>
+        <translation type="unfinished">Nieuw verzoek</translation>
+    </message>
+    <message>
+        <source>You have new chat pending. To start chat click me.</source>
+        <translation type="unfinished">Nieuw binnenkomend chatgesprek.Klik hier om deel te nemen.</translation>
+    </message>
+    <message>
+        <source>To you have been transfered new chat, start chat click me.</source>
+        <translation type="unfinished">Een chatgesprek is aan jou overgedragen, klik hier om deel te nemen.</translation>
+    </message>
+    <message>
+        <source>Waiting for action...</source>
+        <translation type="unfinished">Wachten op actie...</translation>
+    </message>
+    <message>
+        <source>&amp;Restore</source>
+        <translation type="unfinished">&amp;Herstel</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">&amp;Afsluiten</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation type="unfinished">&amp;Afsluiten</translation>
+    </message>
+    <message>
+        <source>Login as</source>
+        <translation type="unfinished">Inloggen als</translation>
+    </message>
+    <message>
+        <source>Chat rooms</source>
+        <translation type="unfinished">Chat Overzicht</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished">Over</translation>
+    </message>
+    <message>
+        <source>About program</source>
+        <translation type="unfinished">Over dit programma</translation>
+    </message>
+    <message>
+        <source>&amp;Actions</source>
+        <translation type="unfinished">&amp;Acties</translation>
+    </message>
+    <message>
+        <source>&amp;Chats</source>
+        <translation type="unfinished">&amp;Chatgesprekken</translation>
+    </message>
+    <message>
+        <source>&amp;Management</source>
+        <translation type="unfinished">&amp;Beheer</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished">&amp;Help</translation>
+    </message>
+</context>
+</TS>
