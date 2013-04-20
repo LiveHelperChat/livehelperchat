c672039ade015ee0bfa05667e78b9f83fd00c31a
Remdex <remdex@gmail.com>
2013-04-20 13:56:16 +0200

    Correct position detect

--- lhc_web/modules/lhchat/getstatus.php
+++ lhc_web/modules/lhchat/getstatus.php
@@ -17,8 +17,8 @@ $tpl->set('click',$Params['user_parameters_unordered']['click']);
 $tpl->set('position',$Params['user_parameters_unordered']['position']);
 $tpl->set('hide_offline',$Params['user_parameters_unordered']['hide_offline']);
 $tpl->set('check_operator_messages',$Params['user_parameters_unordered']['check_operator_messages']);
-$tpl->set('top_pos',(int)$Params['user_parameters_unordered']['top'] >= 0 ? (int)$Params['user_parameters_unordered']['top'] : 350);
-$tpl->set('units',key_exists((string)$Params['user_parameters_unordered']['units'], $validUnits) ? $validUnits[(string)$Params['user_parameters_unordered']['units']] : 'pixels');
+$tpl->set('top_pos',(!is_null($Params['user_parameters_unordered']['top']) && (int)$Params['user_parameters_unordered']['top'] >= 0) ? (int)$Params['user_parameters_unordered']['top'] : 350);
+$tpl->set('units',key_exists((string)$Params['user_parameters_unordered']['units'], $validUnits) ? $validUnits[(string)$Params['user_parameters_unordered']['units']] : 'px');
 
 
 echo $tpl->fetch();
--- lhc_web/modules/lhquestionary/getstatus.php
+++ lhc_web/modules/lhquestionary/getstatus.php
@@ -8,8 +8,8 @@ $validUnits = array('pixels' => 'px','percents' => '%');
 $tpl = erLhcoreClassTemplate::getInstance('lhquestionary/getstatus.tpl.php');
 $tpl->set('position',$Params['user_parameters_unordered']['position']);
 $tpl->set('expand',$Params['user_parameters_unordered']['expand']);
-$tpl->set('top_pos',(int)$Params['user_parameters_unordered']['top'] >= 0 ? (int)$Params['user_parameters_unordered']['top'] : 400);
-$tpl->set('units',key_exists((string)$Params['user_parameters_unordered']['units'], $validUnits) ? $validUnits[(string)$Params['user_parameters_unordered']['units']] : 'pixels');
+$tpl->set('top_pos',(!is_null($Params['user_parameters_unordered']['top']) && (int)$Params['user_parameters_unordered']['top'] >= 0) ? (int)$Params['user_parameters_unordered']['top'] : 400);
+$tpl->set('units',key_exists((string)$Params['user_parameters_unordered']['units'], $validUnits) ? $validUnits[(string)$Params['user_parameters_unordered']['units']] : 'px');
 
 echo $tpl->fetch();
 exit;
