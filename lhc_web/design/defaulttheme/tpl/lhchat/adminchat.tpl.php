a29e8fbb2e1b2d8a5194ee685d37c5870efcb984
Remdex <remdex@gmail.com>
2013-07-25 19:11:28 +0200

    Grammar fix

--- lhc_web/design/defaulttheme/tpl/lhchat/adminchat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/adminchat.tpl.php
@@ -200,7 +200,7 @@ lhinst.initTypingMonitoringAdmin('<?php echo $chat->id?>');
 					<?php if ($chat->lat != 0 && $chat->lon) : ?>
 					<img src="http://maps.google.com/maps/api/staticmap?zoom=13&size=400x300&maptype=roadmap&center=<?php echo $chat->lat?>,<?php echo $chat->lon?>&sensor=false&markers=color:green|<?php echo $chat->lat?>,<?php echo $chat->lon?>" alt="" title="<?php echo $chat->lat?>,<?php echo $chat->lon?>" />
 					<?php else : ?>
-					<p><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Could not detect. Make sure GEO that detection is enabled')?></p>
+					<p><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Could not detect. Make sure that GEO detection is enabled.')?></p>
 					<?php endif;?>
 				</div>
 			</section>
