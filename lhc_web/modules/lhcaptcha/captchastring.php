04cef0c9cc6bd2b6f7fff57c1777ff843248e3f8
Remdex <remdex@gmail.com>
2013-09-14 16:04:54 +0200

    Remove legacy session variable set

--- lhc_web/modules/lhcaptcha/captchastring.php
+++ lhc_web/modules/lhcaptcha/captchastring.php
@@ -1,8 +1,6 @@
 <?php
 
 $hash = sha1($_SERVER['REMOTE_ADDR'].$Params['user_parameters']['timets'].erConfigClassLhConfig::getInstance()->getSetting( 'site', 'secrethash' ));
-$_SESSION[$_SERVER['REMOTE_ADDR']][$Params['user_parameters']['captcha_name']] = $hash;
-
 echo json_encode(array('result' => $hash));
 
 exit;
