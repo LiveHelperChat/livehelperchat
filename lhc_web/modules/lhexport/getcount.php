17a024a5c9c8b896f83db678b3a77b5a13050fcd
Remdex <remdex@gmail.com>
2013-09-18 17:05:57 +0200

    Before switch

--- lhc_web/modules/lhexport/getcount.php
+++ lhc_web/modules/lhexport/getcount.php
@@ -3,6 +3,7 @@
 $hash = (string)$Params['user_parameters']['hash'];
 $format = (string)$Params['user_parameters_unordered']['format'] == 'xml' ? 'xml' : 'json';
 
+
 $hashSecret = erLhcoreClassModelChatConfig::fetch('export_hash')->current_value;
 
 if (sha1('getcount'.$hashSecret) == $hash) {
