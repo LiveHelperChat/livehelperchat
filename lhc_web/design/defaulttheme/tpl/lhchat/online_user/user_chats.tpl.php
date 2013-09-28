123d44f7210dd2fc24922491acb83884971afc25
Remdex <remdex@gmail.com>
2013-09-28 21:46:05 +0200

    Do not show current chat

--- lhc_web/design/defaulttheme/tpl/lhchat/online_user/user_chats.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/online_user/user_chats.tpl.php
@@ -3,13 +3,16 @@
     <div class="content" data-section-content>
       	<div>
 		<ul class="foot-print-content circle mb0" style="max-height: 170px;">
-		<?php foreach (erLhcoreClassChat::getList(array('limit' => 100, 'filter' => array('online_user_id' => $online_user->id))) as $chat) : ?>
-		<li>
-		  <?php if ( !empty($chat->country_code) ) : ?><img src="<?php echo erLhcoreClassDesign::design('images/flags');?>/<?php echo $chat->country_code?>.png" alt="<?php echo htmlspecialchars($chat->country_name)?>" title="<?php echo htmlspecialchars($chat->country_name)?>" />&nbsp;<?php endif; ?>
-	      <img class="action-image" align="absmiddle" data-title="<?php echo htmlspecialchars($chat->nick,ENT_QUOTES);?>" onclick="lhinst.startChatNewWindow('<?php echo $chat->id;?>',$(this).attr('data-title'))" src="<?php echo erLhcoreClassDesign::design('images/icons/application_add.png');?>" alt="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Open in a new window');?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Open in a new window');?>"><?php echo $chat->id;?>. <?php echo htmlspecialchars($chat->nick);?> (<?php echo date('Y-m-d H:i:s',$chat->time);?>) (<?php echo htmlspecialchars($chat->department);?>)
-		</li>
+		<?php foreach (erLhcoreClassChat::getList(array('limit' => 100, 'filter' => array('online_user_id' => $online_user->id))) as $chatPrev) : ?>
+			<?php if (!isset($chat) || $chat->id != $chatPrev->id) : ?>
+				<li>
+				  <?php if ( !empty($chatPrev->country_code) ) : ?><img src="<?php echo erLhcoreClassDesign::design('images/flags');?>/<?php echo $chatPrev->country_code?>.png" alt="<?php echo htmlspecialchars($chatPrev->country_name)?>" title="<?php echo htmlspecialchars($chatPrev->country_name)?>" />&nbsp;<?php endif; ?>
+			      <img class="action-image" align="absmiddle" data-title="<?php echo htmlspecialchars($chatPrev->nick,ENT_QUOTES);?>" onclick="lhinst.startChatNewWindow('<?php echo $chatPrev->id;?>',$(this).attr('data-title'))" src="<?php echo erLhcoreClassDesign::design('images/icons/application_add.png');?>" alt="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Open in a new window');?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Open in a new window');?>"><?php echo $chatPrev->id;?>. <?php echo htmlspecialchars($chatPrev->nick);?> (<?php echo date('Y-m-d H:i:s',$chatPrev->time);?>) (<?php echo htmlspecialchars($chatPrev->department);?>)
+				</li>
+			<?php endif; ?>
 		<?php endforeach;?>
 		</ul>
+
 		</div>
     </div>
 </section>
