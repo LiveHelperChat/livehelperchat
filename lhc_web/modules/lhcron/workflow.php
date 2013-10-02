3b7413dd1bf8c23ef36c0998274fb0cf92f4852c
Remdex <remdex@gmail.com>
2013-10-02 18:12:55 +0200

    Same logic for crojob

--- lhc_web/modules/lhcron/workflow.php
+++ lhc_web/modules/lhcron/workflow.php
@@ -16,8 +16,20 @@ if ( erLhcoreClassModelChatConfig::fetch('run_departments_workflow')->current_va
 	$ts = time();
 
 	foreach (erLhcoreClassChat::getList(array('limit' => 500, 'customfilter' => array('transfer_timeout_ts < ('.$ts.'-transfer_timeout_ac)'), 'filter' => array('status' => erLhcoreClassModelChat::STATUS_PENDING_CHAT, 'transfer_if_na' => 1))) as $chat){
-		erLhcoreClassChatWorkflow::transferWorkflow($chat);
-		echo "executing department transfer workflow for - ",$chat->id,"\n";
+		$canExecuteWorkflow = true;
+
+		if (erLhcoreClassModelChatConfig::fetch('pro_active_limitation')->current_value >= 0) {
+			if ($chat->department !== false && $chat->department->department_transfer_id > 0) {
+				$canExecuteWorkflow = erLhcoreClassChat::getPendingChatsCountPublic($chat->department->department_transfer_id) <= erLhcoreClassModelChatConfig::fetch('pro_active_limitation')->current_value;
+			}
+		}
+
+		if ($canExecuteWorkflow == true) {
+			erLhcoreClassChatWorkflow::transferWorkflow($chat);
+			echo "executing department transfer workflow for - ",$chat->id,"\n";
+		} else {
+			echo "Skipping transfer because dedicated department queue is full\n";
+		}
 	}
 
 	echo "Ended departments workflow\n";
