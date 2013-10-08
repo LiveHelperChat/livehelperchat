a0555ba69ac42d5a71764afc8833a330f07a2f80
Remdex <remdex@gmail.com>
2013-10-08 16:52:47 +0200

    Cleanup

--- lhc_web/modules/lhchatarchive/module.php
+++ lhc_web/modules/lhchatarchive/module.php
@@ -5,37 +5,31 @@ $Module = array( "name" => "Chat archive module");
 $ViewList = array();
 
 $ViewList['archive'] = array(
-    'script' => 'archive.php',
     'params' => array(),
     'functions' => array( 'archive' )
 );
 
 $ViewList['newarchive'] = array(
-    'script' => 'newarchive.php',
     'params' => array(),
     'functions' => array( 'archive' )
 );
 
 $ViewList['startarchive'] = array(
-    'script' => 'startarchive.php',
     'params' => array(),
     'functions' => array( 'archive' )
 );
 
 $ViewList['archivechats'] = array(
-    'script' => 'archivechats.php',
     'params' => array(),
     'functions' => array( 'archive' )
 );
 
 $ViewList['list'] = array(
-    'script' => 'list.php',
     'params' => array(),
     'functions' => array( 'archive' )
 );
 
 $ViewList['listarchivechats'] = array(
-    'script' => 'listarchivechats.php',
     'params' => array('id'),
     'functions' => array( 'archive' )
 );
@@ -46,7 +40,6 @@ $ViewList['edit'] = array(
 );
 
 $ViewList['viewarchivedchat'] = array(
-    'script' => 'viewarchivedchat.php',
     'params' => array('archive_id','chat_id'),
     'functions' => array( 'archive' )
 );
@@ -58,7 +51,6 @@ $ViewList['deletearchivechat'] = array(
 );
 
 $ViewList['process'] = array(
-    'script' => 'process.php',
     'params' => array('id'),
     'functions' => array( 'archive' )
 );
