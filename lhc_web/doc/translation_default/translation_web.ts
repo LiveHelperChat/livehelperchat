4477e17cd2fd08a7e4c0446ef5f3635e7e8372df
Remdex <remdex@gmail.com>
2013-11-27 18:42:17 +0100

    Translations update

--- lhc_web/doc/translation_default/translation_web.ts
+++ lhc_web/doc/translation_default/translation_web.ts
@@ -2585,6 +2585,10 @@
       <translation type="unfinished"/>
     </message>
     <message>
+      <source>Skype</source>
+      <translation type="unfinished"/>
+    </message>
+    <message>
       <source>Photo</source>
       <translation type="unfinished"/>
     </message>
@@ -4448,6 +4452,13 @@
     </message>
     <message>
       <source>XMPP message was sent succesfuly</source>
+      <translation type="unfinished"/>
+    </message>
+  </context>
+  <context>
+    <name>chat/part/operator_profile</name>
+    <message>
+      <source>Skype call</source>
       <translation type="unfinished"/>
     </message>
   </context>
