d60fd3554dce6e2e9f28e14075c2537d069efeab
Remdex <remdex@gmail.com>
2014-03-08 11:51:37 +0100

    Remove space

--- lhc_web/modules/lhinstall/install.php
+++ lhc_web/modules/lhinstall/install.php
@@ -305,7 +305,7 @@ switch ((int)$Params['user_parameters']['step_id']) {
                   KEY `ip` (`ip`)
                 ) DEFAULT CHARSET=utf8;");
 
-        	   $db->query(" CREATE TABLE `lh_chat_archive_range` (
+        	   $db->query("CREATE TABLE `lh_chat_archive_range` (
         	   `id` int(11) NOT NULL AUTO_INCREMENT,
         	   `range_from` int(11) NOT NULL,
         	   `range_to` int(11) NOT NULL,
