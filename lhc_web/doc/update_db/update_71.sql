fa7fd5b40d1b530cbe68138418da4f06aa504b94
Remdex <remdex@gmail.com>
2014-03-22 15:22:03 +0100

    Database update

---
+++ lhc_web/doc/update_db/update_71.sql
@@ -0,0 +1,2 @@
+INSERT INTO `lh_chat_config` (`identifier`, `value`, `type`, `explain`, `hidden`) VALUES ('disable_print','0',0,'Disable chat print', '0');
+INSERT INTO `lh_chat_config` (`identifier`, `value`, `type`, `explain`, `hidden`) VALUES ('disable_send','0',0,'Disable chat transcript send', '0');
