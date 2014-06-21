c6790b28f45aad51fb59b80eef02a6eff7627d27
Remdex <remdex@gmail.com>
2014-06-21 20:11:36 +0200

    Preparing for release 1

--- lhc_web/design/defaulttheme/tpl/lhsystem/update/statusdb.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsystem/update/statusdb.tpl.php
@@ -14,7 +14,7 @@
 		$queries = array_merge($queries,$status['queries']);
 		$hasError = $status['error'] == true ? true : $hasError;
 		if ($status['error'] == true) : ?>
-			<li><label class="<?php echo $status['error'] == false ? 'success' : 'alert'?> label"><?php echo $table?> - <?php echo $status['status']?></label></li>
+			<li><div class="alert-box <?php echo $status['error'] == false ? 'success' : 'alert'?>"><?php echo $table?> - <?php echo $status['status']?></div></li>
 		<?php endif; endforeach;?>
 		</ul>
 		<?php if ($hasError == false) : ?>
@@ -22,18 +22,20 @@
 		<?php endif; ?>
 		
 		<?php if ($hasError) : ?>
-		<a class="button radius small" onclick="updateDatabase()"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('update/statusdb','Update database')?></a>
+		<a class="button radius success small" onclick="updateDatabase()"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('update/statusdb','Update database')?></a>
 		<?php endif;?>
 
 	</div>
 	<?php if ( !empty($queries) ) : ?>
 	<div class="columns small-12">
+	<div class="panel">
 		<h3><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('update/statusdb','Queries which will be executed on update')?></h3>
 		<ul class="fs11 circle">
 			<?php foreach ($queries as $query) : ?>
 				<li class="fs11"><?php echo $query;?></li>
 			<?php endforeach; ?>
 		</ul>
+	</div>
 	</div>
 	<?php endif; ?>
 </div>
