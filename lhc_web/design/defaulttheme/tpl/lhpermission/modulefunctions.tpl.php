e958111331b34ea83c20704947feaf3878ec35e0
Remdex <remdex@gmail.com>
2014-06-24 16:59:36 +0200

    #414 #415

--- lhc_web/design/defaulttheme/tpl/lhpermission/modulefunctions.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhpermission/modulefunctions.tpl.php
@@ -1,6 +1,6 @@
-<select name="ModuleFunction">
-<option value="*"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('permission/modulefunctions','All functions');?></option>
+<label class="fs12"><input class="mb0" type="checkbox" name="ModuleFunction[]" value="*"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('permission/modulefunctions','All functions');?></label>
+
 <?php foreach ($functions as $key => $Function) : ?>
-    <option value="<?php echo $key?>"><?php echo htmlspecialchars($Function['explain'])?></option>
+<label class="fs12"><input class="mb0" type="checkbox" name="ModuleFunction[]" value="<?php echo $key?>"> <?php echo htmlspecialchars($Function['explain'])?></label>
 <?php endforeach; ?>
-</select>
+<br/>
