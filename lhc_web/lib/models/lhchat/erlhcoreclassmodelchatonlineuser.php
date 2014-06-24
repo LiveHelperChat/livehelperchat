71305be9f94779b5e3b53e5824dc12193fd5bd87
Remdex <remdex@gmail.com>
2014-06-24 15:58:55 +0200

    Add automatic follow

--- lhc_web/lib/models/lhchat/erlhcoreclassmodelchatonlineuser.php
+++ lhc_web/lib/models/lhchat/erlhcoreclassmodelchatonlineuser.php
@@ -287,8 +287,9 @@ class erLhcoreClassModelChatOnlineUser {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 5);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 5);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         $content = curl_exec($ch);
 
         return $content;
