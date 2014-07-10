e2569fa6d712320f5697c0084fccc17843198c4c
Remdex <remdex@gmail.com>
2014-07-10 07:19:22 +0200

    Show operator name as image title

--- lhc_web/modules/lhchat/readoperatormessage.php
+++ lhc_web/modules/lhchat/readoperatormessage.php
@@ -182,7 +182,7 @@ if (isset($_POST['askQuestion']))
 		       	$msg = new erLhcoreClassModelmsg();
 		       	$msg->msg = trim($userInstance->invitation->wait_message);
 		       	$msg->chat_id = $chat->id;
-		       	$msg->name_support = $userInstance->operator_user !== false ? trim($userInstance->operator_user->name.' '.$userInstance->operator_user->surname) : (!empty($userInstance->operator_user_proactive) ? $userInstance->operator_user_proactive : erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Live Support'));
+		       	$msg->name_support = $userInstance->operator_user !== false ? $userInstance->operator_user->name_support : (!empty($userInstance->operator_user_proactive) ? $userInstance->operator_user_proactive : erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Live Support'));
 		       	$msg->user_id = $userInstance->operator_user_id > 0 ? $userInstance->operator_user_id : 1;
 		       	$msg->time = time()+5;
 		       	erLhcoreClassChat::getSession()->save($msg);
