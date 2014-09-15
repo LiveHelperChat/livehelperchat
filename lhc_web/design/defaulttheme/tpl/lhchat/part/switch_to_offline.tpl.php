a3513f613bb91a87f617cc6d2dbbb239b147db56
Remdex <remdex@gmail.com>
2014-09-15 19:06:44 +0200

    Changes #511

--- lhc_web/design/defaulttheme/tpl/lhchat/part/switch_to_offline.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/switch_to_offline.tpl.php
@@ -3,19 +3,22 @@
   <h3><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Department is offline');?></h3>
   <ul class="button-group radius">
   	<li><a href="#" class="button tiny success" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Go to leave a message form');?>" onclick="return lhinst.switchToOfflineForm();"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Leave a message');?></a></li>
-  	<li><a href="#" class="button tiny secondary" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Cancel and choose another department');?>" onclick="return lhinst.closeReveal('#offline-modal');"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/buttons','Cancel');?></a></li>
+  	<li><a href="#" class="button tiny secondary" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Cancel and choose another department');?>" onclick="closeRevealForm();return lhinst.closeReveal('#offline-modal');"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/buttons','Cancel');?></a></li>
   </ul>
-  <a class="close-reveal-modal">&#215;</a>
+  <a class="close-reveal-modal" onclick="closeRevealForm();">&#215;</a>
 </div>
 <script>
+function closeRevealForm() {
+	$('#id_DepartamentID').find("[data-attr-online='true']").attr('selected',true);
+};
 $('#id_DepartamentID').change(function() {	
-	if ( $(this).find('option:selected').attr('data-attr-online') == 'false' ) {
-		$('#offline-modal').foundation('reveal','open');
+	if ( $(this).find('option:selected').attr('data-attr-online') == 'false' ) {		
+		$('#offline-modal').foundation('reveal','open',{closeOnBackgroundClick: false});
 	};
 });
 $(document).ready(function() {
 	if ( $('#id_DepartamentID').find('option:selected').attr('data-attr-online') == 'false' ) {
-		$('#offline-modal').foundation('reveal','open');
+		$('#offline-modal').foundation('reveal','open',{closeOnBackgroundClick: false});
 	};
 });
 </script>
