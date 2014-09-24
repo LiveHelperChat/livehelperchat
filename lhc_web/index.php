6cec0b5b11f8714e44aa6a7f184e71f546821178
Remgijus Kiminas <root@devauto.livehelperchat.com>
2014-09-24 12:32:00 -0400

    Local changes

--- lhc_web/index.php
+++ lhc_web/index.php
@@ -1,19 +1,17 @@
 <?php
 
-@ini_set('error_reporting', 0);
-@ini_set('display_errors', 0);
+@ini_set('error_reporting', E_ALL);
+@ini_set('display_errors', 1);
 @ini_set('session.gc_maxlifetime', 200000);
 @ini_set('session.cookie_lifetime', 2000000);
 @ini_set('session.cookie_httponly',1);
 
 require_once "ezcomponents/Base/src/base.php"; // dependent on installation method, see below
 
-function __autoload( $className )
-{
-        ezcBase::autoload( $className );
-}
-
 ezcBase::addClassRepository( './','./lib/autoloads');
+
+spl_autoload_register(array('ezcBase','autoload'), true, false);
+
 erLhcoreClassSystem::init();
 
 // your code here
