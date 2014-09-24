1d753039ebb3b575d3bb893a269fcfbe8c41fc0c
Remgijus Kiminas <root@devauto.livehelperchat.com>
2014-09-24 14:10:47 -0400

    No errors

--- lhc_web/index.php
+++ lhc_web/index.php
@@ -1,7 +1,7 @@
 <?php
 
-@ini_set('error_reporting', E_ALL);
-@ini_set('display_errors', 1);
+@ini_set('error_reporting', 0);
+@ini_set('display_errors', 0);
 @ini_set('session.gc_maxlifetime', 200000);
 @ini_set('session.cookie_lifetime', 2000000);
 @ini_set('session.cookie_httponly',1);
