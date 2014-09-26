61ebc72dac5c55a36f3ead544e60fca9b5c898c5
Remdex <remdex@gmail.com>
2014-09-26 20:40:23 +0200

    Upgrade desktop client accept workflow Part 2

--- lhc_web/modules/lhxml/closechat.php
+++ lhc_web/modules/lhxml/closechat.php
@@ -11,16 +11,29 @@ $chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params
 // Chat can be closed only by owner
 if ($chat->user_id == $currentUser->getUserID() || $currentUser->hasAccessTo('lhchat','allowcloseremote'))
 {
-    $chat->status = 2;
-    erLhcoreClassChat::getSession()->update($chat);
-           
-    erLhcoreClassChat::updateActiveChats($chat->user_id);
-    
-    $userData = $currentUser->getUserData(true);
-    
-    // Execute callback for close chat
-    erLhcoreClassChat::closeChatCallback($chat,$userData);
-    
+	if ($chat->status != erLhcoreClassModelChat::STATUS_CLOSED_CHAT) {
+
+	    $chat->status = erLhcoreClassModelChat::STATUS_CLOSED_CHAT;
+	    $chat->chat_duration = erLhcoreClassChat::getChatDurationToUpdateChatID($chat->id);
+
+	    $userData = $currentUser->getUserData(true);
+
+	    $msg = new erLhcoreClassModelmsg();
+	    $msg->msg = (string)$userData.' '.erTranslationClassLhTranslation::getInstance()->getTranslation('chat/closechatadmin','has closed the chat!');
+	    $msg->chat_id = $chat->id;
+	    $msg->user_id = -1;
+
+	    $chat->last_user_msg_time = $msg->time = time();
+
+	    erLhcoreClassChat::getSession()->save($msg);
+
+	    $chat->updateThis();
+	    
+	    erLhcoreClassChat::updateActiveChats($chat->user_id);
+	    
+	    // Execute callback for close chat
+	    erLhcoreClassChat::closeChatCallback($chat,$userData);
+	}    
 }
 
 exit;
