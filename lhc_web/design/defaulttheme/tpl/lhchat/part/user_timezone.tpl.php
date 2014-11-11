71c8edbf2ed6970ba13edf53c9e9265c24a1dead
root <root@remdex>
2014-11-11 18:32:54 +0100

    fix #541 DST stuff

--- lhc_web/design/defaulttheme/tpl/lhchat/part/user_timezone.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/user_timezone.tpl.php
@@ -1,6 +1,26 @@
 <input type="hidden" name="user_timezone" value="" />
 <script>
 $(document).ready(function() {
-  	$('input[name=user_timezone]').val(((new Date().getTimezoneOffset())/60) * -1);
+	
+	Date.prototype.stdTimezoneOffset = function() {
+	    var jan = new Date(this.getFullYear(), 0, 1);
+	    var jul = new Date(this.getFullYear(), 6, 1);
+	    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+	}
+
+	Date.prototype.dst = function() {
+	    return this.getTimezoneOffset() < this.stdTimezoneOffset();
+	}
+	
+	var today = new Date();
+	var timeZoneOffset = 0;
+	
+	if (today.dst()) { 
+		timeZoneOffset = today.getTimezoneOffset();
+	} else {
+		timeZoneOffset = today.getTimezoneOffset()+3600;
+	}
+	
+  	$('input[name=user_timezone]').val(((timeZoneOffset)/60) * -1);
 });
 </script>
