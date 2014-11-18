a8f01db45fd6404c52f4241c21595b82a9e65e96
Allan Dumaine <ADumaine@users.noreply.github.com>
2014-11-18 10:14:41 -0700

    Update lhlog.php
    Now writes only one entry each time it is called. Previously each call added an additional line of the entry being written during that session. e.g. the tenth time the log is written there would be 10 duplicate entries.

--- lhc_web/lib/core/lhcore/lhlog.php
+++ lhc_web/lib/core/lhcore/lhlog.php
@@ -1,28 +1,31 @@
 <?php
 
-class erLhcoreClassLog {
-           
-    static function write($msg)
-    {
-         // Get the one and only instance of the ezcLog.
-         $log = ezcLog::getInstance();
-         // Get an instance to the default log mapper.
-         $mapper = $log->getMapper();
-         // Create a new Unix file writer, that writes to the file: "default.log".
-         $writer = new ezcLogUnixFileWriter( "cache", "default.log" );
-         // Create a filter that accepts every message (default behavior).
-         $filter = new ezcLogFilter;
-         // Combine the filter with the writer in a filter rule.
-         $rule = new ezcLogFilterRule( $filter, $writer, true );
-         // And finally assign the rule to the mapper.
-         $mapper->appendRule( $rule );
-         // Write a message to the log
-         $log->log( $msg, ezcLog::WARNING ); 
-    }
-    
-  
+class erLhcoreClassLog implements ezcBaseConfigurationInitializer {
 
+	//to write to the log put a write statement in the code with the content to log
+	//
+	//Example:	erLhcoreClassLog::write(print_r($_POST)); 
+	//or:      erLhcoreClassLog::write("Log entry");
+	//
+	//default log location is CACHE/DEFAULT.LOG since it is writable  Change below.
+
+	static function write($msg)
+		{
+		           // Use log
+		           $log = ezcLog::getInstance();
+	           	$log->log( $msg, ezcLog::WARNING ); 
+		}
+
+	 public static function configureObject( $log )
+		{
+		           $writeAll = new ezcLogUnixFileWriter( "cache", "default.log" );
+			$log->getMapper()->appendRule( new ezcLogFilterRule( new ezcLogFilter, $writeAll, true ) );
+		}
 }
 
+ezcBaseInit::setCallback( 
+    'ezcInitLog', 
+    'erLhcoreClassLog'
+);
 
 ?>
