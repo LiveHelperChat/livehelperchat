331be532af2955b52c53c521ead8711677892b31
Remdex <remdex@gmail.com>
2014-11-27 21:34:23 +0100

    Cleanup

--- lhc_web/lib/core/lhcore/lhlog.php
+++ lhc_web/lib/core/lhcore/lhlog.php
@@ -1,31 +1,25 @@
 <?php
 
 class erLhcoreClassLog implements ezcBaseConfigurationInitializer {
-
-	//to write to the log put a write statement in the code with the content to log
+	
+	// to write to the log put a write statement in the code with the content to log
 	//
-	//Example:	erLhcoreClassLog::write(print_r($_POST)); 
-	//or:      erLhcoreClassLog::write("Log entry");
+	// Example: erLhcoreClassLog::write(print_r($_POST,true));
+	// or: erLhcoreClassLog::write("Log entry");
 	//
-	//default log location is CACHE/DEFAULT.LOG since it is writable  Change below.
-
-	static function write($msg)
-		{
-		           // Use log
-		           $log = ezcLog::getInstance();
-	           	$log->log( $msg, ezcLog::WARNING ); 
-		}
-
-	 public static function configureObject( $log )
-		{
-		           $writeAll = new ezcLogUnixFileWriter( "cache", "default.log" );
-			$log->getMapper()->appendRule( new ezcLogFilterRule( new ezcLogFilter, $writeAll, true ) );
-		}
+	// default log location is CACHE/DEFAULT.LOG since it is writable Change below.
+	static function write($msg) {
+		// Use log
+		$log = ezcLog::getInstance ();
+		$log->log ( $msg, ezcLog::WARNING );
+	}
+	
+	public static function configureObject($log) {
+		$writeAll = new ezcLogUnixFileWriter ( "cache", "default.log" );
+		$log->getMapper ()->appendRule ( new ezcLogFilterRule ( new ezcLogFilter (), $writeAll, true ) );
+	}
 }
 
-ezcBaseInit::setCallback( 
-    'ezcInitLog', 
-    'erLhcoreClassLog'
-);
+ezcBaseInit::setCallback ( 'ezcInitLog', 'erLhcoreClassLog' );
 
 ?>
