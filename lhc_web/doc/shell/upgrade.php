c26b045e846a2b4dc6a85ee5913a4bb97bace46f
Petri Iivonen <petri.iivonen@tmbc.gov.uk>
2014-11-27 09:19:48 +0000

    added workaround to https://bugs.php.net/bug.php?id=49184

--- lhc_web/doc/shell/upgrade.php
+++ lhc_web/doc/shell/upgrade.php
@@ -7,8 +7,20 @@
 @ini_set('session.gc_maxlifetime', 200000);
 @ini_set('session.cookie_lifetime', 2000000);
 @ini_set('session.cookie_httponly',1);
-if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
-@ini_set('session.cookie_secure',1);
+// https://bugs.php.net/bug.php?id=49184
+// https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=730094
+if (filter_has_var(INPUT_SERVER, "HTTPS")) {
+        $is_secure_conn = filter_input(INPUT_SERVER, "HTTPS",
+FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE);
+    } else {
+        if (isset($_SERVER["HTTPS"]))
+            $is_secure_conn = filter_var($_SERVER["HTTPS"],
+FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE);
+        else
+            $is_secure_conn = null;
+    }
+if ($is_secure_conn != null) {
+    @ini_set('session.cookie_secure',1);
 }
 
 require_once "ezcomponents/Base/src/base.php"; // dependent on installation method, see below
