f6949c48bf0c685c42e0165c1fbfa33ddfc9f0bf
Remdex <remdex@gmail.com>
2014-12-14 15:35:38 +0100

    enhancements #532

--- lhc_web/design/defaulttheme/tpl/lhchat/adminchat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/adminchat.tpl.php
@@ -17,15 +17,16 @@
 				<?php endif;?>
 
 			</div>
-			<div class="user-is-typing" id="user-is-typing-<?php echo $chat->id?>">
-				<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','User is typing now...')?>
-			</div>
+			
 		</div>
 
 		<?php include(erLhcoreClassDesign::designtpl('lhchat/part/above_textarea.tpl.php')); ?>
 
 		<textarea rows="4" <?php if ($chat->status == erLhcoreClassModelChat::STATUS_CLOSED_CHAT) : ?>readonly="readonly"<?php endif;?> name="ChatMessage" id="CSChatMessage-<?php echo $chat->id?>"></textarea>
-
+		<div class="user-is-typing" id="user-is-typing-<?php echo $chat->id?>">
+				<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','User is typing now...')?>
+		</div>
+			
 		<script type="text/javascript">
 		jQuery('#CSChatMessage-<?php echo $chat->id?>').bind('keydown', 'return', function (evt){
 		    lhinst.addmsgadmin('<?php echo $chat->id?>');
