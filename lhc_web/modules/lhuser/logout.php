940987dcafa07f0a212f45a308907632d855ea05
Remdex <remdex@gmail.com>
2014-12-27 15:42:27 +0100

    User logout event

--- lhc_web/modules/lhuser/logout.php
+++ lhc_web/modules/lhuser/logout.php
@@ -1,6 +1,9 @@
 <?php
 
 $lhUser = erLhcoreClassUser::instance();
+
+erLhcoreClassChatEventDispatcher::getInstance()->dispatch('user.logout',array('user' => & $lhUser));
+
 $lhUser->logout();
 
 erLhcoreClassModule::redirect('user/login');
