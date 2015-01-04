3a433f9d2d7b4a3eaca05569ad12b8ebb380a237
Remdex <remdex@gmail.com>
2015-01-04 12:38:00 +0100

    New default page with all lists at one page

--- lhc_web/modules/lhfront/default.php
+++ lhc_web/modules/lhfront/default.php
@@ -1,6 +1,23 @@
 <?php
 
 $tpl = erLhcoreClassTemplate::getInstance( 'lhfront/default.tpl.php');
+$tpl->set('geo_location_data',erLhcoreClassModelChatConfig::fetch('geo_location_data')->data);
+$tpl->set('tracking_enabled',erLhcoreClassModelChatConfig::fetch('track_online_visitors')->current_value == 1);
+/**
+ * Append user departments filter
+ * */
+$departmentParams = array();
+$userDepartments = erLhcoreClassUserDep::parseUserDepartmetnsForFilter($currentUser->getUserID());
+if ($userDepartments !== true){
+    $departmentParams['filterin']['id'] = $userDepartments;
+    if (!$currentUser->hasAccessTo('lhchat','sees_all_online_visitors')) {
+        $filter['filterin']['dep_id'] = $userDepartments;
+    }
+}
+
+$tpl->set('departmentParams',$departmentParams);
+
 $Result['content'] = $tpl->fetch();
+$Result['additional_footer_js'] = '<script src="'.erLhcoreClassDesign::designJS('js/angular.lhc.online.js').'"></script>';
 
 ?>
