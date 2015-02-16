1e74baab0dd3449bc502ecc927e39d5aa2c55ac6
Remdex <remdex@gmail.com>
2015-02-16 20:05:24 +0100

    Adjust nav bar style

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/top_menu.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/top_menu.tpl.php
@@ -1,5 +1,5 @@
 <?php $currentUser = erLhcoreClassUser::instance(); ?>
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default navbar-lhc">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
