0ce0ab34f402058c76ee27c9b108974ba11b33af
Remdex <remdex@gmail.com>
2015-02-16 18:00:02 +0100

    Use icons for account menu

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/user_box.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/user_box.tpl.php
@@ -4,8 +4,8 @@ $UserData = $currentUser->getUserData(true); ?>
 <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo htmlspecialchars($UserData->name),' ',htmlspecialchars($UserData->surname)?> <span class="caret"></span></a>
     <ul class="dropdown-menu" role="menu">
-        <li><a href="<?php echo erLhcoreClassDesign::baseurl('user/account')?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Account')?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Account')?></a></li>
-        <li><a href="<?php echo erLhcoreClassDesign::baseurl('user/logout')?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Logout');?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Logout');?> <i class="icon-logout"></i></a></li>
+        <li><a href="<?php echo erLhcoreClassDesign::baseurl('user/account')?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Account')?>"><i class="glyphicon glyphicon-user"></i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Account')?></a></li>
+        <li><a href="<?php echo erLhcoreClassDesign::baseurl('user/logout')?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Logout');?>"><i class="glyphicon glyphicon-log-out"></i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Logout');?></a></li>
     </ul>
 </li>
  <?php unset($currentUser);unset($UserData);?>
