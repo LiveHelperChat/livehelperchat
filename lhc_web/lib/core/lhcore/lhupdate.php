2d9949c478f5fbc0f684f78c8ce8f2133f59c08e
Remdex <remdex@gmail.com>
2015-02-19 09:06:53 +0100

    Increase version

--- lhc_web/lib/core/lhcore/lhupdate.php
+++ lhc_web/lib/core/lhcore/lhupdate.php
@@ -2,8 +2,8 @@
 
 class erLhcoreClassUpdate
 {
-	const DB_VERSION = 100;
-	const LHC_RELEASE = 223;
+	const DB_VERSION = 101;
+	const LHC_RELEASE = 224;
 		
 	public static function doTablesUpdate($definition){
 		$updateInformation = self::getTablesStatus($definition);
