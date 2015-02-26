7306c51b1ec5f8b97b2a6ce0e7bbe044ed44901b
Remdex <remdex@gmail.com>
2015-02-26 21:25:19 +0100

    fix #639

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/page_footer_user.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/page_footer_user.tpl.php
@@ -2,7 +2,7 @@
     <div class="row mt10 footer-row">
         <div class="columns twelve">
             <p class="pull-right"><a target="_blank" href="http://livehelperchat.com">Live Helper Chat &copy; <?php echo date('Y')?></a></p>
-            <p><a href="<?php echo erLhcoreClassModelChatConfig::fetch('customer_site_url')->current_value?>" target="_blank"><?php echo erLhcoreClassModelChatConfig::fetch('customer_company_name')->current_value?></a></p>
+            <p><a href="<?php if (isset($Result['theme']) !== false && $Result['theme']->widget_copyright_url != '') : ?><?php echo htmlspecialchars($Result['theme']->widget_copyright_url) ?><?php else : ?><?php echo erLhcoreClassModelChatConfig::fetch('customer_site_url')->current_value?><?php endif;?>" target="_blank"><?php if (isset($Result['theme'])) : ?><?php echo htmlspecialchars($Result['theme']->name)?><?php else : ?><?php echo htmlspecialchars(erLhcoreClassModelChatConfig::fetch('customer_company_name')->current_value)?><?php endif;?></a></p>
         </div>
     </div>
 </div>
