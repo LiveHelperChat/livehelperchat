de6112b75c7381835d6c71338f5620c5e223d97b
Remdex <remdex@gmail.com>
2015-03-28 21:58:25 +0100

    fix #661

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/dynamic_height.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/dynamic_height.tpl.php
@@ -6,8 +6,12 @@ if (isset($Result['dynamic_height'])) : ?>
 <script>
 $( window ).load(function() {
 	if (window.opener && $('#user-popup-window').size() > 0) {
-		var windowHeight = $('#user-popup-window').height()+90<?php if (isset($Result['dynamic_height_adjust'])) {echo $Result['dynamic_height_adjust'];}?>;		  
-		window.resizeBy(0, windowHeight - $( window ).height());
+		var windowHeight = $('#user-popup-window').height()+90<?php if (isset($Result['dynamic_height_adjust'])) {echo $Result['dynamic_height_adjust'];}?>;	
+
+		// Don't do anything like popup is bigger than our screen		
+		if (screen.availHeight > (windowHeight+90)){		
+			  window.resizeBy(0, windowHeight - $( window ).height());
+		}
 	}
 });
 </script>
