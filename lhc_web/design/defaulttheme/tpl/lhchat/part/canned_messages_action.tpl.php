fee78d778cf8b63ee7d098fd0cecb9d9a8e3b766
Remdex <remdex@gmail.com>
2015-04-17 13:35:10 +0200

    Allow extension to append replecable variables

--- lhc_web/design/defaulttheme/tpl/lhchat/part/canned_messages_action.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/canned_messages_action.tpl.php
@@ -6,8 +6,18 @@
         	<option value=""><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Select a canned message')?></option>
         <?php 
         $nameSupport = (string)erLhcoreClassUser::instance()->getUserData(true)->name_support;
+        
+        $replaceArray = array(
+            '{nick}' => $chat->nick, 
+            '{email}' => $chat->email, 
+            '{phone}' => $chat->phone, 
+            '{operator}' => $nameSupport            
+        );
+        
+        erLhcoreClassChatEventDispatcher::getInstance()->dispatch('chat.workflow.canned_message_replace',array('replace_array' => & $replaceArray));
+        
         foreach (erLhcoreClassModelCannedMsg::getCannedMessages($chat->dep_id,erLhcoreClassUser::instance()->getUserID()) as $item) :         
-        $item->setReplaceData(array('{nick}' => $chat->nick, '{operator}' => $nameSupport)); ?>
+        $item->setReplaceData($replaceArray); ?>
         	<option data-msg="<?php echo htmlspecialchars($item->msg_to_user)?>" data-delay="<?php echo $item->delay?>" value="<?php echo $item->id?>"><?php echo htmlspecialchars($item->message_title)?></option>
         <?php endforeach;?>
         </select>
