de8b4a2bc1d5f6ded86dfb68e03b0735430013bb
Remdex <remdex@gmail.com>
2015-05-14 20:37:16 +0200

    Split apart

---
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/after_text_area_block.tpl.php
@@ -0,0 +1,5 @@
+<div class="form-group">
+<?php include(erLhcoreClassDesign::designtpl('lhchat/part/action_block.tpl.php')); ?>
+
+<?php include(erLhcoreClassDesign::designtpl('lhchat/part/below_action_block.tpl.php')); ?>
+</div>
