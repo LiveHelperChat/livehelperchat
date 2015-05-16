ba20a76dc467181e50f5eed628bff6aca1ec8588
Remdex <remdex@gmail.com>
2015-05-16 19:29:28 +0200

    New interface for messages

--- lhc_web/design/defaulttheme/tpl/lhchat/lists/msg_obj_list_admin.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/lists/msg_obj_list_admin.tpl.php
@@ -1,7 +1,7 @@
 <?php foreach ($messages as $msg) : ?>
 <?php if ($msg->user_id == -1) : ?>
-	<div class="message-row system-response" id="msg-<?php echo $msg->id?>"><div class="msg-date"><?php echo date(erLhcoreClassModule::$dateDateHourFormat,$msg->time);?></div><i><span class="usr-tit"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmin','System assistant')?>:&nbsp;</span><?php echo erLhcoreClassBBCode::make_clickable(htmlspecialchars($msg->msg))?></i></div>
+	<div class="message-row system-response" id="msg-<?php echo $msg->id?>"><div class="msg-date"><?php echo date(erLhcoreClassModule::$dateDateHourFormat,$msg->time);?></div><i><span class="usr-tit sys-tit"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmin','System assistant')?></span><?php echo erLhcoreClassBBCode::make_clickable(htmlspecialchars($msg->msg))?></i></div>
 <?php else : ?>
-	<div class="message-row<?php echo $msg->user_id == 0 ? ' response' : ''?>" id="msg-<?php echo $msg->id?>"><div class="msg-date"><?php echo date(erLhcoreClassModule::$dateDateHourFormat,$msg->time);?></div><span class="usr-tit"><?php echo $msg->user_id == 0 ? htmlspecialchars($chat->nick) : htmlspecialchars($msg->name_support) ?>:&nbsp;</span><?php echo erLhcoreClassBBCode::make_clickable(htmlspecialchars($msg->msg))?></div>
+	<div class="message-row<?php echo $msg->user_id == 0 ? ' response' : ' message-admin'?>" id="msg-<?php echo $msg->id?>"><div class="msg-date"><?php echo date(erLhcoreClassModule::$dateDateHourFormat,$msg->time);?></div><span class="usr-tit<?php echo $msg->user_id == 0 ? ' vis-tit' : ' op-tit'?>"><?php echo $msg->user_id == 0 ? htmlspecialchars($chat->nick) : htmlspecialchars($msg->name_support) ?></span><?php echo erLhcoreClassBBCode::make_clickable(htmlspecialchars($msg->msg))?></div>
 <?php endif;?>
 <?php endforeach;?>
--- lhc_web/design/defaulttheme/tpl/lhchat/userjoined.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/userjoined.tpl.php
@@ -1,1 +1,1 @@
-<div><strong><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userjoined','User has joined the chat!')?></strong></div> 
+<div class="message-row system-response"><i><span class="usr-tit sys-tit"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmin','System assistant')?></span><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userjoined','User has joined the chat!')?></i></div>
