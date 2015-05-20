bd091bf31999564005246d1bfba0305c794811ff
Remdex <remdex@gmail.com>
2015-05-20 21:50:02 +0200

    UX

--- lhc_web/design/defaulttheme/tpl/lhchat/previewchat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/previewchat.tpl.php
@@ -1,16 +1,18 @@
-<?php include(erLhcoreClassDesign::designtpl('lhkernel/modal_header.tpl.php'));?>
-
-<h4>
-	<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Chat owner')?> <?php $user = $chat->getChatOwner();  if ($user !== false) : ?>
+<div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"><span class="glyphicon glyphicon-info-sign"></span>&nbsp;<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Chat owner')?> <?php $user = $chat->getChatOwner();  if ($user !== false) : ?>
 	<?php echo htmlspecialchars($user->name)?>&nbsp;<?php echo htmlspecialchars($user->surname)?>
 	<?php else : ?>
 	-
-<?php endif; ?>
-</h4>
-
+<?php endif; ?></h4>
+      </div>
+      <div class="modal-body">
+    
 		
-<div class="p10 wb border-grey fs12">
-<h5><strong><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/previewchat','Last 100 messages rows');?></strong></h5>
+<div class="fs12">
+<h6><strong><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/previewchat','Last 100 messages rows');?></strong></h6>
 <?php $messages = array_reverse(erLhcoreClassModelmsg::getList(array('limit' => 100,'sort' => 'id DESC','filter' => array('chat_id' => $chat->id)))); ?>
 <?php include(erLhcoreClassDesign::designtpl('lhchat/lists/msg_obj_list_admin.tpl.php'));?>
 </div>
