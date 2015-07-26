15d0a81d416ce5fdb0adf437955566329a2e4c91
Remdex <remdex@gmail.com>
2015-07-26 19:34:19 +0200

    UX

--- lhc_web/design/defaulttheme/tpl/lhchat/chat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat.tpl.php
@@ -70,7 +70,7 @@
 		  	    	    	
 	    </div>
        
-        <textarea class="form-control" rows="4" name="ChatMessage" placeholder="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Enter your message')?>" id="CSChatMessage" class="live-chat-message"></textarea>
+        <textarea class="form-control live-chat-message" rows="4" name="ChatMessage" placeholder="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Enter your message')?>" id="CSChatMessage" ></textarea>
 	    
         <script type="text/javascript">        
         jQuery('#CSChatMessage').bind('keydown', 'return', function (evt){
