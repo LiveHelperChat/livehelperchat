ee218d167b058e8a020faddc91c55394ff82c7a4
Remdex <remdex@gmail.com>
2015-08-07 23:14:45 +0200

    Fix scoring

--- lhc_web/design/defaulttheme/tpl/lhsurvey/forms/fill.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsurvey/forms/fill.tpl.php
@@ -2,7 +2,7 @@
     <?php if ($survey->max_stars > 0) : ?>        
         <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Please choose stars evaluation')?></label>
         <?php for ($i = 0; $i < $survey->max_stars; $i++) : ?>
-            <div><label><input type="radio" name="StarsValue" value="<?php echo $i?>"> <?php echo $i+1?> <?php if ($i == 0) : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','star')?><?php else : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','stars')?><?php endif;?></label></div>
+            <div><label><input type="radio" name="StarsValue" value="<?php echo $i+1?>"> <?php echo $i+1?> <?php if ($i == 0) : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','star')?><?php else : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','stars')?><?php endif;?></label></div>
         <?php endfor;?>
     <?php endif;?>
 <?php else : ?>
