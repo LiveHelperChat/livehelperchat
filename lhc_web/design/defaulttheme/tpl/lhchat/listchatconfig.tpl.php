a589230bd1a0b8819bebf2b8ee1adc50c52202f7
Remdex <remdex@gmail.com>
2015-08-09 18:48:44 +0200

    fix #735

--- lhc_web/design/defaulttheme/tpl/lhchat/listchatconfig.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/listchatconfig.tpl.php
@@ -169,7 +169,8 @@
     		    <?php $attribute = 'hide_disabled_department';$boolValue = true;?>
     		    <?php include(erLhcoreClassDesign::designtpl('lhchat/part/chat_settings.tpl.php'));?>
     		    
-    		    <?php $attribute = 'front_tabs'; ?>
+    		    <?php $attribute = 'front_tabs'; 
+    		    $configExplain = erTranslationClassLhTranslation::getInstance()->getTranslation('system/configuration','Supported: dashboard,online_map,online_users,pending_chats,online_map,active_chats,unread_chats,closed_chats,online_operators');?>
     		    <?php include(erLhcoreClassDesign::designtpl('lhchat/part/chat_settings.tpl.php'));?>
     		    
     		    <?php $attribute = 'hide_right_column_frontpage';$boolValue = true;?>
