44f249e73a57e18e5cf183958a265ae970d80893
Remdex <remdex@gmail.com>
2015-08-10 17:40:34 +0200

    Missed event

--- lhc_web/design/defaulttheme/tpl/lhsurvey/fill.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsurvey/fill.tpl.php
@@ -12,6 +12,6 @@
         <?php if ($survey_item->is_filled == false) : ?>
             <input type="submit" class="btn btn-success btn-sm" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/buttons','Save')?>" name="Vote" />
         <?php endif;?>
-            <a class="btn btn-info btn-sm"><i class="material-icons">chat</i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Preview chat')?></a>
+            <a class="btn btn-info btn-sm" onclick="lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'chat/chatpreview/<?php echo $chat->id?>/<?php echo $chat->hash?>'})"><i class="material-icons">chat</i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Preview chat')?></a>
      </div>
 </form>
