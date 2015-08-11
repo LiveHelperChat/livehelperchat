6ec09e2f4e17afa56754ebf86fd42bc40fdfc09e
Remdex <remdex@gmail.com>
2015-08-11 21:48:40 +0200

    fix #740

--- lhc_web/design/defaulttheme/tpl/lhsurvey/forms/fill.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsurvey/forms/fill.tpl.php
@@ -2,7 +2,11 @@
     <?php if ($survey->max_stars > 0) : ?>        
         <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','How well did we do?')?></label>
         <?php for ($i = 0; $i < $survey->max_stars; $i++) : ?>
-            <div><label><input type="radio" name="StarsValue" value="<?php echo $i+1?>"> <?php echo $i+1?> <?php if ($i == 0) : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','star')?> - <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Poor')?><?php else : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','stars')?><?php if ($i == $survey->max_stars - 1) : ?> - <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Excellent')?><?php endif;endif;?></label></div>
+        <div class="radio radio-widget">
+          <label>
+            <input type="radio" name="StarsValue" value="<?php echo $i+1?>"><?php echo $i+1?>&nbsp;<?php if ($i == 0) : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','star')?> - <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Poor')?><?php else : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','stars')?><?php if ($i == $survey->max_stars - 1) : ?> - <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Excellent')?><?php endif;endif;?>
+          </label>
+        </div>
         <?php endfor;?>
     <?php endif;?>
 <?php else : ?>
