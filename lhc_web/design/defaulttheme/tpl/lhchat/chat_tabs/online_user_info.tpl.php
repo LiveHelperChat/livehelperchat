8052068b848e738b1c5acdebf1040212f8f10a8d
Remdex <remdex@gmail.com>
2015-09-13 09:27:05 +0200

    Show previous chats

--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/online_user_info.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/online_user_info.tpl.php
@@ -17,8 +17,8 @@
 <?php if ($information_tab_online_user_info_chats_enabled == true) : ?>
 <div role="tabpanel" class="tab-pane" id="online-user-info-chats-tab-<?php echo $chat->id?>">
   <ul class="foot-print-content list-unstyled" style="max-height: 170px;">
-	<?php foreach (erLhcoreClassChat::getList(array('limit' => 100, 'filter' => array('online_user_id' => $online_user->id))) as $chatPrev) : ?>
-		<?php if (!isset($chat) || $chat->id != $chatPrev->id) : ?>
+	<?php $hasPrevChat = false; foreach (erLhcoreClassChat::getList(array('limit' => 100, 'filter' => array('online_user_id' => $online_user->id))) as $chatPrev) : ?>
+		<?php if (!isset($chat) || $chat->id != $chatPrev->id) : $hasPrevChat = true;?>
 			<li>
 			  <?php if ( !empty($chatPrev->country_code) ) : ?><img src="<?php echo erLhcoreClassDesign::design('images/flags');?>/<?php echo $chatPrev->country_code?>.png" alt="<?php echo htmlspecialchars($chatPrev->country_name)?>" title="<?php echo htmlspecialchars($chatPrev->country_name)?>" />&nbsp;<?php endif; ?>
 		      <a title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Open in a new window');?>" class="material-icons" onclick="lhinst.startChatNewWindow('<?php echo $chatPrev->id;?>',$(this).attr('data-title'))" data-title="<?php echo htmlspecialchars($chatPrev->nick,ENT_QUOTES);?>">open_in_new</a><?php echo $chatPrev->id;?>. <?php echo htmlspecialchars($chatPrev->nick);?> (<?php echo date(erLhcoreClassModule::$dateDateHourFormat,$chatPrev->time);?>) (<?php echo htmlspecialchars($chatPrev->department);?>)
@@ -26,6 +26,11 @@
 		<?php endif; ?>
 	<?php endforeach;?>
 	</ul>
+	
+	<?php if ($hasPrevChat == false) : ?> 
+	   <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','No previous chats');?>
+	<?php endif;?>
+	
 </div>
 <?php endif; ?>
 
