6aa6f6b207c7cd82bcdac34dbd88dae6c33c65b2
Remdex <remdex@gmail.com>
2015-09-13 20:44:37 +0200

    fix typo

--- lhc_web/lib/models/lhabstract/erlhabstractmodelautoresponder.php
+++ lhc_web/lib/models/lhabstract/erlhabstractmodelautoresponder.php
@@ -85,7 +85,7 @@ class erLhAbstractModelAutoResponder {
    						)),
    				'timeout_message' => array(
    						'type' => 'text',
-   						'trans' => erTranslationClassLhTranslation::getInstance()->getTranslation('abstract/proactivechatinvitation','Show visitor this message then wait timeout passes.'),
+   						'trans' => erTranslationClassLhTranslation::getInstance()->getTranslation('abstract/proactivechatinvitation','Show visitor this message when wait timeout passes'),
    						'required' => true,
    						'hidden' => true,
    						'validation_definition' => new ezcInputFormDefinitionElement(
