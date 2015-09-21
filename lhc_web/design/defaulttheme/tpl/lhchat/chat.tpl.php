3573d2828fe9e2d6e8150c5ba7b54e9dd781b6a3
Remdex <remdex@gmail.com>
2015-09-21 12:19:11 +0200

    Split template

--- lhc_web/design/defaulttheme/tpl/lhchat/chat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat.tpl.php
@@ -53,24 +53,8 @@
  
     <?php if ($chat->status != erLhcoreClassModelChat::STATUS_CLOSED_CHAT) : ?>
     <div id="ChatMessageContainer">    
-		<div class="user-chatwidget-buttons" id="ChatSendButtonContainer">     
-		
-		
-		   	<div class="btn-group" role="group" aria-label="...">
-		   	    <a href="#" class="btn btn-default btn-xs trigger-button"><i class="material-icons fs14 mr-0">mode_edit</i></a>
-	    	
-	    		<input type="button" class="btn btn-default btn-xs sendbutton invisible-button" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Send')?>" onclick="lhinst.addmsguser()" />
-	    	
-	    		<?php if (erLhcoreClassModelChatConfig::fetch('bbc_button_visible')->current_value == 1) : ?>
-	    		<input type="button" class="btn btn-default btn-xs invisible-button" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','BB Code')?>" onclick="return lhc.revealModal({'url':'<?php echo erLhcoreClassDesign::baseurl('chat/bbcodeinsert')?>'})" />
-	    		<?php endif; ?>
-	    		
-	    		<?php if ( (isset($chat_embed_mode) && $chat_embed_mode == true) || (isset($chat_embed_mode) && $chat_embed_mode == true)) : ?>
-	    		<input type="button" class="btn btn-default btn-xs invisible-button" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Close')?>" onclick="lhinst.userclosedchatembed();" />
-	    		<?php endif;?>
-		   	</div>
-		  	    	    	
-	    </div>
+	    
+	    <?php include(erLhcoreClassDesign::designtpl('lhchat/part/above_text_area_user.tpl.php'));?>	
        
         <textarea class="form-control live-chat-message" rows="4" name="ChatMessage" placeholder="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Enter your message')?>" id="CSChatMessage" ></textarea>
 	    
