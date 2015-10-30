158de8c14a2dcfe16b37bc90c7f3992e5d3cb929
Damian Ivereigh <damo@launtel.net.au>
2015-10-30 15:42:28 +1100

    If operator does not have access to view online visitors, then there is no point in showing them either the tab or the option for listing the online vistors or the map

--- lhc_web/design/defaulttheme/tpl/lhchat/onlineusers/section_online_users_tab.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/onlineusers/section_online_users_tab.tpl.php
@@ -1,4 +1,4 @@
 <?php include(erLhcoreClassDesign::designtpl('lhchat/onlineusers/section_online_users_tab_pre.tpl.php')); ?>
-<?php if ($chat_onlineusers_section_online_users_tab_enabled == true) : ?>
+<?php if ($chat_onlineusers_section_online_users_tab_enabled == true && $currentUser->hasAccessTo('lhchat', 'use_onlineusers') == true) : ?>
 <li role="presentation"><a href="#onlineusers" aria-controls="onlineusers" role="tab" data-toggle="tab" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/onlineusers','Online visitors list');?>"><i class="material-icons mr-0">face</i></a></li>
 <?php endif;?>
