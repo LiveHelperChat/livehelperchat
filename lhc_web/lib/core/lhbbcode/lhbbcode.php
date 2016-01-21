8e2d0104669f1bc53f6a703708bf8bbe1e16b5ee
Remdex <remdex@gmail.com>
2016-01-21 20:34:20 +0100

    Enable back bbcode

--- lhc_web/lib/core/lhbbcode/lhbbcode.php
+++ lhc_web/lib/core/lhbbcode/lhbbcode.php
@@ -306,13 +306,12 @@ class erLhcoreClassBBCode
    public static function make_clickable($ret) {
        $ret = ' ' . $ret;
         // in testing, using arrays here was found to be faster
-        $ret = preg_replace_callback('#(?<!=[\'"])(?<=[*\')+.,;:!&$\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#%~/?@\[\]-]|[\'*(+.,;:!=&$](?![\b\)]|(\))?([\s]|$))|(?(1)\)(?![\s<.,;:]|$)|\)
-))+)#is', 'erLhcoreClassBBCode::_make_url_clickable_cb', $ret);
+        $ret = preg_replace_callback('#(?<!=[\'"])(?<=[*\')+.,;:!&$\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#%~/?@\[\]-]|[\'*(+.,;:!=&$](?![\b\)]|(\))?([\s]|$))|(?(1)\)(?![\s<.,;:]|$)|\)))+)#is', 'erLhcoreClassBBCode::_make_url_clickable_cb', $ret);
         $ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]+)#is', 'erLhcoreClassBBCode::_make_web_ftp_clickable_cb', $ret);
         $ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'erLhcoreClassBBCode::_make_email_clickable_cb', $ret);
         // this one is not in an array because we need it to run last, for cleanup of accidental links within links
         $ret = preg_replace("#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret);
-        return $ret;
+        
 
     	$ret = self::BBCode2Html($ret);
 
