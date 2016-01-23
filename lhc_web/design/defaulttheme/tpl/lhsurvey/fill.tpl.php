69d535e7424cf8420971fcbd2cd57aaf370d17c5
Remdex <remdex@gmail.com>
2016-01-23 20:09:52 +0100

    Survey classes css

--- lhc_web/design/defaulttheme/tpl/lhsurvey/fill.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsurvey/fill.tpl.php
@@ -1,17 +1,19 @@
-
-<?php include(erLhcoreClassDesign::designtpl('lhsurvey/forms/parts/header.tpl.php'));?>
-
-<form action="" method="post">
-    <?php if (isset($errors)) : ?>
-    		<?php include(erLhcoreClassDesign::designtpl('lhkernel/validation_error.tpl.php'));?>
-    <?php endif; ?>
-    
-    <?php include(erLhcoreClassDesign::designtpl('lhsurvey/forms/fill.tpl.php'));?>
-    
-    <div class="btn-group" role="group" aria-label="...">
-        <?php if ($survey_item->is_filled == false) : ?>
-            <input type="submit" class="btn btn-success btn-sm" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/buttons','Save')?>" name="Vote" />
-        <?php endif;?>
-            <a class="btn btn-info btn-sm" onclick="lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'chat/chatpreview/<?php echo $chat->id?>/<?php echo $chat->hash?>'})"><i class="material-icons">chat</i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Preview chat')?></a>
-     </div>
-</form>
+<div class="fill-survey-container">
+    <?php include(erLhcoreClassDesign::designtpl('lhsurvey/forms/parts/header.tpl.php'));?>
+    <div class="fill-survey-form">
+        <form action="" method="post">
+            <?php if (isset($errors)) : ?>
+            		<?php include(erLhcoreClassDesign::designtpl('lhkernel/validation_error.tpl.php'));?>
+            <?php endif; ?>
+            
+            <?php include(erLhcoreClassDesign::designtpl('lhsurvey/forms/fill.tpl.php'));?>
+            
+            <div class="btn-group" role="group" aria-label="...">
+                <?php if ($survey_item->is_filled == false) : ?>
+                    <input type="submit" class="btn btn-success btn-sm" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/buttons','Save')?>" name="Vote" />
+                <?php endif;?>
+                    <a class="btn btn-info btn-sm" onclick="lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'chat/chatpreview/<?php echo $chat->id?>/<?php echo $chat->hash?>'})"><i class="material-icons">chat</i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Preview chat')?></a>
+             </div>
+        </form>
+    </div>
+</div>
--- lhc_web/design/defaulttheme/tpl/lhsurvey/fillwidget.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsurvey/fillwidget.tpl.php
@@ -1,29 +1,28 @@
-<?php include(erLhcoreClassDesign::designtpl('lhsurvey/forms/parts/header.tpl.php'));?>
-
-<form action="" method="post">
-    <?php if (isset($errors)) : ?>
-    		<?php include(erLhcoreClassDesign::designtpl('lhkernel/validation_error.tpl.php'));?>
-    <?php endif; ?>
-    
-    <?php include(erLhcoreClassDesign::designtpl('lhsurvey/forms/fill.tpl.php'));?>
-    
-    <hr class="mt10 mb10">
-    
-    
-    <div class="btn-group" role="group" aria-label="...">
-        <?php if ($survey_item->is_filled == false) : ?>
-            <input type="submit" class="btn btn-success btn-sm" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/buttons','Save')?>" name="Vote" />
-        <?php endif;?>
-        <a class="btn btn-info btn-sm" onclick="lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'chat/chatpreview/<?php echo $chat->id?>/<?php echo $chat->hash?>'})"><i class="material-icons">chat</i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Preview chat')?></a>
+<div class="fill-survey-container">
+    <?php include(erLhcoreClassDesign::designtpl('lhsurvey/forms/parts/header.tpl.php'));?>
+    <div class="fill-survey-form">
+        <form action="" method="post">
+            <?php if (isset($errors)) : ?>
+            		<?php include(erLhcoreClassDesign::designtpl('lhkernel/validation_error.tpl.php'));?>
+            <?php endif; ?>
+            
+            <?php include(erLhcoreClassDesign::designtpl('lhsurvey/forms/fill.tpl.php'));?>
+            
+            <hr class="mt10 mb10">
+            
+            <div class="btn-group" role="group" aria-label="...">
+                <?php if ($survey_item->is_filled == false) : ?>
+                    <input type="submit" class="btn btn-success btn-sm" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/buttons','Save')?>" name="Vote" />
+                <?php endif;?>
+                <a class="btn btn-info btn-sm" onclick="lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'chat/chatpreview/<?php echo $chat->id?>/<?php echo $chat->hash?>'})"><i class="material-icons">chat</i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Preview chat')?></a>
+            </div>
+            
+            <?php if ($survey_item->is_filled == true) : ?>
+                 <input type="button" class="btn btn-success mb10 pull-right" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Close')?>" onclick="lhinst.userclosedchatembed();" />
+            <?php endif;?>
+        </form>
     </div>
-    
-    <?php if ($survey_item->is_filled == true) : ?>
-         <input type="button" class="btn btn-success mb10 pull-right" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Close')?>" onclick="lhinst.userclosedchatembed();" />
-    <?php endif;?>
-    
-</form>
-
-
+</div>
 <script type="text/javascript">
 lhinst.setChatID('<?php echo $chat->id?>');
 lhinst.setChatHash('<?php echo $chat->hash?>');
