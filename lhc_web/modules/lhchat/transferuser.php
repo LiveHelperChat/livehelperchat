b6de4dfdc313d85ecb0f15446bbda4dd77642e2b
Donatas <donatas.veikutis@gmail.com>
2016-07-19 11:37:03 +0300

    tarasferuser.php updated

--- lhc_web/modules/lhchat/transferuser.php
+++ lhc_web/modules/lhchat/transferuser.php
@@ -3,9 +3,12 @@
 
 if (is_numeric( $Params['user_parameters']['chat_id']) && is_numeric($Params['user_parameters']['item_id']))
 {
-
 	$Chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params['user_parameters']['chat_id']);
-	if ( erLhcoreClassChat::hasAccessToRead($Chat) )
+	$errors = [];
+
+	erLhcoreClassChatEventDispatcher::getInstance()->dispatch('chat.before_chat_transfered', ['chat' => & $Chat, 'errors' => & $errors]);
+
+	if ( erLhcoreClassChat::hasAccessToRead($Chat) && empty($errors) )
 	{
 		$currentUser = erLhcoreClassUser::instance();
 
@@ -41,10 +44,11 @@ if (is_numeric( $Params['user_parameters']['chat_id']) && is_numeric($Params['us
 	    	$tpl->set('msg',erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferuser','Chat was assigned to selected user'));
 	    }
 
-	    erLhcoreClassChatEventDispatcher::getInstance()->dispatch('chat.chat_transfered',array('chat' => & $Chat));
-	    
-	    echo json_encode(array('error' => 'false', 'result' => $tpl->fetch(), 'chat_id' => $Params['user_parameters']['chat_id']));
-	    exit;
+	    echo json_encode(['error' => 'false', 'result' => $tpl->fetch(), 'chat_id' => $Params['user_parameters']['chat_id']]);;
+	} elseif (!empty($errors)) {
+		$tpl = erLhcoreClassTemplate::getInstance('lhkernel/validation_error.tpl.php');
+		$tpl->set('errors', $errors);
+		echo json_encode(['error' => 'true', 'result' => $tpl->fetch(), 'chat_id' => $Params['user_parameters']['chat_id']]);
 	}
 }
 exit;
