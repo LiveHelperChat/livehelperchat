c2cd14de88e19c38344d0d318ac7f06c9b849161
Donatas <donatas.veikutis@gmail.com>
2016-07-20 09:30:54 +0300

    lhfile/new added event_before dispach

--- lhc_web/modules/lhfile/new.php
+++ lhc_web/modules/lhfile/new.php
@@ -3,27 +3,32 @@
 $tpl = erLhcoreClassTemplate::getInstance( 'lhfile/new.tpl.php');
 
 if (isset($_POST['UploadFileAction'])) {
-	
-	$fileData = erLhcoreClassModelChatConfig::fetch('file_configuration');
-	$data = (array)$fileData->data;
-	
-	try {
-		
-		$path = 'var/storage/'.date('Y').'y/'.date('m').'/'.date('d').'/au/';
-		
-		erLhcoreClassChatEventDispatcher::getInstance()->dispatch('file.new.file_path',array('path' => & $path));
-		
-		$upload_handler = new erLhcoreClassFileUploadAdmin(array('user_id' => $currentUser->getUserID(), 'file_name_manual' => $_POST['Name'], 'upload_dir' => $path, 'download_via_php' => true,'max_file_size' => $data['fs_max']*1024, 'accept_file_types_lhc' => '/\.('.$data['ft_op'].')$/i'));
-		
-		if ($upload_handler->uploadedFile instanceof erLhcoreClassModelChatFile)
-		{
-			erLhcoreClassChatEventDispatcher::getInstance()->dispatch('file.file_new_admin.file_store', array('chat_file' => $upload_handler->uploadedFile));
-		}
-		
-		$tpl->set('file_uploaded',true);
-	} catch (Exception $e) {		
-		$tpl->set('errors',array($e->getMessage()));		
-	}	
+
+    $errors = [];
+    erLhcoreClassChatEventDispatcher::getInstance()->dispatch('file.before_file_new_admin.file_store', array('errors' => & $errors));
+
+    if (empty($errors)) {
+        $fileData = erLhcoreClassModelChatConfig::fetch('file_configuration');
+        $data = (array)$fileData->data;
+
+        try {
+            $path = 'var/storage/' . date('Y') . 'y/' . date('m') . '/' . date('d') . '/au/';
+
+            erLhcoreClassChatEventDispatcher::getInstance()->dispatch('file.new.file_path', array('path' => & $path));
+
+            $upload_handler = new erLhcoreClassFileUploadAdmin(array('user_id' => $currentUser->getUserID(), 'file_name_manual' => $_POST['Name'], 'upload_dir' => $path, 'download_via_php' => true, 'max_file_size' => $data['fs_max'] * 1024, 'accept_file_types_lhc' => '/\.(' . $data['ft_op'] . ')$/i'));
+
+            if ($upload_handler->uploadedFile instanceof erLhcoreClassModelChatFile) {
+                erLhcoreClassChatEventDispatcher::getInstance()->dispatch('file.file_new_admin.file_store', array('chat_file' => $upload_handler->uploadedFile));
+            }
+
+            $tpl->set('file_uploaded', true);
+        } catch (Exception $e) {
+            $tpl->set('errors', array($e->getMessage()));
+        }
+    } else {
+        $tpl->set('errors', $errors);
+    }
 }
 
 $tpl->set('mode',$Params['user_parameters_unordered']['mode']);
