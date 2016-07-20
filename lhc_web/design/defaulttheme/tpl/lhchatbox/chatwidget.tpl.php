c5bc14862b95611afd873f927d1c3c7e23b8664d
Donatas <doncaz111@gmail.com>
2016-07-20 14:36:34 +0300

    lhchatbox/chatwidget added before chat created event dispacher, added error display to chatboxwidget template

--- lhc_web/design/defaulttheme/tpl/lhchatbox/chatwidget.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchatbox/chatwidget.tpl.php
@@ -1,69 +1,75 @@
-<?php if ($chatbox !== false) : ?>
-<div class="row form-group">
-	<div class="col-xs-10">
-		<span><b><?php echo htmlspecialchars($chatbox->name)?></b></span>
-	</div>
-	<div class="col-xs-2">
-		<?php include(erLhcoreClassDesign::designtpl('lhchat/customer_user_settings.tpl.php'));?>
-	</div>
-</div>
+<?php if (isset($chatbox)) : ?>
+    <?php if ($chatbox !== false) : ?>
+        <div class="row form-group">
+            <div class="col-xs-10">
+                <span><b><?php echo htmlspecialchars($chatbox->name)?></b></span>
+            </div>
+            <div class="col-xs-2">
+                <?php include(erLhcoreClassDesign::designtpl('lhchat/customer_user_settings.tpl.php'));?>
+            </div>
+        </div>
 
-<?php if ($chatbox->active == 1) : ?>
+        <?php if ($chatbox->active == 1) : ?>
 
-    <div id="messages" class="form-group">
-        <div class="msgBlock" id="messagesBlock" style="height:<?php isset($chatbox_chat_height) ? print (int)$chatbox_chat_height : print 220?>px"><?php
-        $lastMessageID = 0;
-        $messages = erLhcoreClassChat::getChatMessages($chatbox->chat->id);
-        $chat = $chatbox->chat; ?>
-        <?php include(erLhcoreClassDesign::designtpl('lhchatbox/syncuser.tpl.php'));?>
-        <?php if (isset($msg)) { $lastMessageID = $msg['id'];} ?>
-       </div>
-    </div>
+            <div id="messages" class="form-group">
+                <div class="msgBlock" id="messagesBlock" style="height:<?php isset($chatbox_chat_height) ? print (int)$chatbox_chat_height : print 220?>px"><?php
+                    $lastMessageID = 0;
+                    $messages = erLhcoreClassChat::getChatMessages($chatbox->chat->id);
+                    $chat = $chatbox->chat; ?>
+                    <?php include(erLhcoreClassDesign::designtpl('lhchatbox/syncuser.tpl.php'));?>
+                    <?php if (isset($msg)) { $lastMessageID = $msg['id'];} ?>
+                </div>
+            </div>
 
-    <div>
-	    <?php if (isset($_GET['dnc']) && $_GET['dnc'] == 'true') : ?>
-	    <input type="hidden" class="mt5 mb0" value="<?php echo htmlspecialchars(erLhcoreClassChatbox::getVisitorName(),ENT_QUOTES); ?>" id="CSChatNick" />
-	    <?php else : ?>
-	    <input class="form-control" type="text" class="mt5 mb0" placholder="Nick" title="Nick" value="<?php echo htmlspecialchars(erLhcoreClassChatbox::getVisitorName(),ENT_QUOTES); ?>" id="CSChatNick" />
-	    <?php endif;?>
-	</div>
+            <div>
+                <?php if (isset($_GET['dnc']) && $_GET['dnc'] == 'true') : ?>
+                    <input type="hidden" class="mt5 mb0" value="<?php echo htmlspecialchars(erLhcoreClassChatbox::getVisitorName(),ENT_QUOTES); ?>" id="CSChatNick" />
+                <?php else : ?>
+                    <input class="form-control" type="text" class="mt5 mb0" placholder="Nick" title="Nick" value="<?php echo htmlspecialchars(erLhcoreClassChatbox::getVisitorName(),ENT_QUOTES); ?>" id="CSChatNick" />
+                <?php endif;?>
+            </div>
 
-    <div class="pt5">
-    	<?php if (isset($_GET['dnc']) && $_GET['dnc'] == 'true') : ?>
-    		<span><?php echo htmlspecialchars(erLhcoreClassChatbox::getVisitorName(),ENT_QUOTES); ?></span>
-    	<?php endif;?>
-        <textarea class="form-control" rows="4" name="ChatMessage" placeholder="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Enter your message')?>" id="CSChatMessage"></textarea>
-        <script type="text/javascript">
-        jQuery('#CSChatMessage').bind('keydown', 'return', function (evt){
-            lhinst.addmsguserchatbox();
-            return false;
-        });
-        </script>
-    </div>
+            <div class="pt5">
+                <?php if (isset($_GET['dnc']) && $_GET['dnc'] == 'true') : ?>
+                    <span><?php echo htmlspecialchars(erLhcoreClassChatbox::getVisitorName(),ENT_QUOTES); ?></span>
+                <?php endif;?>
+                <textarea class="form-control" rows="4" name="ChatMessage" placeholder="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Enter your message')?>" id="CSChatMessage"></textarea>
+                <script type="text/javascript">
+                    jQuery('#CSChatMessage').bind('keydown', 'return', function (evt){
+                        lhinst.addmsguserchatbox();
+                        return false;
+                    });
+                </script>
+            </div>
 
-	<div class="pt5">
-    	<input type="button" class="btn btn-default btn-sm" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Send')?>" onclick="lhinst.addmsguserchatbox()" />
-    	<input type="button" class="btn btn-default btn-sm pull-right" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','BB Code')?>" onclick="return lhc.revealModal({'url':'<?php echo erLhcoreClassDesign::baseurl('chat/bbcodeinsert')?>'})" />
-    </div>
+            <div class="pt5">
+                <input type="button" class="btn btn-default btn-sm" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Send')?>" onclick="lhinst.addmsguserchatbox()" />
+                <input type="button" class="btn btn-default btn-sm pull-right" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','BB Code')?>" onclick="return lhc.revealModal({'url':'<?php echo erLhcoreClassDesign::baseurl('chat/bbcodeinsert')?>'})" />
+            </div>
 
-<script type="text/javascript">
-    lhinst.setChatID('<?php echo $chatbox->chat->id?>');
-    lhinst.setChatHash('<?php echo $chatbox->chat->hash?>');
-    lhinst.setLastUserMessageID('<?php echo $lastMessageID;?>');
-    lhinst.setWidgetMode(true);
-    lhinst.setSyncUserURL('chatbox/syncuser/');
-    
-    $( window ).load(function() {
-    	setTimeout(function(){
-    		$('#messagesBlock').scrollTop($('#messagesBlock').prop('scrollHeight'));
-    	},100);
-    });
-	
-    lhinst.scheduleSync();
-</script>
+            <script type="text/javascript">
+                lhinst.setChatID('<?php echo $chatbox->chat->id?>');
+                lhinst.setChatHash('<?php echo $chatbox->chat->hash?>');
+                lhinst.setLastUserMessageID('<?php echo $lastMessageID;?>');
+                lhinst.setWidgetMode(true);
+                lhinst.setSyncUserURL('chatbox/syncuser/');
+
+                $( window ).load(function() {
+                    setTimeout(function(){
+                        $('#messagesBlock').scrollTop($('#messagesBlock').prop('scrollHeight'));
+                    },100);
+                });
+
+                lhinst.scheduleSync();
+            </script>
+        <?php else : ?>
+            <div class="alert alert-danger"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Chatbox is disabled.')?></div>
+        <?php endif;?>
+    <?php else : ?>
+        <div class="alert alert-danger"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Invalid hash or auto creation is disabled')?></div>
+    <?php endif;?>
 <?php else : ?>
-<div class="alert alert-danger"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Chatbox is disabled.')?></div>
-<?php endif;?>
-<?php else : ?>
-<div class="alert alert-danger"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Invalid hash or auto creation is disabled')?></div>
+    <?php if (isset($errors)) : ?>
+        <?php include(erLhcoreClassDesign::designtpl('lhkernel/validation_error.tpl.php'));?>
+    <?php endif; ?>
 <?php endif;?>
--- lhc_web/modules/lhchatbox/chatwidget.php
+++ lhc_web/modules/lhchatbox/chatwidget.php
@@ -8,72 +8,80 @@ $cache = CSCacheAPC::getMem();
 $themeID = isset($Params['user_parameters_unordered']['theme']) && (int)$Params['user_parameters_unordered']['theme'] > 0 ? (int)$Params['user_parameters_unordered']['theme'] : 0;
 
 if ($themeID == 0) {
-	$defaultTheme = erLhcoreClassModelChatConfig::fetch('default_theme_id')->current_value;
-	if ($defaultTheme > 0) {
-		$themeID = $defaultTheme;
-	}
+    $defaultTheme = erLhcoreClassModelChatConfig::fetch('default_theme_id')->current_value;
+    if ($defaultTheme > 0) {
+        $themeID = $defaultTheme;
+    }
 }
 
 $cacheKey = md5('chatbox_version_'.$cache->getCacheVersion('chatbox_'.(string)$Params['user_parameters_unordered']['identifier']).'_theme_'.$themeID.'_identifier_'.(string)$Params['user_parameters_unordered']['identifier'].'_hash_'.(string)$Params['user_parameters_unordered']['hashchatbox'].$visitorName.'_height_'.(int)$Params['user_parameters_unordered']['chat_height'].'_sound_'.(int)$Params['user_parameters_unordered']['sound'].'_mode_'.(string)$Params['user_parameters_unordered']['mode'].'_siteaccess_'.erLhcoreClassSystem::instance()->SiteAccess);
 
 if (($Result = $cache->restore($cacheKey)) === false)
 {
-	$referer = '';
-	$tpl = erLhcoreClassTemplate::getInstance( 'lhchatbox/chatwidget.tpl.php');
-	$tpl->set('chatbox_chat_height',(!is_null($Params['user_parameters_unordered']['chat_height']) && (int)$Params['user_parameters_unordered']['chat_height'] > 0) ? (int)$Params['user_parameters_unordered']['chat_height'] : 220);
-	
-	if ($Params['user_parameters_unordered']['sound'] !== null && is_numeric($Params['user_parameters_unordered']['sound'])) {
-		erLhcoreClassModelUserSetting::setSetting('chat_message',(int)$Params['user_parameters_unordered']['sound'] == 1 ? 1 : 0);
-	}
-	
-	$chatbox = erLhcoreClassChatbox::getInstance((string)$Params['user_parameters_unordered']['identifier'],(string)$Params['user_parameters_unordered']['hashchatbox']);
-	$tpl->set('chatbox',$chatbox);
-	
-	$tpl->set('referer',$referer);
-	if (isset($_GET['URLReferer']))
-	{
-		$referer = $_GET['URLReferer'];
-	    $tpl->set('referer',$referer);
-	}
-	
-	if (isset($_POST['URLRefer']))
-	{
-		$referer = $_POST['URLRefer'];
-	    $tpl->set('referer',$_POST['URLRefer']);
-	}
-	
-	$embedMode = false;
-	$modeAppend = '';
-	if ((string)$Params['user_parameters_unordered']['mode'] == 'embed') {
-		$embedMode = true;
-		$modeAppend = '/(mode)/embed';
-	}
-		
-	if ($themeID > 0){
-		try {
-			$theme = erLhAbstractModelWidgetTheme::fetch($themeID);
-			$Result['theme'] = $theme;
-			$modeAppend .= '/(theme)/'.$theme->id;
-		} catch (Exception $e) {
-	
-		}
-	}
-	
-	$tpl->set('append_mode',$modeAppend);
-	
-	$Result['content'] = $tpl->fetch();
-	$Result['pagelayout'] = 'widget';
-	$Result['pagelayout_css_append'] = 'widget-chat';
-	$Result['dynamic_height'] = true;
-	$Result['dynamic_height_message'] = 'lhc_sizing_chatbox';
-	$Result['additional_post_message'] = 'lhc_chb:nick:'.htmlspecialchars($visitorName,ENT_QUOTES);
-	$Result['is_sync_required'] = true;
-	
-	if ($embedMode == true) {
-		$Result['dynamic_height_message'] = 'lhc_sizing_chatbox_page';
-		$Result['pagelayout_css_append'] = 'embed-widget';
-	}
+    $referer = '';
+    $tpl = erLhcoreClassTemplate::getInstance( 'lhchatbox/chatwidget.tpl.php');
+    $tpl->set('chatbox_chat_height',(!is_null($Params['user_parameters_unordered']['chat_height']) && (int)$Params['user_parameters_unordered']['chat_height'] > 0) ? (int)$Params['user_parameters_unordered']['chat_height'] : 220);
 
-	$cache->store($cacheKey,$Result);
+    if ($Params['user_parameters_unordered']['sound'] !== null && is_numeric($Params['user_parameters_unordered']['sound'])) {
+        erLhcoreClassModelUserSetting::setSetting('chat_message',(int)$Params['user_parameters_unordered']['sound'] == 1 ? 1 : 0);
+    }
+
+    $errors = [];
+
+    erLhcoreClassChatEventDispatcher::getInstance()->dispatch('chatbox.before_created', array('errors' => & $errors));
+
+    if (empty($errors)) {
+        $chatbox = erLhcoreClassChatbox::getInstance((string)$Params['user_parameters_unordered']['identifier'],(string)$Params['user_parameters_unordered']['hashchatbox']);
+        $tpl->set('chatbox',$chatbox);
+    } else {
+        $tpl->set('errors', $errors);
+    }
+
+    $tpl->set('referer',$referer);
+    if (isset($_GET['URLReferer']))
+    {
+        $referer = $_GET['URLReferer'];
+        $tpl->set('referer',$referer);
+    }
+
+    if (isset($_POST['URLRefer']))
+    {
+        $referer = $_POST['URLRefer'];
+        $tpl->set('referer',$_POST['URLRefer']);
+    }
+
+    $embedMode = false;
+    $modeAppend = '';
+    if ((string)$Params['user_parameters_unordered']['mode'] == 'embed') {
+        $embedMode = true;
+        $modeAppend = '/(mode)/embed';
+    }
+
+    if ($themeID > 0){
+        try {
+            $theme = erLhAbstractModelWidgetTheme::fetch($themeID);
+            $Result['theme'] = $theme;
+            $modeAppend .= '/(theme)/'.$theme->id;
+        } catch (Exception $e) {
+
+        }
+    }
+
+    $tpl->set('append_mode',$modeAppend);
+
+    $Result['content'] = $tpl->fetch();
+    $Result['pagelayout'] = 'widget';
+    $Result['pagelayout_css_append'] = 'widget-chat';
+    $Result['dynamic_height'] = true;
+    $Result['dynamic_height_message'] = 'lhc_sizing_chatbox';
+    $Result['additional_post_message'] = 'lhc_chb:nick:'.htmlspecialchars($visitorName,ENT_QUOTES);
+    $Result['is_sync_required'] = true;
+
+    if ($embedMode == true) {
+        $Result['dynamic_height_message'] = 'lhc_sizing_chatbox_page';
+        $Result['pagelayout_css_append'] = 'embed-widget';
+    }
+
+    $cache->store($cacheKey,$Result);
 }
 ?>
