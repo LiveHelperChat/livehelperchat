4cd517f24b2bf1316c3890206c8b25bbf5ac3420
Donatas <doncaz111@gmail.com>
2016-08-04 15:29:51 +0300

    dispacher speech.before_getchatdialect added

--- lhc_web/modules/lhspeech/getchatdialect.php
+++ lhc_web/modules/lhspeech/getchatdialect.php
@@ -3,17 +3,25 @@
 if (is_numeric($Params['user_parameters']['chat_id']))
 {
     $chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params['user_parameters']['chat_id']);
-    
-    if ( erLhcoreClassChat::hasAccessToRead($chat) )
-    {                
-         $chatSpeech = erLhcoreClassSpeech::getSpeechInstance($chat);
-         
-         erLhcoreClassChatEventDispatcher::getInstance()->dispatch('speech.getchatdialect',array('chat' => & $chat));
-         
-         echo json_encode(array('error' => false, 'dialect' => $chatSpeech->dialect));         
+
+    $errors = [];
+    erLhcoreClassChatEventDispatcher::getInstance()->dispatch('speech.before_getchatdialect',array('chat' => & $chat, 'errors' => & $errors));
+
+    if(empty($errors)) {
+        if ( erLhcoreClassChat::hasAccessToRead($chat) )
+        {
+            $chatSpeech = erLhcoreClassSpeech::getSpeechInstance($chat);
+
+            erLhcoreClassChatEventDispatcher::getInstance()->dispatch('speech.getchatdialect',array('chat' => & $chat));
+
+            echo json_encode(array('error' => false, 'dialect' => $chatSpeech->dialect));
+        } else {
+            echo json_encode(array('error' => true, 'result' => 'No permission'));
+        }
     } else {
-         echo json_encode(array('error' => true, 'result' => 'No permission'));
+        echo json_encode(array('error' => true, 'result' => implode(PHP_EOL, $errors)));
     }
+
 }
 
 exit;
