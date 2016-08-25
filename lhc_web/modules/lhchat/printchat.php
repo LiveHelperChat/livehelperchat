9020ebb42d099038d787ced0f13ffd817879bc58
Donatas <doncaz111@gmail.com>
2016-08-25 11:23:02 +0300

    chat.before_send && chat.before_print dispachers added

--- lhc_web/modules/lhchat/printchat.php
+++ lhc_web/modules/lhchat/printchat.php
@@ -3,20 +3,29 @@
 $tpl = erLhcoreClassTemplate::getInstance( 'lhchat/printchat.tpl.php');
 
 if ((int)erLhcoreClassModelChatConfig::fetch('disable_print')->current_value == 1){
-	exit;
+    exit;
 }
 
 try {
     $chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params['user_parameters']['chat_id']);
     if ($chat->hash == $Params['user_parameters']['hash'] && ($chat->status == erLhcoreClassModelChat::STATUS_ACTIVE_CHAT || erLhcoreClassChat::canReopen($chat,true) || ($chat->status == erLhcoreClassModelChat::STATUS_CLOSED_CHAT && $chat->time > time()-1800))) {
-        erLhcoreClassChat::setTimeZoneByChat($chat);
-        $tpl->set('chat',$chat);
+        $errors = array();
+        erLhcoreClassChatEventDispatcher::getInstance()->dispatch('chat.before_print',array('chat' => & $chat, 'errors' => & $errors));
+
+        if(empty($errors)) {
+            erLhcoreClassChat::setTimeZoneByChat($chat);
+            $tpl->set('chat',$chat);
+        } else {
+            $tpl->set('errors',$errors);
+            $tpl->setFile('lhkernel/validation_error.tpl.php');
+        }
+
     } else {
         $tpl->setFile( 'lhchat/errors/chatnotexists.tpl.php');
     }
 
 } catch(Exception $e) {
-   $tpl->setFile('lhchat/errors/chatnotexists.tpl.php');
+    $tpl->setFile('lhchat/errors/chatnotexists.tpl.php');
 }
 
 $Result['content'] = $tpl->fetch();
--- lhc_web/modules/lhchat/sendchat.php
+++ lhc_web/modules/lhchat/sendchat.php
@@ -4,69 +4,71 @@
  * Override
  * */
 try {
-	$chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params['user_parameters']['chat_id']);
+    $chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params['user_parameters']['chat_id']);
 } catch (Exception $e) {
-	$chat = false;
+    $chat = false;
 }
 
 if ((int)erLhcoreClassModelChatConfig::fetch('disable_send')->current_value == 1){
-	exit;
+    exit;
 }
 
 if (is_object($chat) && $chat->hash == $Params['user_parameters']['hash'] && ($chat->status == erLhcoreClassModelChat::STATUS_ACTIVE_CHAT || $chat->status == erLhcoreClassModelChat::STATUS_PENDING_CHAT || erLhcoreClassChat::canReopen($chat,true)))
 {
-	if ( ezcInputForm::hasPostData() ) {
+    if ( ezcInputForm::hasPostData() ) {
 
-		$definition = array(
-				'email' => new ezcInputFormDefinitionElement(
-						ezcInputFormDefinitionElement::OPTIONAL, 'validate_email'
-				)
-		);
+        $definition = array(
+            'email' => new ezcInputFormDefinitionElement(
+                ezcInputFormDefinitionElement::OPTIONAL, 'validate_email'
+            )
+        );
 
-		$form = new ezcInputForm( INPUT_POST, $definition );
-		$Errors = array();
+        $form = new ezcInputForm( INPUT_POST, $definition );
+        $Errors = array();
 
-		if ( !$form->hasValidData( 'email' ) )
-		{
-			$Errors[] =  erTranslationClassLhTranslation::getInstance()->getTranslation('user/edit','Wrong email address');
-		}
+        if ( !$form->hasValidData( 'email' ) )
+        {
+            $Errors[] =  erTranslationClassLhTranslation::getInstance()->getTranslation('user/edit','Wrong email address');
+        }
 
-		if (!isset($_SERVER['HTTP_X_CSRFTOKEN']) || !isset($_POST['csfr_token']) || $_POST['csfr_token'] != $_SERVER['HTTP_X_CSRFTOKEN']) {
-			$Errors[] =  erTranslationClassLhTranslation::getInstance()->getTranslation('user/edit','Invalid CSRF token!');
-		}
+        if (!isset($_SERVER['HTTP_X_CSRFTOKEN']) || !isset($_POST['csfr_token']) || $_POST['csfr_token'] != $_SERVER['HTTP_X_CSRFTOKEN']) {
+            $Errors[] =  erTranslationClassLhTranslation::getInstance()->getTranslation('user/edit','Invalid CSRF token!');
+        }
 
-		if ( empty($Errors) ) {
+        erLhcoreClassChatEventDispatcher::getInstance()->dispatch('chat.before_send',array('chat' => & $chat, 'errors' => & $Errors));
 
-			$tpl = erLhcoreClassTemplate::getInstance('lhchat/sendmail.tpl.php');
-			$mailTemplate = erLhAbstractModelEmailTemplate::fetch(3);
-			erLhcoreClassChatMail::prepareSendMail($mailTemplate);
-			$mailTemplate->recipient = $form->email;
+        if ( empty($Errors) ) {
 
-			$messages = array_reverse(erLhcoreClassModelmsg::getList(array('customfilter' => array('user_id != -1'),'limit' => 500, 'sort' => 'id DESC','filter' => array('chat_id' => $chat->id))));
+            $tpl = erLhcoreClassTemplate::getInstance('lhchat/sendmail.tpl.php');
+            $mailTemplate = erLhAbstractModelEmailTemplate::fetch(3);
+            erLhcoreClassChatMail::prepareSendMail($mailTemplate);
+            $mailTemplate->recipient = $form->email;
 
-			// Fetch chat messages
-			$tpl = new erLhcoreClassTemplate( 'lhchat/messagelist/plain.tpl.php');
-			$tpl->set('chat', $chat);
-			$tpl->set('messages', $messages);
+            $messages = array_reverse(erLhcoreClassModelmsg::getList(array('customfilter' => array('user_id != -1'),'limit' => 500, 'sort' => 'id DESC','filter' => array('chat_id' => $chat->id))));
 
-			$mailTemplate->content = str_replace(array('{user_chat_nick}','{messages_content}'), array($chat->nick,$tpl->fetch()), $mailTemplate->content);
+            // Fetch chat messages
+            $tpl = new erLhcoreClassTemplate( 'lhchat/messagelist/plain.tpl.php');
+            $tpl->set('chat', $chat);
+            $tpl->set('messages', $messages);
 
-			erLhcoreClassChatMail::sendMail($mailTemplate, $chat);
+            $mailTemplate->content = str_replace(array('{user_chat_nick}','{messages_content}'), array($chat->nick,$tpl->fetch()), $mailTemplate->content);
 
-			echo json_encode(array('error' => 'false'));
-			exit;
-		} else {
-			$tpl = erLhcoreClassTemplate::getInstance( 'lhkernel/validation_error.tpl.php');
-			$tpl->set('errors',$Errors);
-			echo json_encode(array('error' => 'true','result' => $tpl->fetch()));
-			exit;
-		}
+            erLhcoreClassChatMail::sendMail($mailTemplate, $chat);
 
-	} else {
-		$tpl = erLhcoreClassTemplate::getInstance( 'lhchat/sendchat.tpl.php');
-		$tpl->set('chat',$chat);
-		echo $tpl->fetch();
-	}
+            echo json_encode(array('error' => 'false'));
+            exit;
+        } else {
+            $tpl = erLhcoreClassTemplate::getInstance( 'lhkernel/validation_error.tpl.php');
+            $tpl->set('errors',$Errors);
+            echo json_encode(array('error' => 'true','result' => $tpl->fetch()));
+            exit;
+        }
+
+    } else {
+        $tpl = erLhcoreClassTemplate::getInstance( 'lhchat/sendchat.tpl.php');
+        $tpl->set('chat',$chat);
+        echo $tpl->fetch();
+    }
 }
 exit;
 ?>
