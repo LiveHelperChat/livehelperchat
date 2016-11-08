4418c721339bcdbd450dc78e1f671920e2e36b2f
wssoffice <office@ws-s.com>
2016-11-08 13:15:31 +0100

    added api methods for getting users

---
+++ lhc_web/modules/lhrestapi/getusers.php
@@ -0,0 +1,36 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Content-Type: application/json');
+
+try 
+{
+    erLhcoreClassRestAPIHandler::validateRequest();
+    
+    $userlist = erLhcoreClassModelUser::getUserList();
+    
+    foreach($userlist as $index => $user)
+    {
+        // loose password
+        unset($user->password);
+        
+        $userlist[$index] = $user;
+    } // end of foreach($userlist as $index => $user)
+    
+    erLhcoreClassRestAPIHandler::outputResponse(array
+            (
+                'error' => false, 
+                'result' => $userlist
+            )
+    );
+    
+} catch (Exception $e) {
+    echo json_encode(array(
+        'error' => true,
+        'result' => $e->getMessage()
+    ));
+}
+
+exit();
+
