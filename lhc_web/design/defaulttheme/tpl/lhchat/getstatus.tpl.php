fe1ea8e98038361e932c48d2084043c9943cbca0
Remdex <remdex@gmail.com>
2017-01-16 13:57:15 +0100

    fix #956

--- lhc_web/design/defaulttheme/tpl/lhchat/getstatus.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/getstatus.tpl.php
@@ -1001,6 +1001,7 @@ var lh_inst  = {
     },  
        
     handleMessage : function(e) {
+        if (typeof e.data !== 'string') { return; }
     	var action = e.data.split(':')[0];    	
     	    	
     	if (action == 'lhc_sizing_chat') {
