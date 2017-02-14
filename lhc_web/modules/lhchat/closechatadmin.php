2b943be292dce5c6f9e197637e7e5fcbcbbffe9c
Remdex <remdex@gmail.com>
2017-02-14 14:20:16 +0100

    Remove from unread list if chat is closed

--- lhc_web/modules/lhchat/closechatadmin.php
+++ lhc_web/modules/lhchat/closechatadmin.php
@@ -17,7 +17,8 @@ if ($chat->user_id == $currentUser->getUserID() || $currentUser->hasAccessTo('lh
 
 	    $chat->status = erLhcoreClassModelChat::STATUS_CLOSED_CHAT;
 	    $chat->chat_duration = erLhcoreClassChat::getChatDurationToUpdateChatID($chat->id);
-
+	    $chat->has_unread_messages = 0;
+	    
 	    $userData = $currentUser->getUserData(true);
 
 	    $msg = new erLhcoreClassModelmsg();
