06ba9100e4ba38a313ad6cea5ef160726c1707d9
Remdex <remdex@gmail.com>
2017-02-22 13:38:55 +0100

    Switch order

--- lhc_web/lib/core/lhuser/lhuser.php
+++ lhc_web/lib/core/lhuser/lhuser.php
@@ -278,8 +278,8 @@ class erLhcoreClassUser{
        
        $this->session->destroy();
        
-       session_destroy();
        session_regenerate_id(true);
+       session_destroy();
    }
 
    public static function getSession()
