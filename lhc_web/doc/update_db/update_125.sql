5fdeb388017aab01a7fe1dfbe9a1326d8235068d
Remdex <remdex@gmail.com>
2017-03-20 20:48:32 +0100

    fix #960

--- lhc_web/doc/update_db/update_125.sql
+++ lhc_web/doc/update_db/update_125.sql
@@ -5,7 +5,7 @@ ALTER TABLE `lh_chat` ADD `has_unread_op_messages` int(11) NOT NULL DEFAULT '0',
 ALTER TABLE `lh_chat` ADD `unread_op_messages_informed` int(11) NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_chat` ADD INDEX `unread_operator` (`has_unread_op_messages`, `unread_op_messages_informed`);
 
-INSERT INTO `lh_abstract_email_template` (`id`,`name`,`from_name`,`from_name_ac`,`from_email`,`from_email_ac`,`content`,`subject`,`bcc_recipients`,`subject_ac`,`reply_to`,`reply_to_ac`,`recipient`) VALUES (11, 'You have unread messages',	'Live Helper Chat',	0,	'',	0,	'Hello,\r\n\r\nOperator {operator} has answered to you\r\n\r\n{messages}\r\n\r\nSincerely,\r\nLive Support Team','Operator has answered to your request',0,'',0,'','','');
+INSERT INTO `lh_abstract_email_template` (`id`,`name`,`from_name`,`from_name_ac`,`from_email`,`from_email_ac`,`content`,`subject`,`bcc_recipients`,`subject_ac`,`reply_to`,`reply_to_ac`,`recipient`) VALUES (11, 'You have unread messages',	'Live Helper Chat',	0,	'',	0,	'Hello,\r\n\r\nOperator {operator} has answered to you\r\n\r\n{messages}\r\n\r\nSincerely,\r\nLive Support Team','Operator has answered to your request','',0,'',0,'');
 
 ALTER TABLE `lh_abstract_survey` ADD `max_stars_1_req` int(11) NOT NULL, COMMENT='';
 ALTER TABLE `lh_abstract_survey` ADD `max_stars_2_req` int(11) NOT NULL, COMMENT='';
