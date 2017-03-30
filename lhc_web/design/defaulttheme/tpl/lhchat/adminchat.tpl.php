b80e239c24ad843fd6a9d3eaa3c03bc8ea9fa7bf
Remdex <remdex@gmail.com>
2017-03-30 18:45:43 +0200

    Show different color background if chat is viewing not an owner

--- lhc_web/design/defaulttheme/tpl/lhchat/adminchat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/adminchat.tpl.php
@@ -25,9 +25,7 @@
 		
 		<div class="user-is-typing" id="user-is-typing-<?php echo $chat->id?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','User is typing now...')?></div>
 		
-		
-		<textarea class="form-control form-group" rows="2" <?php if ($chat->status == erLhcoreClassModelChat::STATUS_CLOSED_CHAT) : ?>readonly="readonly"<?php endif;?> name="ChatMessage" id="CSChatMessage-<?php echo $chat->id?>"></textarea>
-					
+		<textarea class="form-control form-group<?php if ($chat->user_id != erLhcoreClassUser::instance()->getUserID()) : ?> form-control-warning<?php endif;?>" rows="2" <?php if ($chat->status == erLhcoreClassModelChat::STATUS_CLOSED_CHAT) : ?>readonly="readonly"<?php endif;?> name="ChatMessage" id="CSChatMessage-<?php echo $chat->id?>"></textarea>
 
 		<?php include(erLhcoreClassDesign::designtpl('lhchat/part/after_text_area_block.tpl.php')); ?>
 		
