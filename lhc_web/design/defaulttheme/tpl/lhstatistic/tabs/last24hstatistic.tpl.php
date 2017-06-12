fced0fc203fe9044aa7f58e5a0e4db0ee336b1b7
Remdex <remdex@gmail.com>
2017-06-12 11:26:00 +0200

    Split template

--- lhc_web/design/defaulttheme/tpl/lhstatistic/tabs/last24hstatistic.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhstatistic/tabs/last24hstatistic.tpl.php
@@ -147,10 +147,10 @@ $(function() {
 		<td><?php echo $totalMessagesCount-$systemMessagesCount-$totalVisitorsMessagesCount?></td>
 	</tr>
 </table>
-						
-<h2><?php include(erLhcoreClassDesign::designtpl('lhstatistic/tabs/titles/operators_statistic_top_100_by_chats_number.tpl.php'));?></h2>
-<?php $operators = erLhcoreClassChatStatistic::getTopTodaysOperators(100,0,$filter24); ?>
 
+<h2><?php include(erLhcoreClassDesign::designtpl('lhstatistic/tabs/titles/operators_statistic_top_100_by_chats_number.tpl.php'));?></h2>
+
+<?php $operators = erLhcoreClassChatStatistic::getTopTodaysOperators(100,0,$filter24); ?>
 <table class="table">
 	<thead>
 		<tr>
@@ -161,16 +161,5 @@ $(function() {
 			<td><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/statistic','Last activity');?></td>
 		</tr>
 	</thead>
-	<?php foreach ($operators as $operator) : ?>
-	<tr>
-		<td><?php echo htmlspecialchars((string)$operator)?></td>
-		<td><?php echo $operator->statistic_total_chats?></td>
-		<td><?php echo $operator->statistic_total_messages?></td>
-		<td>
-		  <span class="up-voted"><i class="material-icons up-voted">thumb_up</i><?php echo $operator->statistic_upvotes?></span>
-		  <span class="down-voted"><i class="material-icons down-voted">thumb_down</i><?php echo $operator->statistic_downvotes?></span>
-		</td>
-		<td><?php echo $operator->lastactivity_ago?> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/statistic','ago');?></td>
-	</tr>
-	<?php endforeach;?>
+	<?php include(erLhcoreClassDesign::designtpl('lhstatistic/tabs/part/top_24_operators.tpl.php'));?>
 </table>
