f8a712e4606c2d2315f8562b6ba7b842a5ab96c3
Remdex <remdex@gmail.com>
2017-06-14 13:24:25 +0200

    Add missing content

---
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/actions/mail_archive.tpl.php
@@ -0,0 +1,1 @@
+<a class="material-icons mr-0" onclick="return lhc.revealModal({'iframe':true,'height':500,'url':WWW_DIR_JAVASCRIPT +'chatarchive/sendmail/<?php echo $archive->id?>/<?php echo $chat->id?>'})">mail</a>
