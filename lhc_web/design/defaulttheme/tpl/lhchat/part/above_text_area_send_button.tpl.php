774a8b5a1cfeab6ed5f63a949a3607e9d5c59e26
Remdex <remdex@gmail.com>
2017-07-27 15:12:29 +0200

    Send message

--- lhc_web/design/defaulttheme/tpl/lhchat/part/above_text_area_send_button.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/above_text_area_send_button.tpl.php
@@ -1,3 +1,3 @@
 <div class="user-chatwidget-buttons" id="ChatSendButtonContainer"> 
-    <a class="btn btn-default btn-xs" onclick="$('#form-start-chat').submit()" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Start chat');?>"><i class="material-icons mr-0">&#xE569;</i></a>
+    <a class="btn btn-default btn-xs" onclick="$('#form-start-chat').submit()" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Send message');?>"><i class="material-icons mr-0">&#xE569;</i></a>
 </div>
