331421ab54b6ebdd8e68ccbea76e88487245e5ba
Remdex <remdex@gmail.com>
2017-11-24 05:00:12 +0100

    show proactive on non mobile browsers

--- lhc_web/design/defaulttheme/tpl/lhchat/chatcheckoperatormessage.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chatcheckoperatormessage.tpl.php
@@ -10,6 +10,11 @@ lh_inst.stopCheckNewMessage();
 <?php if ($visitor->show_on_mobile == 1) : ?>
     lh_inst.isProactivePending = 1;
 	lh_inst.showStartWindow('<?php echo erLhcoreClassModelChatConfig::fetch('explicit_http_mode')->current_value?>//<?php echo $_SERVER['HTTP_HOST']?><?php echo erLhcoreClassDesign::baseurl('chat/readoperatormessage')?><?php $department !== false ? print '/(department)/'.$department : '' ?><?php $theme !== false ? print '/(theme)/'.$theme : ''?><?php $operator !== false ? print '/(operator)/'.$operator : ''?><?php $priority !== false ? print '/(priority)/'.$priority : ''?><?php $uarguments !== false ? print '/(ua)/'.$uarguments : ''?><?php $survey !== false ? print '/(survey)/'.$survey : ''?>/(vid)/<?php echo $vid;?><?php $visitor->invitation_assigned == true ? print '/(playsound)/true' : ''?>/(fullheight)/<?= $fullheight ? 'true' : 'false' ?>',true);
+<?php else : ?>
+if (window.innerWidth > 1023) {
+    lh_inst.isProactivePending = 1;
+    lh_inst.showStartWindow('<?php echo erLhcoreClassModelChatConfig::fetch('explicit_http_mode')->current_value?>//<?php echo $_SERVER['HTTP_HOST']?><?php echo erLhcoreClassDesign::baseurl('chat/readoperatormessage')?><?php $department !== false ? print '/(department)/'.$department : '' ?><?php $theme !== false ? print '/(theme)/'.$theme : ''?><?php $operator !== false ? print '/(operator)/'.$operator : ''?><?php $priority !== false ? print '/(priority)/'.$priority : ''?><?php $uarguments !== false ? print '/(ua)/'.$uarguments : ''?><?php $survey !== false ? print '/(survey)/'.$survey : ''?>/(vid)/<?php echo $vid;?><?php $visitor->invitation_assigned == true ? print '/(playsound)/true' : ''?>/(fullheight)/<?= $fullheight ? 'true' : 'false' ?>',true);
+}
 <?php endif; ?>
 
 <?php elseif (isset($dynamic)) : ?>
