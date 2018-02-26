de386aca5f9dc0f42d88c81842e092f295ed7e04
Remdex <remdex@gmail.com>
2018-02-26 11:37:43 +0100

    Mobile improve

--- lhc_web/design/defaulttheme/tpl/lhchat/getstatusembed.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/getstatusembed.tpl.php
@@ -118,7 +118,7 @@ var lh_inst_page  = {
 		 var locationCurrent = encodeURIComponent(window.location.href.substring(window.location.protocol.length));
          this.initial_iframe_url = "<?php echo erLhcoreClassModelChatConfig::fetch('explicit_http_mode')->current_value?>//<?php echo $_SERVER['HTTP_HOST']?><?php echo erLhcoreClassDesign::baseurl('chat/chatwidget')?><?php $leaveamessage == true ? print '/(leaveamessage)/true' : ''?><?php $department !== false ? print '/(department)/'.$department : ''?><?php $theme !== false ? print '/(theme)/'.$theme : ''?><?php $operator !== false ? print '/(operator)/'.$operator : ''?><?php $priority !== false ? print '/(priority)/'.$priority : ''?><?php $uarguments !== false ? print '/(ua)/'.$uarguments : ''?>/(mode)/embed"+this.getAppendCookieArguments()+'?URLReferer='+locationCurrent+this.parseOptions();
 
-         var raw_css = "@media only screen and (max-width : 654px) { #lhc_status_container_page{position: fixed; overflow: hidden; right: 0px; left: 0px; top: 0px; bottom: 0px;} #lhc_status_container_page iframe{width:100% !important;height: 100%!important}}";
+         var raw_css = "@media only screen and (max-device-width : 640px) { #lhc_status_container_page{position: fixed; overflow: hidden; right: 0px; left: 0px; top: 0px; bottom: 0px;} #lhc_status_container_page iframe{width:100% !important;height: 100%!important}}";
          this.addCss(raw_css);
 
          this.iframe_html = '<iframe id="fdbk_iframe_page" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/getstatus','Live Help')?>" allowTransparency="true" scrolling="no" class="loading" frameborder="0" ' +
