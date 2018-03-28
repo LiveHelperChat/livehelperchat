b12b9b06b41a0584ba630dd08c625f86eafd2443
Remdex <remdex@gmail.com>
2018-03-28 11:02:38 +0200

    Change header menu

---
+++ lhc_web/design/defaulttheme/tpl/lhchat/getstatus/burger_menu_css.tpl.php
@@ -0,0 +1,54 @@
+<?php
+
+$burgerMenu = "
+
+ul." . $chatCSSPrefix . "-cf {
+    font-size: 0;
+    list-style: none;
+    float:left!important;
+}
+
+ul." . $chatCSSPrefix . "-cf li {
+    display: inline-block;
+    position: relative;
+    min-width: 100%;
+}
+
+ul." . $chatCSSPrefix . "-cf a {
+    display: block;
+    text-decoration: none;
+}
+
+ul." . $chatCSSPrefix . "-cf li ul a{
+    background-color:#525252;
+    padding:6px!important;
+}
+
+ul." . $chatCSSPrefix . "-cf > li > a{
+    font-size:21px!important;
+    color:#a6a6a6;
+    text-align:center!important;
+}
+
+ul." . $chatCSSPrefix . "-cf li ul {
+    left: 0;
+    position: absolute;
+    top: 22px;
+    visibility: hidden;
+    z-index: 1;
+}
+
+ul." . $chatCSSPrefix . "-cf li:hover ul {
+    top: 22px;
+    visibility: visible;
+}
+
+ul." . $chatCSSPrefix . "-cf li ul a:hover {
+    background: #6b6b6b;
+}
+
+";
+
+echo str_replace(array("\n","\r"),"",$burgerMenu);
+
+?>
