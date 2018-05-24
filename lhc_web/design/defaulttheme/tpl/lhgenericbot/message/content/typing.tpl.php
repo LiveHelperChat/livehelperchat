741e4dc290a22eaeba7d116e7510163df6daca51
Remdex <remdex@gmail.com>
2018-05-24 16:43:19 +0200

    Check if not empty

--- lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/typing.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/typing.tpl.php
@@ -1,4 +1,4 @@
 <?php if (isset($async_call) && $async_call == true) : ?>
 <script>lhinst.setDelay(<?php echo $msg['id']?>,<?php echo $metaMessage['duration']?>);</script>
-<div class="msg-body hide"><?php if (isset($metaMessage['text'])) : ?><?php echo htmlspecialchars($metaMessage['text'])?><?php else : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncuser','Typing...');?><?php endif; ?></div>
+<div class="msg-body hide"><?php if (isset($metaMessage['text']) && !empty($metaMessage['text'])) : ?><?php echo htmlspecialchars($metaMessage['text'])?><?php else : ?><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncuser','Typing...');?><?php endif; ?></div>
 <?php endif; ?>
