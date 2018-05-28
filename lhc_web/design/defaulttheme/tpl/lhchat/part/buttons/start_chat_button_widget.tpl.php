4bdab64126514d573052232b7431b82f4b4a7765
Remdex <remdex@gmail.com>
2018-05-28 21:23:38 +0200

    Override button title

--- lhc_web/design/defaulttheme/tpl/lhchat/part/buttons/start_chat_button_widget.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/buttons/start_chat_button_widget.tpl.php
@@ -1,7 +1,7 @@
 <?php $startChatText = erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','Start chat');?>
 <?php if (isset($theme) && $theme !== false && isset($theme->bot_configuration_array['custom_start_button']) && !empty($theme->bot_configuration_array['custom_start_button']) && $onlyBotOnline == false) {
     $startChatText = htmlspecialchars($theme->bot_configuration_array['custom_start_button']);
-} elseif (isset($theme) && $theme !== false && isset($theme->bot_configuration_array['custom_start_button']) && !empty($theme->bot_configuration_array['custom_start_button']) && $onlyBotOnline == true) {
+} elseif (isset($theme) && $theme !== false && isset($theme->bot_configuration_array['custom_start_button_bot']) && !empty($theme->bot_configuration_array['custom_start_button_bot']) && $onlyBotOnline == true) {
     $startChatText = htmlspecialchars($theme->bot_configuration_array['custom_start_button_bot']);
 } ?>
 <input type="submit" class="btn btn-default btn-sm startchat" value="<?php echo $startChatText?>" name="StartChatAction" />
