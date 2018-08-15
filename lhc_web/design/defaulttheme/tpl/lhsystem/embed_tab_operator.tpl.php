5a5c353ca279cbcfb6b856fb9a241da69019f4ed
Remdex <remdex@gmail.com>
2018-08-15 16:28:10 +0200

    Separate template for operator field

---
+++ lhc_web/design/defaulttheme/tpl/lhsystem/embed_tab_operator.tpl.php
@@ -0,0 +1,3 @@
+<div class="col-md-6">
+    <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/htmlcode','Operator ID')?></label> <input type="text" id="id_operator" class="form-control" placeholder="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/htmlcode','To what operator chat should be assigned automatically?')?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/htmlcode','To what operator chat should be assigned automatically?')?>" value="" />
+</div>
