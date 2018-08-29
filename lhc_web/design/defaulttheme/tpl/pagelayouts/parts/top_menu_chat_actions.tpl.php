e2a58968469dff788570d69051a72da4999a644a
Remigijus Kiminas <remdex@gmail.com>
2018-08-29 16:01:08 +0300

    fix #1258

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/top_menu_chat_actions.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/top_menu_chat_actions.tpl.php
@@ -2,7 +2,7 @@
 <?php if ($parts_top_menu_chat_actions_enabled == true) :
 
 if ($currentUser->hasAccessTo('lhchat','allowchattabs')) {
-    $menuItems[] = array('href' => 'javascript:void(0)', 'onclick' => 'javascript:lhinst.chatTabsOpen()' ,'iclass' => 'chat', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Chat tabs')); 		
+    $menuItems[] = array('href' => '#', 'onclick' => 'javascript:lhinst.chatTabsOpen()' ,'iclass' => 'chat', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Chat tabs'));
 } 
 
 $menuItems[] = array('href' => erLhcoreClassDesign::baseurl('chat/list'),'iclass' => 'list', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Chats list'));
