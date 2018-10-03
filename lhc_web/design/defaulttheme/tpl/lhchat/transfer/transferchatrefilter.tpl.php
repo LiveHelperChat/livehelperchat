bfd243173a9cfeb27c0ad27fb97b40e9406d4380
Remigijus Kiminas <remdex@gmail.com>
2018-10-03 01:24:55 -0400

    Show same department also in offline mode

--- lhc_web/design/defaulttheme/tpl/lhchat/transfer/transferchatrefilter.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/transfer/transferchatrefilter.tpl.php
@@ -7,6 +7,6 @@
 <?php endif; endforeach; ?>
 
 <h4><span class="label label-default"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Offline');?></span></h4>
-<?php foreach ($departments as $departament) : if ($departament->id !== $departments_filter['dep_id'] && !in_array($departament->id, $onlineDepartments)) : ?>
+<?php foreach ($departments as $departament) : if (/*$departament->id !== $departments_filter['dep_id'] &&*/ !in_array($departament->id, $onlineDepartments)) : ?>
    <div class="checkbox"><label><input type="radio" name="DepartamentID<?php echo $departments_filter['chat_id']?>" value="<?php echo $departament->id?>"/> <?php echo htmlspecialchars($departament->name)?></label></div>
 <?php endif; endforeach; ?>
