6420b9c9a4485adf98ed91fa3edd5c5a0f990bfc
Remigijus Kiminas <remdex@gmail.com>
2018-12-07 02:57:24 -0500

    Set correct class attribute

--- lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/video.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/video.tpl.php
@@ -1,6 +1,6 @@
 <?php if (isset($metaMessage['payload']) && $metaMessage['payload'] != '') : ?>
 <br/>
-<div className="embed-responsive embed-responsive-16by9">
-    <video className="embed-responsive-item" <?php if (isset($metaMessage['video_options']['autoplay']) && $metaMessage['video_options']['autoplay'] == true) : ?>autoPlay<?php endif;?> <?php if (isset($metaMessage['video_options']['controls']) && $metaMessage['video_options']['controls'] == true) : ?>controls<?php endif;?>><source src=<?php echo htmlspecialchars($metaMessage['payload'])?> /></video>
+<div class="embed-responsive embed-responsive-16by9">
+    <video class="embed-responsive-item" <?php if (isset($metaMessage['video_options']['autoplay']) && $metaMessage['video_options']['autoplay'] == true) : ?>autoPlay<?php endif;?> <?php if (isset($metaMessage['video_options']['controls']) && $metaMessage['video_options']['controls'] == true) : ?>controls<?php endif;?>><source src=<?php echo htmlspecialchars($metaMessage['payload'])?> /></video>
 </div>
 <?php endif; ?>
