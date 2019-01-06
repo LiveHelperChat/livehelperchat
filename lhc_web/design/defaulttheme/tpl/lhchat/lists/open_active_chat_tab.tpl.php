4ff0b16fa7e5106a164c2d13813aedf1551c5eeb
Remigijus Kiminas <remdex@gmail.com>
2019-01-06 02:59:01 -0500

    Open menu on left

--- lhc_web/design/defaulttheme/tpl/lhchat/lists/open_active_chat_tab.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/lists/open_active_chat_tab.tpl.php
@@ -1,6 +1,6 @@
 <nav class="float-right" ng-init="lhc.getToggleWidget('track_open_chats');lhc.getToggleWidget('group_offline_chats')">
     <ul class="nav">
-        <li class="nav-item dropdown">
+        <li class="nav-item dropleft">
             <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="material-icons mr-0">settings_applications</i></a>
             <ul class="dropdown-menu" role="menu">
                 <li class="dropdown-item"><a href="#" ng-click="lhc.appendActiveChats()" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('front/default', 'Open last 10 my active chats')?>"><i class="material-icons chat-active">chat</i> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('front/default', 'Open my active chats'); ?></a></li>
