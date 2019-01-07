ca5e49d9fbe69ecb62cd24a55d4b9c57c7a0eafe
Remigijus Kiminas <remdex@gmail.com>
2019-01-07 00:00:38 -0500

    More higher

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/dynamic_height.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/dynamic_height.tpl.php
@@ -6,9 +6,9 @@ if (isset($Result['dynamic_height'])) : ?>
 <script>
 $( window ).on('load',function() {
 	if (window.opener && $('#user-popup-window').length > 0) {
-		var windowHeight = $('#user-popup-window').height()+55<?php if (isset($Result['dynamic_height_adjust'])) {echo $Result['dynamic_height_adjust'];}?>;
+		var windowHeight = $('#user-popup-window').height()+60<?php if (isset($Result['dynamic_height_adjust'])) {echo $Result['dynamic_height_adjust'];}?>;
 		// Don't do anything like popup is bigger than our screen
-		if (screen.availHeight > (windowHeight+55)){
+		if (screen.availHeight > (windowHeight+60)){
 			  window.resizeBy(0, windowHeight - $( window ).height());
 		}
 	}
