15df263c47d286c4702dd151e93e0465c0250459
Remigijus Kiminas <remdex@gmail.com>
2019-01-13 12:00:04 -0500

    Change to left menu

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/user_box.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/user_box.tpl.php
@@ -1,7 +1,7 @@
 <?php
 $currentUser = erLhcoreClassUser::instance();
 $UserData = $currentUser->getUserData(true); ?>
-<li class="nav-item dropdown">
+<li class="nav-item dropleft">
     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo htmlspecialchars($UserData->name),' ',htmlspecialchars($UserData->surname)?></a>
     <div class="dropdown-menu" role="menu">
         <a class="dropdown-item" href="<?php echo erLhcoreClassDesign::baseurl('user/account')?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Account')?>"><i class="material-icons">account_box</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Account')?></a>
