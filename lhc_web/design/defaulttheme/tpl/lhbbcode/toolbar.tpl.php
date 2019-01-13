ce0e099437c44c098bab7bc2d507b283518f2277
Remigijus Kiminas <remdex@gmail.com>
2019-01-13 07:46:22 -0500

    Different style button for preview

--- lhc_web/design/defaulttheme/tpl/lhbbcode/toolbar.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhbbcode/toolbar.tpl.php
@@ -4,6 +4,6 @@
         <button type="button" class="btn btn-secondary" data-selector="<?php echo $bbcodeOptions['selector']?>" onclick="window.lhcSelector = $(this).attr('data-selector'); lhc.revealModal({'url':WWW_DIR_JAVASCRIPT+'/chat/bbcodeinsert/0/(mode)/editor'})">
             <i class="material-icons mr-0">face</i>
         </button>
-        <button type="button" class="btn btn-info" data-selector="<?php echo $bbcodeOptions['selector']?>" onclick="return lhc.revealModal({'loadmethod':'post', 'datapost':{'msg':$($(this).attr('data-selector')).val()}, 'url':WWW_DIR_JAVASCRIPT +'chat/previewmessage'})" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Preview')?>"><i class="material-icons mr-0">visibility</i></button>
+        <button type="button" class="btn btn-outline-secondary" data-selector="<?php echo $bbcodeOptions['selector']?>" onclick="return lhc.revealModal({'loadmethod':'post', 'datapost':{'msg':$($(this).attr('data-selector')).val()}, 'url':WWW_DIR_JAVASCRIPT +'chat/previewmessage'})" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Preview')?>"><i class="material-icons mr-0">visibility</i></button>
     </div>
 </div>
