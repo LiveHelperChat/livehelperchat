05107dc750bafba469c61c4fbb2fbedce6c3fc67
Remigijus Kiminas <remdex@gmail.com>
2019-01-17 00:38:23 -0500

    Fix themes after messages style change

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/page_head_css_theme.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/page_head_css_theme.tpl.php
@@ -1,27 +1,27 @@
 <?php if (isset($Result['theme']) && $Result['theme'] !== false) : ?>
     <style>
         <?php if ($Result['theme']->buble_visitor_background != '') : ?>
-        div.message-row.response div.msg-body{background-color:#<?php echo htmlspecialchars($Result['theme']->buble_visitor_background)?>;}
+        #messagesBlock div.message-row.response div.msg-body{background-color:#<?php echo htmlspecialchars($Result['theme']->buble_visitor_background)?>;}
         <?php endif;?>
 
         <?php if ($Result['theme']->buble_visitor_text_color != '') : ?>
-        div.message-row.response div.msg-body{color:#<?php echo htmlspecialchars($Result['theme']->buble_visitor_text_color)?>;}
+        #messagesBlock div.message-row.response div.msg-body{color:#<?php echo htmlspecialchars($Result['theme']->buble_visitor_text_color)?>;}
         <?php endif;?>
 
         <?php if ($Result['theme']->buble_visitor_title_color != '') : ?>
-        .vis-tit{color:#<?php echo htmlspecialchars($Result['theme']->buble_visitor_title_color)?>;}
+        #messagesBlock div.response .vis-tit{color:#<?php echo htmlspecialchars($Result['theme']->buble_visitor_title_color)?>;}
         <?php endif;?>
 
         <?php if ($Result['theme']->buble_operator_background != '') : ?>
-        div.message-admin div.msg-body{background-color:#<?php echo htmlspecialchars($Result['theme']->buble_operator_background)?>;}
+        #messagesBlock div.message-admin div.msg-body{background-color:#<?php echo htmlspecialchars($Result['theme']->buble_operator_background)?>;}
         <?php endif;?>
 
         <?php if ($Result['theme']->buble_operator_text_color != '') : ?>
-        div.message-admin div.msg-body{color:#<?php echo htmlspecialchars($Result['theme']->buble_operator_text_color)?>;}
+        #messagesBlock div.message-admin div.msg-body{color:#<?php echo htmlspecialchars($Result['theme']->buble_operator_text_color)?>;}
         <?php endif;?>
 
         <?php if ($Result['theme']->buble_operator_title_color != '') : ?>
-        .op-tit{color:#<?php echo htmlspecialchars($Result['theme']->buble_operator_title_color)?>;}
+        #messagesBlock div.message-admin .op-tit{color:#<?php echo htmlspecialchars($Result['theme']->buble_operator_title_color)?>;}
         <?php endif;?>
 
         .btn-bot,.btn-bot:hover,.btn-bot:focus,.btn-bot:active{
