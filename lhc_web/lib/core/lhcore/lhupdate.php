17b00dbca71d66d861ea70b3abe76d80d8b1bcd4
Remigijus Kiminas <remdex@gmail.com>
2019-01-20 05:06:42 -0500

    Update version numbers

--- lhc_web/lib/core/lhcore/lhupdate.php
+++ lhc_web/lib/core/lhcore/lhupdate.php
@@ -2,8 +2,8 @@
 
 class erLhcoreClassUpdate
 {
-	const DB_VERSION = 191;
-	const LHC_RELEASE = 316;
+	const DB_VERSION = 192;
+	const LHC_RELEASE = 317;
 
 	public static function doTablesUpdate($definition){
 		$updateInformation = self::getTablesStatus($definition);
