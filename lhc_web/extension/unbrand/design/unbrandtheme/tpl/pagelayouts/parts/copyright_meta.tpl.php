32f76d1c8fdf4cca11950ae2224d018bebef2ee8
rehv <rf.rehv@gmail.com>
2019-01-29 16:27:32 -0200

    Extension Unbrand
    Adiciona logomarcas e copyright do TJDFT.

---
+++ lhc_web/extension/unbrand/design/unbrandtheme/tpl/pagelayouts/parts/copyright_meta.tpl.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * Since 2.19v you can remove this even on commercial projects etc.
+ * Read more this article why it was done
+ * https://livehelperchat.com/commercial-support-13c.html
+ * 
+ * */?>
+<meta name="copyright" content="Tribunal de Justiça do Distrito Federal e Territórios">
---
+++ lhc_web/extension/unbrand/design/unbrandtheme/tpl/pagelayouts/parts/page_footer_user.tpl.php
@@ -0,0 +1,8 @@
+<div class="mt10">
+    <div class="row mt10 footer-row">
+        <div class="columns twelve">
+            <p class="pull-right"><a target="_blank" href="http://www.tjdft.jus.br">Tribunal de Justiça do Distrito Federal e Territórios &copy; <?php echo date('Y')?></a></p>
+            <p><a href="<?php if (isset($Result['theme']) !== false && $Result['theme']->widget_copyright_url != '') : ?><?php echo htmlspecialchars($Result['theme']->widget_copyright_url) ?><?php else : ?><?php echo erLhcoreClassModelChatConfig::fetch('customer_site_url')->current_value?><?php endif;?>" target="_blank"><?php if (isset($Result['theme'])) : ?><?php echo htmlspecialchars($Result['theme']->name_company)?><?php else : ?><?php echo htmlspecialchars(erLhcoreClassModelChatConfig::fetch('customer_company_name')->current_value)?><?php endif;?></a></p>
+        </div>
+    </div>
+</div>
