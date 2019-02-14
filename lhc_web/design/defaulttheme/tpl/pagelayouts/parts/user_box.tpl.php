ed1843fad2ef6d9acdaf0dae91e9eb4ac32d1594
Remigijus Kiminas <remdex@gmail.com>
2019-02-14 09:32:46 -0500

    Just in case

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/user_box.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/user_box.tpl.php
@@ -1,5 +1,6 @@
 <?php
 $currentUser = erLhcoreClassUser::instance();
+if ($currentUser->isLogged()) :
 $UserData = $currentUser->getUserData(true); ?>
 <li class="nav-item dropleft">
     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo htmlspecialchars($UserData->name),' ',htmlspecialchars($UserData->surname)?></a>
@@ -8,4 +9,4 @@ $UserData = $currentUser->getUserData(true); ?>
         <a class="dropdown-item" href="<?php echo erLhcoreClassDesign::baseurl('user/logout')?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Logout');?>"><i class="material-icons">exit_to_app</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Logout');?></a>
     </div>
 </li>
-<?php unset($currentUser);unset($UserData);?>
+<?php unset($currentUser);unset($UserData);endif; ?>
