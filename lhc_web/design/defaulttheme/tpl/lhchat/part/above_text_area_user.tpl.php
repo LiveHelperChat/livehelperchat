7967e8c6143c4c672591074fe013cdd5fbeb7011
Irina <you@example.com>
2019-03-07 15:18:43 +0200

    fix chat exept chart and change above_text_area_user.tpl

--- lhc_web/design/defaulttheme/tpl/lhchat/part/above_text_area_user.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/above_text_area_user.tpl.php
@@ -1,12 +1,9 @@
 
 <div class="user-chatwidget-buttons" id="ChatSendButtonContainer">
 
-    <a href="#" class="send-icons-action" onclick="lhinst.addmsguser(true)" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Send')?>"><i class="material-icons">send</i></a>
+    <a class="send-icons-action"  href="#" onclick="lhinst.addmsguser(true)" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Send')?>"><i class="material-icons">send</i></a>
 
     <div class="btn-group float-right dropleft" role="group">
-        <a class="dropdown-toggle dropdown-toggle-widget" data-toggle="dropdown" aria-expanded="false">
-              <i class="material-icons mr-0">settings</i>
-        </a>
         <div role="menu" data-dropdown-content class="dropdown-menu dropup">
     		<?php if (erLhcoreClassModelChatConfig::fetch('bbc_button_visible')->current_value == 1) : ?>
     		<a class="dropdown-item" href="#" onclick="return lhc.revealModal({'url':'<?php echo erLhcoreClassDesign::baseurl('chat/bbcodeinsert')?>'})" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','BB Code')?>"><i class="material-icons">&#xE24E;</i></a>
