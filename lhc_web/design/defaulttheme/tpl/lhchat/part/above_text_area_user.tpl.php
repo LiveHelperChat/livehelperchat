4a55f123325c547be89e4d0547fe0ca6f8ec7f02
Irina <you@example.com>
2019-03-12 11:04:31 +0200

    change hieght of input

--- lhc_web/design/defaulttheme/tpl/lhchat/part/above_text_area_user.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/above_text_area_user.tpl.php
@@ -2,25 +2,7 @@
 
     <a href="#" onclick="lhinst.addmsguser(true)"
        title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat', 'Send') ?>">
-        <i class="material-icons text-muted  settings pt-2">send</i>
+        <i class="material-icons text-muted  settings">send</i>
     </a>
-
-    <div class="btn-group float-right dropleft" role="group">
-        <div role="menu" data-dropdown-content class="dropdown-menu dropup">
-            <?php if (erLhcoreClassModelChatConfig::fetch('bbc_button_visible')->current_value == 1) : ?>
-                <a class="dropdown-item" href="#"
-                   onclick="return lhc.revealModal({'url':'<?php echo erLhcoreClassDesign::baseurl('chat/bbcodeinsert') ?>'})"
-                   title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat', 'BB Code') ?>"><i
-                        class="material-icons">&#xE24E;</i></a>
-            <?php endif; ?>
-
-            <?php if (isset($chat_embed_mode) && $chat_embed_mode == true) : ?>
-                <a class="dropdown-item" href="#"
-                   title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat', 'Close') ?>"
-                   onclick="lhinst.userclosedchatembed();"><i class="material-icons">close</i></a></li>
-            <?php endif; ?>
-
-        </div>
-    </div>
 </div>
 
