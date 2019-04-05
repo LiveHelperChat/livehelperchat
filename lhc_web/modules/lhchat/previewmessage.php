5b016a7090180ef581dc8af8827d6787f49722c4
Remigijus Kiminas <remdex@gmail.com>
2019-04-05 13:30:35 -0400

    Allow extension to render custom

--- lhc_web/modules/lhchat/previewmessage.php
+++ lhc_web/modules/lhchat/previewmessage.php
@@ -1,7 +1,13 @@
 <?php
 
 $tpl = erLhcoreClassTemplate::getInstance('lhchat/previewmessage.tpl.php');
-$tpl->set('msg',(isset($_POST['msg']) ? $_POST['msg'] : null));
+
+$msg = new stdClass();
+$msg->msg = (isset($_POST['msg']) ? $_POST['msg'] : null);
+
+erLhcoreClassChatEventDispatcher::getInstance()->dispatch('chat.before_msg_admin_saved',array('msg' => & $msg));
+
+$tpl->set('msg',$msg->msg);
 
 echo $tpl->fetch();
 exit;
