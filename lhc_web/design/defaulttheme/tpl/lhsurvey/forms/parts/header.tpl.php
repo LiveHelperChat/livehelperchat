eaeb0ed63dc123a5a2bd8699cb119d0af1003602
Remigijus Kiminas <remdex@gmail.com>
2019-04-14 05:10:04 -0400

    No need for menu

--- lhc_web/design/defaulttheme/tpl/lhsurvey/forms/parts/header.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsurvey/forms/parts/header.tpl.php
@@ -1,10 +1,7 @@
 <div class="row">
-	<div class="col-9">
+	<div class="col-12">
 		<div>
 			<h6><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('survey/fill','Please complete this short evaluation survey'); ?></h6>
 		</div>
-	</div>
-	<div class="col-3 mb-1">	
-		<?php include(erLhcoreClassDesign::designtpl('lhchat/customer_user_settings.tpl.php'));?>
 	</div>
 </div>
