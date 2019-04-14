f1bc42729870f6840a469afcdd56e9ff33604a19
Remigijus Kiminas <remdex@gmail.com>
2019-04-14 04:02:24 -0400

    Not needed

--- lhc_web/design/defaulttheme/tpl/lhchat/chat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat.tpl.php
@@ -16,11 +16,11 @@
 			<a href="<?php echo erLhcoreClassDesign::baseurl('chat/reopen')?>/<?php echo $chat->id?>/<?php echo $chat->hash?><?php if ( isset($chat_widget_mode) && $chat_widget_mode == true ) : ?>/(mode)/widget<?php endif; ?><?php if ( isset($chat_embed_mode) && $chat_embed_mode == true ) : ?>/(embedmode)/embed<?php endif;?>" class="btn btn-secondary" ><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chatnotexists','Resume chat');?></a>
 		<?php endif; ?>
 						
-		<?php if (!isset($paid_chat_params['allow_read']) || $paid_chat_params['allow_read'] == false) : ?>
+		<?php /*if (!isset($paid_chat_params['allow_read']) || $paid_chat_params['allow_read'] == false) : ?>
     		<?php if ($chat->status == erLhcoreClassModelChat::STATUS_CLOSED_CHAT && ( (isset($chat_widget_mode) && $chat_widget_mode == true && $chat->time < time()-1800)) ) : ?>
     			<input type="button" class="btn btn-secondary mb-1" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/chat','Close')?>" onclick="lhinst.userclosedchatembed();" />
     		<?php endif;?>
-		<?php endif;?>
+		<?php endif;*/?>
 		
 	</div>
 </div>
