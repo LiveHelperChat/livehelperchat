52261ca7b748b6bc92824ead7cea94b0437b118e
Remigijus Kiminas <remdex@gmail.com>
2019-07-29 09:30:58 +0300

    Master trans (#1371)
    Multilanguage for bot translations

--- lhc_web/lib/models/lhgenericbot/erlhcoreclassmodelgenericbottritem.php
+++ lhc_web/lib/models/lhgenericbot/erlhcoreclassmodelgenericbottritem.php
@@ -29,9 +29,64 @@ class erLhcoreClassModelGenericBotTrItem {
         return $this->name;
     }
 
+    public function __get($var)
+    {
+        switch ($var) {
+            case 'translation_array':
+                $attr = str_replace('_array','',$var);
+                if (!empty($this->{$attr})) {
+                    $jsonData = json_decode($this->{$attr},true);
+                    if ($jsonData !== null) {
+                        $this->{$var} = $jsonData;
+                    } else {
+                        $this->{$var} = array('default' => ($this->{$attr} != '' ? $this->{$attr} : null), 'items' => array());
+                    }
+                } else {
+                    $this->{$var} = array('default' => '', 'items' => array());
+                }
+                return $this->{$var};
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * @desc translate auto responder if translation by chat exists
+     *
+     * @param $locale
+     */
+    public function translateByChat($locale) {
+
+        $this->translation_front = $this->translation_array['default'];
+
+        if ($locale === null) {
+            return;
+        }
+
+        // Try to find exact match
+        foreach ($this->translation_array['items'] as $data) {
+            if (in_array($locale, $data['languages'])) {
+                $this->translation_front = $data['message'];
+                return;
+            }
+        }
+
+        // Try to match general match by first two letters
+        $localeShort = explode('-',$locale)[0];
+        foreach ($this->translation_array['items'] as $data) {
+            if (in_array($localeShort, $data['languages'])) {
+                $this->translation_front = $data['message'];
+                return;
+            }
+        }
+    }
+
     public $id = null;
     public $name = '';
     public $identifier = '';
     public $translation = '';
     public $group_id = 0;
+    public $translation_front = '';
 }
