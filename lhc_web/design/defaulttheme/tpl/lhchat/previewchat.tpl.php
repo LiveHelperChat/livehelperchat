b56107a7395c1d06b7f18261db19f1c6292d4718
Remigijus Kiminas <remdex@gmail.com>
2019-09-30 06:12:10 -0400

    Live Preview Chat

--- lhc_web/design/defaulttheme/tpl/lhchat/previewchat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/previewchat.tpl.php
@@ -16,8 +16,35 @@
             <i class="material-icons">label</i><small>ID - <?php echo $chat->id?></small>&nbsp;<i class="material-icons">label</i><small><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Created')?> - <?php echo date(erLhcoreClassModule::$dateDateHourFormat,$chat->time)?></small>&nbsp;<i class="material-icons">label</i><small><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Chat duration')?> - <?php echo $chat->chat_duration_front?></small>&nbsp;<i class="material-icons">label</i><small><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Waited')?> - <?php echo $chat->wait_time_front?></small>&nbsp;<i class="material-icons">label</i><small><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Priority')?> - (<?php echo $chat->priority?>)</small>
         </div>
       <div class="modal-body">
-<small>
+
+<small id="preview-messages-<?php echo $chat->id?>">
     <?php $messages = array_reverse(erLhcoreClassModelmsg::getList(array('limit' => 100,'sort' => 'id DESC','filter' => array('chat_id' => $chat->id)))); ?>
     <?php include(erLhcoreClassDesign::designtpl('lhchat/lists/msg_obj_list_admin.tpl.php'));?>
 </small>
+
+  <script>
+      (function(chatId, msgId){
+          var currentChatId = chatId;
+          var currentLastMessageID = msgId;
+          function updatePreviewLive() {
+              $.postJSON(WWW_DIR_JAVASCRIPT + 'chat/syncadmin' ,{ 'chats[]': [currentChatId + ',' + currentLastMessageID]}, function(data) {
+                  if (data.result != 'false') {
+                      $.each(data.result, function (i, item) {
+                          currentLastMessageID = item.message_id;
+                          $('#preview-messages-' + item.chat_id).append(item.content);
+                      });
+                  }
+                  if ($('#preview-messages-'+currentChatId).is(':visible') == true){
+                      setTimeout(function () {
+                          updatePreviewLive();
+                      },2000);
+                  }
+              });
+          }
+          setTimeout(function () {
+              updatePreviewLive();
+          },5000);
+      })(<?php echo $chat->id,',',$msg->id?>);
+  </script>
+
 <?php include(erLhcoreClassDesign::designtpl('lhkernel/modal_footer.tpl.php'));?>
