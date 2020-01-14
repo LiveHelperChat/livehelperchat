b63e7ea019bfe8e1fefffac238ca1be9b583bf03
Remigijus Kiminas <remdex@gmail.com>
2020-01-14 10:22:52 -0500

    Update font

--- lhc_web/design/defaulttheme/widget/react-app/src/components/HeaderChat.js
+++ lhc_web/design/defaulttheme/widget/react-app/src/components/HeaderChat.js
@@ -43,19 +43,19 @@ class HeaderChat extends Component {
         return (
             <div className={className}>
                 <div className="col-6 pl-1">
-                    <a href="#" className="text-dark" onClick={this.closeWidget}><i className="material-icons">close</i></a>
+                    <a href="#" className="header-link" onClick={this.closeWidget}><i className="material-icons">minimize</i></a>
                 </div>
                 <div className={classNameMenu}>
                     <div className="d-flex">
                         <div className="ml-auto">
-                            <a href="#" className="text-dark" id="headerDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,5">
+                            <a href="#" className="header-link " id="headerDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,5">
                                 <i className="material-icons">menu</i>
                             </a>
                             <div className="dropdown-menu dropdown-menu-right mr-3" aria-labelledby="dropdownMenuOffset">
                                 {this.props.chatwidget.get('isChatting') === true ? (
-                                        <a className="dropdown-item" onClick={this.endChat} href="#">End Chat</a>
+                                        <a className="dropdown-item" onClick={this.endChat} href="#"><i className="material-icons">close</i>End Chat</a>
                                 ) : ''}
-                                {!this.props.chatwidget.hasIn(['chat_ui','hide_popup']) ? (<a className="dropdown-item" onClick={this.popup} href="#">Popup</a>) : ''}
+                                {!this.props.chatwidget.hasIn(['chat_ui','hide_popup']) ? (<a className="dropdown-item" onClick={this.popup} href="#"><i className="material-icons">open_in_new</i>Popup</a>) : ''}
                             </div>
                         </div>
                     </div>
