b589d8b2f4344e6457909ffa9ddcdc828a480f8b
Remigijus Kiminas <remdex@gmail.com>
2020-01-30 06:00:40 -0500

    Add space

--- lhc_web/design/defaulttheme/widget/react-app/src/components/ChatStartOptions.js
+++ lhc_web/design/defaulttheme/widget/react-app/src/components/ChatStartOptions.js
@@ -15,7 +15,7 @@ class ChatStartOptions extends PureComponent {
         return (
             <div className="btn-group dropup pt-1 disable-select pl-2 pt-2">
                 <i className="material-icons settings text-muted" id="chat-dropdown-options" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">settings</i>
-                <div className="dropdown-menu shadow bg-white lhc-dropdown-menu rounded">
+                <div className="dropdown-menu shadow bg-white lhc-dropdown-menu rounded ml-1">
                     <div className="d-flex flex-row">
                         <a href="#" onClick={(e) => this.props.toggleModal()} title="BB Code"><i className="material-icons chat-setting-item text-muted">face</i></a>
                     </div>
