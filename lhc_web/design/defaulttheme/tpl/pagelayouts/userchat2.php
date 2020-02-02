d42643e22b3e103429c65b4250a3649fd91159ad
Remigijus Kiminas <remdex@gmail.com>
2020-02-02 05:15:49 -0500

    Clear cache

--- lhc_web/design/defaulttheme/tpl/pagelayouts/userchat2.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/userchat2.php
@@ -21,9 +21,9 @@
 <?php
 $detect = new Mobile_Detect();
 if ($detect->version('IE') !== false) : ?>
-    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.ie.js')?>?t=25"></script>
+    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.ie.js')?>?t=26"></script>
 <?php else : ?>
-    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.js')?>?t=25"></script>
+    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.js')?>?t=26"></script>
 <?php endif; ?>
 </body>
 </html>
