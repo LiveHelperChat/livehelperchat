14fdb38f995c49c7bd2a2599381fa1b195a1ec75
Remigijus Kiminas <remdex@gmail.com>
2020-02-04 10:06:10 -0500

    Easier style override

--- lhc_web/design/defaulttheme/tpl/lhchat/checkchatstatus.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/checkchatstatus.tpl.php
@@ -7,12 +7,12 @@
         <?php include(erLhcoreClassDesign::designtpl('lhchat/part/operator_profile_main_pre.tpl.php')); ?>
     	<?php include(erLhcoreClassDesign::designtpl('lhchat/part/operator_profile.tpl.php'));?>
     <?php elseif ($chat->status == erLhcoreClassModelChat::STATUS_BOT_CHAT) : ?>
-        <h6 class="fs12"><?php if ($theme !== false  && $theme->bot_status_text != '') : ?>
+        <h6 class="fs12 status-text"><?php if ($theme !== false  && $theme->bot_status_text != '') : ?>
             <?php echo htmlspecialchars($theme->bot_status_text)?>
         <?php else : ?>
             <?php include(erLhcoreClassDesign::designtpl('lhchat/checkchatstatus_text/bot_chat.tpl.php'));?><?php endif; ?></h6>
     <?php elseif ($is_proactive_based == true) : ?>
-    <h6 class="fs12">
+    <h6 class="fs12 status-text">
     	<?php if ($theme !== false  && $theme->support_joined != '') : ?>
     	   <?php echo htmlspecialchars($theme->support_joined)?>
     	<?php else : ?>
@@ -25,7 +25,7 @@
     </h6>
     <?php endif;?>
     <?php elseif ($is_closed == true) : ?>
-    <h6 class="fs12">
+    <h6 class="fs12 status-text">
     	<?php if ($theme !== false  && $theme->support_closed != '') : ?>
     	   <?php echo htmlspecialchars($theme->support_closed)?>
     	<?php else : ?>
@@ -39,7 +39,7 @@
             <?php include(erLhcoreClassDesign::designtpl('lhchat/part/operator_profile_main_pre.tpl.php')); ?>
             <?php include(erLhcoreClassDesign::designtpl('lhchat/part/operator_profile.tpl.php'));?>
         <?php else : ?>
-            <h6 class="fs12"><?php if ($theme !== false  && $theme->bot_status_text != '') : ?>
+            <h6 class="fs12 status-text"><?php if ($theme !== false  && $theme->bot_status_text != '') : ?>
                 <?php echo htmlspecialchars($theme->bot_status_text)?>
             <?php else : ?>
                <?php include(erLhcoreClassDesign::designtpl('lhchat/checkchatstatus_text/bot_chat.tpl.php'));?>
@@ -47,7 +47,7 @@
         <?php endif; ?>
 
     <?php elseif ($is_online == true) : ?>
-    <h6 class="fs12">
+    <h6 class="fs12 status-text">
          <?php if ($chat->number_in_queue > 1) : ?>
             <?php if ($theme !== false  && $theme->pending_join_queue != '') : ?>
                  <?php echo htmlspecialchars(str_replace('{number}',$chat->number_in_queue,$theme->pending_join_queue))?>
@@ -64,7 +64,7 @@
     </h6>
    
     <?php else : ?>
-    <h6 class="fs12">
+    <h6 class="fs12 status-text">
     <?php if ($theme !== false  && $theme->noonline_operators != '') : ?>
     	   <?php echo htmlspecialchars($theme->noonline_operators)?>
     	<?php else : ?>
