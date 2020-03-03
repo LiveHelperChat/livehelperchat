551847c696f551729e989ca3a014cc0cef496b8d
Remigijus Kiminas <remdex@gmail.com>
2020-03-03 01:42:34 -0500

    Do not send same message twice

--- lhc_web/modules/lhchat/userclosechat.php
+++ lhc_web/modules/lhchat/userclosechat.php
@@ -27,30 +27,32 @@ if (is_object($chat) && $chat->hash == $Params['user_parameters']['hash'])
 		    
 		    if ($Params['user_parameters_unordered']['eclose'] == 't') {
 
-		        erLhcoreClassChat::lockDepartment($chat->dep_id, $db);
+                if ($chat->wait_time == 0) {
+                    $chat->wait_time = time() - ($chat->pnd_time > 0 ? $chat->pnd_time : $chat->time);
+                }
 
-    		    // From now chat will be closed explicitly	   
-    	        $chat->status_sub = erLhcoreClassModelChat::STATUS_SUB_USER_CLOSED_CHAT;
- 
-    	        if ($chat->wait_time == 0) {
-    	            $chat->wait_time = time() - ($chat->pnd_time > 0 ? $chat->pnd_time : $chat->time);
-    	        }
-    	        
-    	        $msg = new erLhcoreClassModelmsg();
-    	        $msg->msg = htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat','Visitor has closed the chat explicitly!'),ENT_QUOTES);;
-    	        $msg->chat_id = $chat->id;
-    	        $msg->user_id = -1;
-    	        $msg->time = time();
-    	    
-    	        erLhcoreClassChat::getSession()->save($msg);
-    	    
-    	        $chat->last_user_msg_time = $msg->time;
-    	    
-    	        // Set last message ID
-    	        if ($chat->last_msg_id < $msg->id) {
-    	            $chat->last_msg_id = $msg->id;
-    	        }
-    	        
+                if ($chat->status_sub != erLhcoreClassModelChat::STATUS_SUB_USER_CLOSED_CHAT) {
+                    erLhcoreClassChat::lockDepartment($chat->dep_id, $db);
+
+                    // From now chat will be closed explicitly
+                    $chat->status_sub = erLhcoreClassModelChat::STATUS_SUB_USER_CLOSED_CHAT;
+
+                    $msg = new erLhcoreClassModelmsg();
+                    $msg->msg = htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'Visitor has closed the chat explicitly!'), ENT_QUOTES);;
+                    $msg->chat_id = $chat->id;
+                    $msg->user_id = -1;
+                    $msg->time = time();
+
+                    erLhcoreClassChat::getSession()->save($msg);
+
+                    $chat->last_user_msg_time = $msg->time;
+
+                    // Set last message ID
+                    if ($chat->last_msg_id < $msg->id) {
+                        $chat->last_msg_id = $msg->id;
+                    }
+                }
+
     	        $explicitClosed = true;
 		    }
 
