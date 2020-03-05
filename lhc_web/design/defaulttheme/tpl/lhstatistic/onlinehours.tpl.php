02862500fa2bca64130cd9b48748718c9865e9d3
Remigijus Kiminas <remdex@gmail.com>
2020-03-05 08:26:52 -0500

    Show offline time if user filter is applied

--- lhc_web/design/defaulttheme/tpl/lhstatistic/onlinehours.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhstatistic/onlinehours.tpl.php
@@ -12,7 +12,19 @@
     <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/userlist','Duration');?></th>
 </tr>
 </thead>
+<?php $parentItem = null;?>
+
 <?php foreach ($items as $item) : ?>
+    <?php if (isset($input->user_id) && $input->user_id > 0 && is_object($parentItem)) : ?>
+        <tr>
+
+            <td colspan="4">
+            </td>
+            <td colspan="1">
+                <div class="text-danger" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/userlist','Was offline for');?>"><b><?php echo erLhcoreClassChat::formatSeconds($parentItem->time - $item->lactivity)?></b></div>
+            </td>
+        </tr>
+    <?php endif; ?>
     <tr>
         <td><?php echo $item->id?></td>
         <td><?php echo htmlspecialchars($item->user_name)?></td>
@@ -20,7 +32,7 @@
         <td><?php echo htmlspecialchars($item->lactivity_front)?></td>
         <td><?php echo $item->duration_front?></td>
     </tr>
-<?php endforeach; ?>
+<?php $parentItem = $item; endforeach; ?>
 </table>
 
 <?php include(erLhcoreClassDesign::designtpl('lhkernel/secure_links.tpl.php')); ?>
