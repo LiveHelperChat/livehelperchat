7ec6ae83094f0ec1e83442a41506f510ea831a2f
Remigijus Kiminas <remdex@gmail.com>
2020-03-31 08:32:08 -0400

    Increase version

--- lhc_web/design/defaulttheme/tpl/pagelayouts/userchat2.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/userchat2.php
@@ -21,9 +21,9 @@
 <?php
 $detect = new Mobile_Detect();
 if ($detect->version('IE') !== false) : ?>
-    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.ie.js')?>?t=43"></script>
+    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.ie.js')?>?t=44"></script>
 <?php else : ?>
-    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.js')?>?t=43"></script>
+    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.js')?>?t=44"></script>
 <?php endif; ?>
 </body>
 </html>
