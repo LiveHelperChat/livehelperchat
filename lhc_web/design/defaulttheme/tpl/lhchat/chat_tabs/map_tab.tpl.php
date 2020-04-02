5f3725d0f1d5e246524321e1ccad8fc32336c2a9
Remigijus Kiminas <remdex@gmail.com>
2020-04-02 08:55:37 -0400

    Changes
    * Lazy loading images for map tab
    * Examples for cors for new widget

--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/map_tab.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/map_tab.tpl.php
@@ -3,7 +3,14 @@
 <div role="tabpanel" class="tab-pane<?php if ($chatTabsOrderDefault == 'map_tab_tab') print ' active';?>" id="map-tab-chat-<?php echo $chat->id?>">
         <?php if ($chat->lat != 0 && $chat->lon) : ?>
             <?php $geo_location_data = erLhcoreClassModelChatConfig::fetch('geo_location_data')->data; ?>
-		    <a target="_blank" href="//maps.google.com/maps?t=h&q=<?php echo $chat->lat?>,<?php echo $chat->lon?>&z=17&hl=en&z=11&t=m"><img src="//maps.google.com/maps/api/staticmap?<?php if (erConfigClassLhConfig::getInstance()->getSetting( 'site', 'maps_api_key', false)) {echo 'key=' , erConfigClassLhConfig::getInstance()->getSetting( 'site', 'maps_api_key', false) , '&';} elseif (isset($geo_location_data['gmaps_api_key'])) {echo 'key=' ,$geo_location_data['gmaps_api_key'], '&';}?>zoom=13&size=400x300&maptype=roadmap&center=<?php echo $chat->lat?>,<?php echo $chat->lon?>&sensor=false&markers=color:green|<?php echo $chat->lat?>,<?php echo $chat->lon?>" alt="" title="<?php echo $chat->lat?>,<?php echo $chat->lon?>" /></a>
+		    <a target="_blank" href="//maps.google.com/maps?t=h&q=<?php echo $chat->lat?>,<?php echo $chat->lon?>&z=17&hl=en&z=11&t=m">
+                <img id="chat-map-img-<?php echo $chat->id?>" data-src="//maps.google.com/maps/api/staticmap?<?php if (erConfigClassLhConfig::getInstance()->getSetting( 'site', 'maps_api_key', false)) {echo 'key=' , erConfigClassLhConfig::getInstance()->getSetting( 'site', 'maps_api_key', false) , '&';} elseif (isset($geo_location_data['gmaps_api_key'])) {echo 'key=' ,$geo_location_data['gmaps_api_key'], '&';}?>zoom=13&size=400x300&maptype=roadmap&center=<?php echo $chat->lat?>,<?php echo $chat->lon?>&sensor=false&markers=color:green|<?php echo $chat->lat?>,<?php echo $chat->lon?>" src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg" alt="" title="<?php echo $chat->lat?>,<?php echo $chat->lon?>" />
+            </a>
+            <script>
+                $('#map-tab-chat-link-<?php echo $chat->id?>').click(function () {
+                    $('#chat-map-img-<?php echo $chat->id?>').attr('src',$('#chat-map-img-<?php echo $chat->id?>').attr('data-src'));
+                });
+            </script>
 		<?php else : ?>
 		    <p><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Could not detect. Make sure that GEO detection is enabled.')?></p>
 		<?php endif;?>
--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/map_tab_tab.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/map_tab_tab.tpl.php
@@ -1,4 +1,4 @@
 <?php include(erLhcoreClassDesign::designtpl('lhchat/chat_tabs/map_tab_tab_pre.tpl.php'));?>	
 <?php if ($information_tab_map_tab_tab_enabled == true) : ?>
-<li role="presentation" class="nav-item"><a class="nav-link <?php if ($chatTabsOrderDefault == 'map_tab_tab') print ' active';?>" href="#map-tab-chat-<?php echo $chat->id?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Map')?>" aria-controls="map-tab-chat-<?php echo $chat->id?>" role="tab" data-toggle="tab"><i class="material-icons mr-0">place</i></a></li>
+<li role="presentation" class="nav-item"><a class="nav-link <?php if ($chatTabsOrderDefault == 'map_tab_tab') print ' active';?>" href="#map-tab-chat-<?php echo $chat->id?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Map')?>" id="map-tab-chat-link-<?php echo $chat->id?>" aria-controls="map-tab-chat-<?php echo $chat->id?>" role="tab" data-toggle="tab"><i class="material-icons mr-0">place</i></a></li>
 <?php endif;?>
