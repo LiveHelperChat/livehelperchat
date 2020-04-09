600d435bb3a36d78724ee58a50753677a510ffb1
Remigijus Kiminas <remdex@gmail.com>
2020-04-09 02:07:17 -0400

    Increase version

--- lhc_web/lib/core/lhcore/lhupdate.php
+++ lhc_web/lib/core/lhcore/lhupdate.php
@@ -2,8 +2,8 @@
 
 class erLhcoreClassUpdate
 {
-	const DB_VERSION = 204;
-	const LHC_RELEASE = 329;
+	const DB_VERSION = 205;
+	const LHC_RELEASE = 330;
 
 	public static function doTablesUpdate($definition){
 		$updateInformation = self::getTablesStatus($definition);
