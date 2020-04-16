9992a17f4fc99f494fd35de82aa0e4c182c8cc79
Remigijus Kiminas <remdex@gmail.com>
2020-04-16 14:50:03 -0400

    Do not send referrer

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/page_footer.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/page_footer.tpl.php
@@ -1,6 +1,6 @@
 <div class="p-1 border-top">
-        <p class="float-right small"><a target="_blank" href="http://livehelperchat.com">Live Helper Chat &copy; <?php echo date('Y')?></a></p>
-        <p class="small"><a href="<?php echo erLhcoreClassModelChatConfig::fetch('customer_site_url')->current_value?>"><?php echo htmlspecialchars(erLhcoreClassModelChatConfig::fetch('customer_company_name')->current_value)?></a></p>
+        <p class="float-right small"><a target="_blank" rel="noreferrer" href="http://livehelperchat.com">Live Helper Chat &copy; <?php echo date('Y')?></a></p>
+        <p class="small"><a rel="noreferrer" href="<?php echo erLhcoreClassModelChatConfig::fetch('customer_site_url')->current_value?>"><?php echo htmlspecialchars(erLhcoreClassModelChatConfig::fetch('customer_company_name')->current_value)?></a></p>
 </div>
 
 <?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/page_footer_js.tpl.php'));?>
