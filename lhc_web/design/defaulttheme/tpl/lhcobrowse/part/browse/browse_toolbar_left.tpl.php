08eb474c5aaaa9981c550cf2557a8c6b7b684833
Remigijus Kiminas <remdex@gmail.com>
2020-04-28 03:05:25 -0400

    Adjust style co-browsing window

--- lhc_web/design/defaulttheme/tpl/lhcobrowse/part/browse/browse_toolbar_left.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhcobrowse/part/browse/browse_toolbar_left.tpl.php
@@ -1,10 +1,10 @@
 <div class="col-6">
     <div class="row">
         <div class="col-9">
-            <input class="form-control" id="awesomebar" name="url" value="<?php echo htmlspecialchars($browse->url)?>" type="text">
+            <input class="form-control form-control-sm" id="awesomebar" name="url" value="<?php echo htmlspecialchars($browse->url)?>" type="text">
         </div>
         <div class="col-3">
-            <a href="#" class="btn btn-primary btn-xs col-12" onclick="return lhinst.addRemoteCommand('<?php echo $chat->id?>','lhc_cobrowse:<?php echo $browse->chat_id?>_<?php echo $browse->chat->hash?>')"><i title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('cobrowse/browse','Request screen share')?>" class="material-icons">visibility</i></a>
+            <a href="#" class="btn btn-primary btn-sm col-12" onclick="return lhinst.addRemoteCommand('<?php echo $chat->id?>','lhc_cobrowse:<?php echo $browse->chat_id?>_<?php echo $browse->chat->hash?>')"><i title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('cobrowse/browse','Request screen share')?>" class="material-icons">visibility</i></a>
         </div>
     </div>
 </div>
