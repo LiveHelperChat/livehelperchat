0fa0c77ceda4db366670608210a4c052501a7691
Remigijus Kiminas <remdex@gmail.com>
2020-05-06 02:34:08 -0400

    Allow update by visitor only if we don't have this value

--- lhc_web/modules/lhwidgetrestapi/submitonline.php
+++ lhc_web/modules/lhwidgetrestapi/submitonline.php
@@ -197,9 +197,11 @@ if (empty($Errors)) {
 
                 if ($chat->nick != 'Visitor') {
                     $onlineAttr = $userInstance->online_attr_system_array;
-                    $onlineAttr['username'] = $chat->nick;
-                    $userInstance->online_attr_system = json_encode($onlineAttr);
-                    $userInstance->online_attr_system_array = $onlineAttr;
+                    if (!isset($onlineAttr['username'])) {
+                        $onlineAttr['username'] = $chat->nick;
+                        $userInstance->online_attr_system = json_encode($onlineAttr);
+                        $userInstance->online_attr_system_array = $onlineAttr;
+                    }
                 } elseif ($chat->nick == 'Visitor') {
                     if ($userInstance->nick && $userInstance->has_nick) {
                         $chat->nick = $userInstance->nick;
