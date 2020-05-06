f527e4f48b6b4f37fd7d7f66964574d4a86e5a94
Remigijus Kiminas <remdex@gmail.com>
2020-05-06 01:15:46 -0400

    Few small updates fix #1457
    By default browser forbids autoplay if it was not user direct action
    Added support for youtube iframes so you just paste youtube embed code direcly

--- lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/video.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/video.tpl.php
@@ -1,6 +1,10 @@
 <?php if (isset($metaMessage['payload']) && $metaMessage['payload'] != '') : ?>
 <br/>
 <div class="embed-responsive embed-responsive-16by9">
-    <video class="embed-responsive-item" <?php if (isset($metaMessage['video_options']['autoplay']) && $metaMessage['video_options']['autoplay'] == true) : ?>autoPlay<?php endif;?> <?php if (isset($metaMessage['video_options']['controls']) && $metaMessage['video_options']['controls'] == true) : ?>controls<?php endif;?>><source src=<?php echo htmlspecialchars($metaMessage['payload'])?> /></video>
+    <?php if (strpos($metaMessage['payload'],'<iframe') !== -1) : ?>
+        <?php echo $metaMessage['payload']?>
+    <?php else : ?>
+        <video class="embed-responsive-item" <?php if (isset($metaMessage['video_options']['autoplay']) && $metaMessage['video_options']['autoplay'] == true) : ?>autoPlay<?php endif;?> <?php if (isset($metaMessage['video_options']['controls']) && $metaMessage['video_options']['controls'] == true) : ?>controls<?php endif;?>><source src="<?php echo htmlspecialchars($metaMessage['payload'])?>" /></video>
+    <?php endif; ?>
 </div>
 <?php endif; ?>
