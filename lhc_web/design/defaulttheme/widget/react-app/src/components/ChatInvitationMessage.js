7dcab346b5ec4fea4c97ab0973ccf4ab21e936a7
Remigijus Kiminas <remdex@gmail.com>
2020-05-28 01:21:47 -0400

    Less is more

--- lhc_web/design/defaulttheme/widget/react-app/src/components/ChatInvitationMessage.js
+++ lhc_web/design/defaulttheme/widget/react-app/src/components/ChatInvitationMessage.js
@@ -8,13 +8,7 @@ class ChatInvitationMessage extends PureComponent {
 
     render() {
 
-        let classProfile = "operator-info d-flex mb10 round-profile";
-
-        if (this.props.mode == 'profile_only') {
-            classProfile +=" mt-1 ml-1 mr-1";
-        } else {
-            classProfile +=" mt-2";
-        }
+        let classProfile = "operator-info d-flex border-bottom p-2";
 
         if (this.props.mode == 'message') {
             return (
@@ -22,7 +16,7 @@ class ChatInvitationMessage extends PureComponent {
                     <span className="usr-tit op-tit">
                          {this.props.invitation.bubble && <i title={this.props.invitation.name_support || this.props.invitation.extra_profile} className="chat-operators mi-fs15 mr-0">
                              {this.props.invitation.photo && <img src={this.props.invitation.photo} alt="" className="profile-msg-pic" />}
-                             {!this.props.invitation.photo && <i className="icon-assistant material-icons">account_box</i>}
+                             {!this.props.invitation.photo && <i className="icon-assistant material-icons mr-0">account_box</i>}
                          </i>}
                         {!this.props.invitation.bubble && (this.props.invitation.name_support || this.props.invitation.extra_profile)}
                      </span>
@@ -35,9 +29,9 @@ class ChatInvitationMessage extends PureComponent {
                     <div className={classProfile}>
                          <div>
                              {this.props.invitation.photo && <img src={this.props.invitation.photo} title={this.props.invitation.photo_title} alt=""/>}
-                             {!this.props.invitation.photo && <i className="icon-assistant material-icons">account_box</i>}
+                             {!this.props.invitation.photo && <i className="icon-assistant material-icons mr-0">account_box</i>}
                          </div>
-                         <div className="p-1 w-100">
+                         <div className="p-1 pl-2 w-100">
                              <div>
                                  <strong>{this.props.invitation.name_support || this.props.invitation.extra_profile}<br/></strong>
                                  {this.props.invitation.name_support && <span><i>{this.props.invitation.extra_profile}</i></span>}
