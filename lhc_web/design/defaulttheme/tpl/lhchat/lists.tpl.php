3e6622729a20d7054ef43a9411dff3794afedc88
Remigijus Kiminas <remdex@gmail.com>
2020-06-01 09:37:26 -0400

    Bot chat icon

--- lhc_web/design/defaulttheme/tpl/lhchat/lists.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/lists.tpl.php
@@ -84,6 +84,8 @@
                                      <i class="material-icons chat-active">chat</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/pendingchats','Chatbox chat');?>
                                 <?php elseif ($chat->status == erLhcoreClassModelChat::STATUS_OPERATORS_CHAT) : ?>
                                      <i class="material-icons chat-active">face</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/pendingchats','Operators chat');?>
+                                <?php elseif ($chat->status == erLhcoreClassModelChat::STATUS_BOT_CHAT) : ?>
+                                     <i class="material-icons chat-active">chat</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/pendingchats','Bot chat');?>
                                 <?php endif;?>
                                     <?php include(erLhcoreClassDesign::designtpl('lhchat/lists_chats_parts/status_multiinclude.tpl.php'));?>
                             </td>
