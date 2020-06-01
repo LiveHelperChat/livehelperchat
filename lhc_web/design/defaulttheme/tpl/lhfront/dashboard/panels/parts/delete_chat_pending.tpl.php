44a804c0a50e86b53505177e9d19beb189880ebf
Remigijus Kiminas <remdex@gmail.com>
2020-06-01 03:32:15 -0400

    fix #1476

--- lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/parts/delete_chat_pending.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/parts/delete_chat_pending.tpl.php
@@ -1,1 +1,3 @@
-<a ng-show="chat.can_edit_chat" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Delete chat')?>" class="material-icons float-right" ng-click="lhc.deleteChat(chat.id)">delete</a>
+<?php if (erLhcoreClassUser::instance()->hasAccessTo('lhchat','deletechat')) : ?>
+<a title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Delete chat')?>" class="material-icons float-right" ng-click="lhc.deleteChat(chat.id)">delete</a>
+<?php endif; ?>
