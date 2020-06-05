fc96e76377115e1cc1bb6fde5cd62d69b925adc4
Remigijus Kiminas <remdex@gmail.com>
2020-06-05 05:56:30 -0400

    Cleanup

--- lhc_web/modules/lhwidgetrestapi/settings.php
+++ lhc_web/modules/lhwidgetrestapi/settings.php
@@ -152,10 +152,6 @@ if (isset($outputResponse['theme'])){
         if (isset($theme->bot_configuration_array['header_html']) && $theme->bot_configuration_array['header_html'] != '') {
             $outputResponse['chat_ui']['hhtml'] = $theme->bot_configuration_array['header_html'];
         }
-        
-        /*(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-KPFF73Q');
-
-        inject_html*/
 
     }
 }
