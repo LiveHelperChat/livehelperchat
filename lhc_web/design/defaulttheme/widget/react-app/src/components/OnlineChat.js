f2c4d4e96e89c2839daa1ab3bedea3b4bbdbb535
Remigijus Kiminas <remdex@gmail.com>
2020-06-15 09:23:46 -0400

    Avoid flickers on embed mode

--- lhc_web/design/defaulttheme/widget/react-app/src/components/OnlineChat.js
+++ lhc_web/design/defaulttheme/widget/react-app/src/components/OnlineChat.js
@@ -511,12 +511,20 @@ class OnlineChat extends Component {
     render() {
 
         if (this.props.chatwidget.get('initLoaded') === false || this.props.chatwidget.get('msgLoaded') === false) {
+
+                var msg_expand = "flex-grow-1 overflow-scroll position-relative";
+
+                if (this.props.chatwidget.hasIn(['chat_ui','msg_expand'])) {
+                    msg_expand = "overflow-scroll position-relative";
+                }
+
                 return (<React.Fragment>
                     {this.props.profileBefore !== null && <div dangerouslySetInnerHTML={{__html:this.props.profileBefore}}></div>}
-                    {this.props.messagesBefore !== null && <div className="flex-grow-1 overflow-scroll position-relative" id="messagesBlock" dangerouslySetInnerHTML={{__html:this.props.messagesBefore}}></div>}
+                    {this.props.messagesBefore !== null && <div className={msg_expand} id="messagesBlock" dangerouslySetInnerHTML={{__html:this.props.messagesBefore}}></div>}
                     <div className="d-flex flex-row border-top position-relative message-send-area" >
+                        <div className="btn-group dropup disable-select pl-2 pt-2"><i className="material-icons settings text-muted" id="chat-dropdown-options" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">settings</i></div>
                         <div className="mx-auto pb-1 w-100">
-                            <textarea aria-label="Type your message here..." rows="1" className="pl-0 no-outline form-control rounded-0 form-control border-left-0 border-right-0 border-0" />
+                            <textarea aria-label="Type your message here..." id="CSChatMessage" rows="1" className="pl-0 no-outline form-control rounded-0 form-control border-left-0 border-right-0 border-0" />
                         </div>
                     </div>
                 </React.Fragment>);
