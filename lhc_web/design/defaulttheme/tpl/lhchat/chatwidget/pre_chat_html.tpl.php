bc408f7a8516e5d482caee821299d082e469b58c
Remigijus Kiminas <remdex@gmail.com>
2020-06-26 03:04:08 -0400

    Why to hide chat fields if pre html is used?

--- lhc_web/design/defaulttheme/tpl/lhchat/chatwidget/pre_chat_html.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chatwidget/pre_chat_html.tpl.php
@@ -1,2 +1,2 @@
 <?php echo $start_data_fields['pre_chat_html']?>
-<div class="hide" id="post-chat-content">
+<div id="post-chat-content">
--- lhc_web/design/defaulttheme/tpl/lhchat/chatwidget/pre_offline_chat_html.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chatwidget/pre_offline_chat_html.tpl.php
@@ -1,2 +1,2 @@
 <?php echo $start_data_fields['pre_offline_chat_html']?>
-<div class="hide" id="post-chat-content">
+<div id="post-chat-content">
