9592afc1ddf4383f59fc6f0e683923a53625ae38
Remigijus Kiminas <remdex@gmail.com>
2020-07-24 08:35:26 -0400

    If permission to upload files are removed we have to update UI also

--- lhc_web/design/defaulttheme/tpl/pagelayouts/userchat2.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/userchat2.php
@@ -24,9 +24,9 @@
 <?php
 $detect = new Mobile_Detect();
 if ($detect->version('IE') !== false) : ?>
-    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.ie.js')?>?t=89"></script>
+    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.ie.js')?>?t=90"></script>
 <?php else : ?>
-    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.js')?>?t=89"></script>
+    <script src="<?php echo erLhcoreClassDesign::design('js/widgetv2/react.app.js')?>?t=90"></script>
 <?php endif; ?>
 
 <?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/page_footer_widget2_js_extension_multiinclude.tpl.php'));?>
