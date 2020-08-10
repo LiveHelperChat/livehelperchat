d859c12af08c7a7ab9deba1d5024f16a182779db
Remigijus Kiminas <remdex@gmail.com>
2020-08-10 08:31:47 -0400

    Show some meta messages for operators also

--- lhc_web/design/defaulttheme/tpl/lhchat/lists/msg_obj_list_admin.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/lists/msg_obj_list_admin.tpl.php
@@ -15,15 +15,25 @@ if ($lastOperatorId !== false && ($lastOperatorId != $msg->user_id || $lastOpera
 
 $lastOperatorId = $msg->user_id;
 
+    if ($msg->meta_msg != '') {
+        $metaMessageData = json_decode($msg->meta_msg, true); $messageId = $msg->id;
+    } else if (isset($metaMessageData)) {
+        unset($metaMessageData);
+    }
 
 ?>
 <?php if ($msg->user_id == -1) : ?>
 	<div class="message-row system-response" id="msg-<?php echo $msg->id?>"><div class="msg-date"><?php echo date(erLhcoreClassModule::$dateDateHourFormat,$msg->time);?></div><i><span class="usr-tit sys-tit"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmin','System assistant')?></span><?php echo erLhcoreClassBBCode::make_clickable(htmlspecialchars($msg->msg))?></i></div>
 <?php else : ?>
-    <?php if ($msg->msg != '') : ?>
+    <?php if ($msg->msg != '' || isset($metaMessageData['content']['text_conditional'])) : ?>
         <div class="message-row<?php echo $msg->user_id == 0 ? ' response' : ' message-admin'.($lastOperatorChanged == true ? ' operator-changes' : '')?>" id="msg-<?php echo $msg->id?>"><div class="msg-date"><?php echo date(erLhcoreClassModule::$dateDateHourFormat,$msg->time);?></div><span class="usr-tit<?php echo $msg->user_id == 0 ? ' vis-tit' : ' op-tit'?>"><?php if ($msg->user_id == 0) : ?><i class="material-icons"><?php echo ($chat->device_type == 0 ? 'computer' : ($chat->device_type == 1 ? 'smartphone' : 'tablet'))?></i><?php endif;?><?php echo $msg->user_id == 0 ? htmlspecialchars($chat->nick) : htmlspecialchars($msg->name_support) ?></span>
             <?php $msgBody = $msg->msg; $paramsMessageRender = array('sender' => $msg->user_id);?>
             <?php include(erLhcoreClassDesign::designtpl('lhchat/lists/msg_body.tpl.php'));?>
+
+            <?php if (isset($metaMessageData)) : ?>
+                <?php include(erLhcoreClassDesign::designtpl('lhgenericbot/message/meta_render_admin.tpl.php'));?>
+            <?php endif; ?>
+            
         </div>
     <?php endif;?>
 <?php endif;?>
