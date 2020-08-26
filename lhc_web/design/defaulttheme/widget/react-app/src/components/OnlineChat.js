5c5d7b965bcb00454040a69d9853f83134903442
Remigijus Kiminas <remdex@gmail.com>
2020-08-26 04:48:33 -0400

    cleanup

--- lhc_web/design/defaulttheme/widget/react-app/src/components/OnlineChat.js
+++ lhc_web/design/defaulttheme/widget/react-app/src/components/OnlineChat.js
@@ -142,10 +142,7 @@ class OnlineChat extends Component {
             if (elmtmp !== null) {
                 document.body.removeChild(elmtmp);
             }
-        } else {
-            console.log('element not found');
         }
-
     }
 
     focusMessage() {
@@ -557,24 +554,6 @@ class OnlineChat extends Component {
                 }
 
                 return <ChatIntroStatus profileBefore={this.props.profileBefore} msg_expand={msg_expand} messagesBefore={this.props.messagesBefore} placeholderMessage={this.props.chatwidget.hasIn(['chat_ui','placeholder_message']) ? this.props.chatwidget.getIn(['chat_ui','placeholder_message']) : t('chat.type_here')} />;
-
-                /*return (<React.Fragment>
-                    {this.props.profileBefore !== null && <div dangerouslySetInnerHTML={{__html:this.props.profileBefore}}></div>}
-                    <div className={msg_expand} id="messagesBlock" dangerouslySetInnerHTML={{__html:this.props.messagesBefore}}></div>
-
-                    {!this.props.hideMessageField && <div className="d-flex flex-row border-top position-relative message-send-area">
-                        <div className="btn-group dropup disable-select pl-2 pt-2"><i className="material-icons settings text-muted" aria-haspopup="true" aria-expanded="false">&#xf100;</i></div>
-                        <div className="mx-auto pb-1 w-100">
-                            <textarea aria-label="Type your message here..." placeholder={this.props.chatwidget.hasIn(['chat_ui','placeholder_message']) ? this.props.chatwidget.getIn(['chat_ui','placeholder_message']) : t('chat.type_here')} id="CSChatMessage" rows="1" className="pl-0 no-outline form-control rounded-0 form-control border-left-0 border-right-0 border-0" />
-                        </div>
-                        <div className="disable-select">
-                            <div className="user-chatwidget-buttons pt-1" id="ChatSendButtonContainer">
-                               <i className="material-icons text-muted settings mr-0">&#xf113;</i>
-                            </div>
-                        </div>
-                    </div>}
-
-                </React.Fragment>);*/
         }
         
         if (this.props.chatwidget.hasIn(['chatLiveData','ru']) && this.props.chatwidget.getIn(['chatLiveData','ru'])) {
