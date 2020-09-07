4a174b0ebbe16ff3343fc505e90898c29d5dd29d
Remigijus Kiminas <remdex@gmail.com>
2020-09-07 03:13:48 -0400

    Avoid notice error for mobile app request untill we migrate to propert Rest API call

--- lhc_web/lib/core/lhcore/lhmodule.php
+++ lhc_web/lib/core/lhcore/lhmodule.php
@@ -217,6 +217,10 @@ class erLhcoreClassModule{
             erLhcoreClassLog::write('Unexpected warning, the message was : ' . $errstr . ' in ' . $errfile . ' on line ' . $errline);
         }
 
+        if ($errno == E_NOTICE || $errno == E_STRICT) {
+            erLhcoreClassLog::write('Unexpected notice, the message was : ' . $errstr . ' in ' . $errfile . ' on line ' . $errline);
+        }
+        
         return false;
     }
 
