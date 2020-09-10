66a7c4a49b91fcc1212bf49fe00ad741970e796b
Remigijus Kiminas <remdex@gmail.com>
2020-09-10 15:02:32 -0400

    Fix path futher

--- lhc_web/lib/core/lhcore/lhdesign.php
+++ lhc_web/lib/core/lhcore/lhdesign.php
@@ -371,7 +371,7 @@ class erLhcoreClassDesign
             ));
 
         if (self::$buildMode == true) {
-            file_put_contents($filenameStaticPath, str_replace(['\'/design/','\'/extension/'],['\'../../../../design/','\'../../../../extension/'],$filesToCompress));
+            file_put_contents($filenameStaticPath, str_replace(['\'/design/','\'/extension/','url(/design/'],['\'../../../../design/','\'../../../../extension/','url(../../../../design/'],$filesToCompress));
         }
         
         return $instance->wwwDir() . '/cache/compiledtemplates/' . $fileName . '.css';
