2395ac1c723afa81c1d3bf8316d7e997fd368ce5
Remigijus Kiminas <remdex@gmail.com>
2020-09-15 01:54:37 -0400

    Allow multiple messages sending while another in process

--- lhc_web/design/defaulttheme/js/lh.js
+++ lhc_web/design/defaulttheme/js/lh.js
@@ -1923,8 +1923,9 @@ function lh(){
 	        	                	  var scrollHeight = messageBlock.prop("scrollHeight");
 	        	                	  var isAtTheBottom = Math.abs((scrollHeight - messageBlock.prop("scrollTop")) - messageBlock.prop("clientHeight"));
 
-	        	                	  messageBlock.find('.pending-storage').remove();
+	        	                	  messageBlock.find('.pending-storage').first().remove();
 	        	                	  messageBlock.append(item.content);
+                                      messageBlock.find('.pending-storage').appendTo(messageBlock);
 
 	        	                	  lhinst.addQuateHandler(item.chat_id);
 
@@ -2277,7 +2278,7 @@ function lh(){
 	this.addUserMessageQueue = [];
 	this.addDelayedTimeout = null;
 
-	this.addmsgadmin = function (chat_id)
+	this.addmsgadmin = function (chat_id, message)
 	{
 		var textArea = $("#CSChatMessage-"+chat_id);
 
@@ -2286,26 +2287,30 @@ function lh(){
 		}
 
 		var pdata = {
-				msg	: textArea.val()
+				msg	: message || textArea.val()
 		};
+
+		if (pdata.msg == '') {
+		    return;
+        }
 
 		if (this.speechHandler !== false) {
 			this.speechHandler.messageSend();
 		};
 
-		textArea.val('');
+        message || textArea.val('');
+
 		var placeholerOriginal = textArea.attr('placeholder');
 
         textArea.attr('placeholder',confLH.transLation.sending || 'Sending...');
-        textArea.attr('readonly','readonly');
-        
+
 		if (textArea.hasClass('edit-mode')) {
 
 			pdata.msgid = textArea.attr('data-msgid');
 
 			$.postJSON(this.wwwDir + 'chat/updatemsg/' + chat_id, pdata , function(data){
 
-			    textArea.removeAttr('readonly').attr('placeholder',placeholerOriginal);
+			    textArea.attr('placeholder',placeholerOriginal);
 
 				if (data.error == 'f') {
 					textArea.removeClass('edit-mode');
@@ -2328,11 +2333,11 @@ function lh(){
 
 			var messagesBlock = $('#messagesBlock-'+chat_id);
 
-            messagesBlock.append("<div class=\"message-row message-admin pending-storage\"><div class=\"msg-body\">" + $("<div>").text(pdata.msg).html() + "</div></div>");
+            message || messagesBlock.append("<div class=\"message-row message-admin pending-storage\"><div class=\"msg-body\"><span class=\"material-icons lhc-spin\">autorenew</span>" + $("<div>").text(pdata.msg).html() + "</div></div>");
 
 			messagesBlock.stop(true,false).animate({ scrollTop: messagesBlock.prop('scrollHeight') }, 500);
 
-			if (this.addingUserMessage == false && this.addUserMessageQueue.length == 0)
+			if (this.addingUserMessage == false)
 			{
 				this.addingUserMessage = true;
 
@@ -2359,92 +2364,40 @@ function lh(){
 
                         lhinst.syncadmincall();
                     } else {
-                        textArea.removeAttr('readonly').attr('placeholder',placeholerOriginal).val(pdata.msg);
-                        $('.pending-storage').remove();
-                        var escaped = '<div style="margin:10px 10px 30px 10px;" class="alert alert-warning pending-storage" role="alert">' + $("<div>").text(data.r).html() + '</div>';
+                        textArea.attr('placeholder',placeholerOriginal).val(textArea.val() + ' ' + pdata.msg);
+                        $('.pending-storage').first().remove();
+                        var escaped = '<div style="margin:10px 10px 30px 10px;" class="alert alert-warning" role="alert">' + $("<div>").text(data.r).html() + '</div>';
                         $('#messagesBlock-'+chat_id).append(escaped);
                         $('#messagesBlock-'+chat_id).animate({ scrollTop: $("#messagesBlock-"+chat_id).prop("scrollHeight") }, 500);
                     }
 
 					inst.addingUserMessage = false;
 
+                    if (inst.addUserMessageQueue.length > 0) {
+                        var elementAdd = inst.addUserMessageQueue.shift()
+                        inst.addmsgadmin(elementAdd.chat_id,elementAdd.msg);
+                    }
+
 					return true;
 				}).fail(function(respose) {
-                    textArea.removeAttr('readonly').attr('placeholder',placeholerOriginal).val(pdata.msg);
+                    textArea.attr('placeholder',placeholerOriginal).val(textArea.val() + ' ' + pdata.msg);
                     var escaped = '<div style="margin:10px 10px 30px 10px;" class="alert alert-warning" role="alert">' + $("<div>").text('You have weak internet connection or the server has problems. Try to refresh the page or send the message again.' + (typeof respose.status !== 'undefined' ? ' Error code ['+respose.status+']' : '') + (typeof respose.responseText !== 'undefined' ? respose.responseText : '')).html() + '</div>';
                     $('#messagesBlock-'+chat_id).append(escaped);
                     $('#messagesBlock-'+chat_id).animate({ scrollTop: $("#messagesBlock-"+chat_id).prop("scrollHeight") }, 500);
-                    $('.pending-storage').remove();
+                    $('.pending-storage').first().remove();
                     inst.addingUserMessage = false;
+                    if (inst.addUserMessageQueue.length > 0) {
+                        var elementAdd = inst.addUserMessageQueue.shift()
+                        inst.addmsgadmin(elementAdd.chat_id,elementAdd.msg);
+                    }
 		    	});
 
 			} else {
-                textArea.removeAttr('readonly').attr('placeholder', placeholerOriginal).val(pdata.msg);
+                textArea.attr('placeholder', placeholerOriginal);
+                this.addUserMessageQueue.push({'chat_id':chat_id,'msg':pdata.msg});
 			}
 		}
 	};
-
-	
-	this.addDelayedMessageAdmin = function()
-    {
-    	var inst = this;
-
-    	if (this.addingUserMessage == false) {
-
-    		if (this.addUserMessageQueue.length > 0)
-    		{
-	    		var elementAdd = this.addUserMessageQueue.shift();
-	    		this.addingUserMessage = true;
-
-                elementAdd.retries = elementAdd.retries + 1;
-
-		        $.postJSON(elementAdd.url, elementAdd.pdata , function(data) {
-
-		        	if (LHCCallbacks.addmsgadmin) {
-		        		LHCCallbacks.addmsgadmin(elementAdd.chat_id);
-		        	};
-
-		        	ee.emitEvent('chatAddMsgAdmin', [elementAdd.chat_id]);
-
-		        	if (data.r != '') {
-	            		$('#messagesBlock-'+elementAdd.chat_id).append(data.r);
-		                $('#messagesBlock-'+elementAdd.chat_id).animate({ scrollTop: $("#messagesBlock-"+elementAdd.chat_id).prop("scrollHeight") }, 500);
-	            	};
-
-	            	lhinst.syncadmincall();
-
-		        	inst.addingUserMessage = false;
-
-		        	// There is still pending messages, add them
-		        	if (inst.addUserMessageQueue.length > 0) {
-		        		clearTimeout(inst.addDelayedTimeout);
-		            	inst.addDelayedMessageAdmin();
-		        	}
-
-				}).fail(function(respose) {
-
-                    var escaped = '<div style="margin:10px 10px 30px 10px;" class="alert alert-warning" role="alert">' + $("<div>").text('You have weak internet connection or the server has problems. Try to refresh the page.' + (typeof respose.status !== 'undefined' ? ' Error code ['+respose.status+']' : '') + (typeof respose.responseText !== 'undefined' ? respose.responseText : '')).html() + '</div>';
-                    $('#messagesBlock-'+elementAdd.chat_id).append(escaped);
-
-                    if (elementAdd.retries < 2) {
-                        inst.addUserMessageQueue.unshift(elementAdd);
-                        inst.addingUserMessage = false;
-                        clearTimeout(inst.addDelayedTimeout);
-                        inst.addDelayedTimeout = setTimeout(function(){
-                            inst.addDelayedMessageAdmin();
-                        }, 500);
-                    }
-
-				});
-    		}
-
-    	} else {
-    		clearTimeout(this.addDelayedTimeout);
-        	this.addDelayedTimeout = setTimeout(function(){
-        		inst.addDelayedMessageAdmin();
-        	},50);
-    	}
-    }
 
 	this.editPrevious = function(chat_id) {
 		var textArea = $('#CSChatMessage-'+chat_id);
