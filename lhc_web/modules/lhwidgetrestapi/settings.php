910b75217a6a8d553b7782b7d759637dc99e9877
Remigijus Kiminas <remdex@gmail.com>
2020-09-18 06:11:00 -0400

    Close on chosen emoji

--- lhc_web/modules/lhwidgetrestapi/settings.php
+++ lhc_web/modules/lhwidgetrestapi/settings.php
@@ -295,7 +295,7 @@ if (isset($startDataFields['lazy_load']) && $startDataFields['lazy_load'] == tru
 }
 
 $ts = time();
-$outputResponse['v'] = 118;
+$outputResponse['v'] = 119;
 $outputResponse['hash'] = sha1(erLhcoreClassIPDetect::getIP() . $ts . erConfigClassLhConfig::getInstance()->getSetting( 'site', 'secrethash' ));
 $outputResponse['hash_ts'] = $ts;
 
