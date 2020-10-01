998906f2f82489d07137410e055cb468d9c7b1b7
Remigijus Kiminas <remdex@gmail.com>
2020-09-30 23:42:47 -0400

    Cleanup test file

--- lhc_web/modules/lhcron/test.php
+++ lhc_web/modules/lhcron/test.php
@@ -5,27 +5,4 @@
  * For various testing purposes
  *
  * */
-/*'found' => true,
-            'typos_used' => 0,*/
-
-
-
-$lhcinsultOptions = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChatConfig', 'lhcinsult_options' );
-$data = (array)$lhcinsultOptions->data;
-
-if (isset($data['safe_comb']) && trim($data['safe_comb']) != '') {
-    $rulesCheck = explode("\n",trim(str_replace(array("\r\n"),"\n",$data['safe_comb'])));
-    foreach ($rulesCheck as $ruleCheck) {
-        $presenceOutcome = erLhcoreClassGenericBotWorkflow::checkPresenceMessage(array(
-            'pattern' => $ruleCheck,
-            'msg' => mb_strtolower('please close my duper'),
-        ));
-        // check is it safe combination
-        if ($presenceOutcome['found']) {
-            echo "asdasd";
-        }
-    }
-}
-
-
 ?>
