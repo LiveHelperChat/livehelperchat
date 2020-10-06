febeacc66c3b369c136dabfd24fa1540117a095c
Remigijus Kiminas <remdex@gmail.com>
2020-10-06 01:20:05 -0400

    Make sure chat exists

--- lhc_web/modules/lhchat/single.php
+++ lhc_web/modules/lhchat/single.php
@@ -4,15 +4,18 @@ $tpl = erLhcoreClassTemplate::getInstance('lhchat/single.tpl.php');
 
 if (is_numeric($Params['user_parameters']['chat_id']))
 {
-    $chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params['user_parameters']['chat_id']);
+    try {
+        $chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params['user_parameters']['chat_id']);
 
-    if ( erLhcoreClassChat::hasAccessToRead($chat) )
-    {
-         $tpl->set('chat_to_load',$chat);
-    } else {
-         $tpl->setFile('lhchat/errors/adminchatnopermission.tpl.php');
+        if ( erLhcoreClassChat::hasAccessToRead($chat) )
+        {
+            $tpl->set('chat_to_load',$chat);
+        } else {
+            $tpl->setFile('lhchat/errors/adminchatnopermission.tpl.php');
+        }
+    } catch (Exception $e) {
+        $tpl->setFile('lhchat/errors/adminchatnopermission.tpl.php');
     }
-
 }
 $tpl->set('chat_id',$Params['user_parameters']['chat_id']);
 
