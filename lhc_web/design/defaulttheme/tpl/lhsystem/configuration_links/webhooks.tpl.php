ad80107a038bda015f7958d2af28b0e763482fc7
Remigijus Kiminas <remdex@gmail.com>
2020-10-12 05:49:32 -0400

    Change title

--- lhc_web/design/defaulttheme/tpl/lhsystem/configuration_links/webhooks.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhsystem/configuration_links/webhooks.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($currentUser->hasAccessTo('lhwebhooks','configuration')) : ?>
-    <h5><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/configuration','Group chat');?></h5>
+    <h5><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/configuration','Webhooks');?></h5>
     <ul>
         <li><a href="<?php echo erLhcoreClassDesign::baseurl('webhooks/configuration')?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/configuration','Webhooks');?></a></li>
     </ul>
