4e72f3d081332c9f10ad9ffa8f04a36ff6b68bdc
Remigijus Kiminas <remdex@gmail.com>
2020-11-10 10:11:57 -0500

    Logging javascritp errors is turned off by default

--- lhc_web/modules/lhaudit/logjserror.php
+++ lhc_web/modules/lhaudit/logjserror.php
@@ -1,18 +1,22 @@
 <?php
 
 if (isset($_POST['data'])) {
-    $dataLog = json_decode($_POST['data'], true);
+    $auditOptions = erLhcoreClassModelChatConfig::fetch('audit_configuration');
+    $data = (array)$auditOptions->data;
 
-    erLhcoreClassLog::write(trim($dataLog['message'] . "\n" . json_decode($dataLog['stack'],true)),
-        ezcLog::SUCCESS_AUDIT,
-        array(
-            'source' => 'lhc',
-            'category' => 'js',
-            'line' => (int)$dataLog['line'],
-            'file' => $dataLog['file'],
-            'object_id' => isset($dataLog['column']) ? (int)$dataLog['column'] : 0
-        )
-    );
+    if (isset($data['log_js']) && $data['log_js'] == 1) {
+        $dataLog = json_decode($_POST['data'], true);
+        erLhcoreClassLog::write(trim($dataLog['message'] . "\n" . json_decode($dataLog['stack'],true)),
+            ezcLog::SUCCESS_AUDIT,
+            array(
+                'source' => 'lhc',
+                'category' => 'js',
+                'line' => (int)$dataLog['line'],
+                'file' => $dataLog['file'],
+                'object_id' => isset($dataLog['column']) ? (int)$dataLog['column'] : 0
+            )
+        );
+    }
 }
 
 exit;
