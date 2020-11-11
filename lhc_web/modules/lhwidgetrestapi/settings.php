62365155ad315f5360a1bf55bb58a2795f3e22cc
Remigijus Kiminas <remdex@gmail.com>
2020-11-11 01:17:36 -0500

    Increase version

--- lhc_web/modules/lhwidgetrestapi/settings.php
+++ lhc_web/modules/lhwidgetrestapi/settings.php
@@ -325,7 +325,7 @@ if (isset($startDataFields['lazy_load']) && $startDataFields['lazy_load'] == tru
 }
 
 $ts = time();
-$outputResponse['v'] = 130;
+$outputResponse['v'] = 131;
 $outputResponse['hash'] = sha1(erLhcoreClassIPDetect::getIP() . $ts . erConfigClassLhConfig::getInstance()->getSetting( 'site', 'secrethash' ));
 $outputResponse['hash_ts'] = $ts;
 
