796fb4407865f4ab8214ff930d969bc82a723564
Remigijus Kiminas <remdex@gmail.com>
2020-11-12 05:06:23 -0500

    Log IP and referrer in error log

--- lhc_web/modules/lhaudit/logjserror.php
+++ lhc_web/modules/lhaudit/logjserror.php
@@ -8,7 +8,15 @@ if (isset($_POST['data'])) {
 
     if (isset($data['log_js']) && $data['log_js'] == 1) {
         $dataLog = json_decode($_POST['data'], true);
-        erLhcoreClassLog::write(trim((isset($dataLog['message']) ? $dataLog['message'] : '') . "\n" . json_decode($dataLog['stack'],true)),
+
+        $referrer = '';
+        if (isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])) {
+            $referrer .= $_SERVER['HTTP_REFERER'] . "\n";
+        }
+        
+        $messageLog = erLhcoreClassIPDetect::getIP() . "\n" . trim((isset($dataLog['message']) ? $dataLog['message'] : '') . "\n" . json_decode($dataLog['stack'],true));
+
+        erLhcoreClassLog::write($messageLog,
             ezcLog::SUCCESS_AUDIT,
             array(
                 'source' => 'lhc',
