55d06ae87db63f15d0042114559c64fd39af04e8
Remigijus Kiminas <remdex@gmail.com>
2020-11-30 06:10:18 -0500

    Last message time in chat

--- lhc_web/lib/models/lhchat/erlhcoreclassmodelchat.php
+++ lhc_web/lib/models/lhchat/erlhcoreclassmodelchat.php
@@ -269,6 +269,10 @@ class erLhcoreClassModelChat {
        		   $this->wait_time_seconds = time() - max($this->time,$this->pnd_time);
        		   return $this->wait_time_seconds;
 
+       case 'last_msg_time':
+            $this->last_msg_time = max($this->last_user_msg_time, $this->last_op_msg_time);
+            return $this->last_msg_time;
+
        	case 'wait_time_front':
        		   $this->wait_time_front = erLhcoreClassChat::formatSeconds($this->wait_time);
        		   return $this->wait_time_front;
