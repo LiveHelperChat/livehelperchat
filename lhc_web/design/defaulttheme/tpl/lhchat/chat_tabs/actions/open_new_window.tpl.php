1b18c89bbc9f75131d2b2fea45cdfba257ea3fd1
Remigijus Kiminas <remdex@gmail.com>
2021-01-12 09:14:38 -0500

    Allwo control visibility for pritn and new window icons

--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/actions/open_new_window.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/actions/open_new_window.tpl.php
@@ -1,5 +1,7 @@
+<?php if (isset($orderChatButtons['open_new_window']) && $orderChatButtons['open_new_window']['enabled'] == 1) : ?>
 <div class="col-6 pb-1">
 <a class="text-muted" data-title="<?php echo htmlspecialchars($chat->nick,ENT_QUOTES);?>" onclick="lhinst.startChatCloseTabNewWindow('<?php echo $chat->id;?>',$('#tabs'),$(this).attr('data-title'))">
 <span class="material-icons">open_in_new</span><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','New window');?>
 </a>
 </div>
+<?php endif; ?>
