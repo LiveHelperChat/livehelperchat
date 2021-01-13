ceecfcd057041d48df66bb5868e633a112643bf6
Remigijus Kiminas <remdex@gmail.com>
2021-01-13 08:07:22 -0500

    Update city

--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/information_rows/city.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/information_rows/city.tpl.php
@@ -1,5 +1,5 @@
 <?php if (isset($orderInformation['city']['enabled']) && $orderInformation['city']['enabled'] == true && !empty($chat->city) ) : ?>
 <div class="col-6 pb-1">
-    <?php echo htmlspecialchars($chat->city);?>
+    <span class="material-icons">location_city</span><?php echo htmlspecialchars($chat->city);?>
 </div>
 <?php endif;?>
