ec537ffce03a4e1ff1d9bae8fb3fd2233f51f06b
Remigijus Kiminas <remdex@gmail.com>
2021-02-17 04:04:36 -0500

    Give more space for operator name

--- lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/bodies/online_operators.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/bodies/online_operators.tpl.php
@@ -1,14 +1,14 @@
 <table class="table table-sm mb-0 table-small table-fixed">
     <thead>
     <tr>
-        <th width="30%">
+        <th width="50%">
             <a ng-click="lhc.toggleWidgetSort('onop_sort','onl_dsc','onl_asc',true)">
                 <i title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Operator');?>" class="material-icons">account_box</i>
                 <i ng-class="{'text-muted' : (lhc.toggleWidgetData['onop_sort'] != 'onl_dsc' && lhc.toggleWidgetData['onop_sort'] != 'onl_asc')}" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Sort by online status')?>" class="material-icons">{{lhc.toggleWidgetData['onop_sort'] == 'onl_dsc' || lhc.toggleWidgetData['onop_sort'] != 'onl_asc' ? 'trending_up' : 'trending_down'}}</i>
             </a>
         </th>
-        <th width="20%"><i title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Last activity ago');?>" class="material-icons">access_time</i></th>
-        <th width="20%">
+        <th width="5%"><i title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Last activity ago');?>" class="material-icons">access_time</i></th>
+        <th width="15%">
             <a ng-click="lhc.toggleWidgetSort('onop_sort','ac_dsc','ac_asc',true)">
                 <i title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Active chats');?>" class="material-icons chat-active">chat</i>
                 <i ng-class="{'text-muted' : (lhc.toggleWidgetData['onop_sort'] != 'ac_dsc' && lhc.toggleWidgetData['onop_sort'] != 'ac_asc')}" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Sort by active chats number')?>" class="material-icons">{{lhc.toggleWidgetData['onop_sort'] == 'ac_dsc' || lhc.toggleWidgetData['onop_sort'] != 'ac_asc' ? 'trending_up' : 'trending_down'}}</i>
