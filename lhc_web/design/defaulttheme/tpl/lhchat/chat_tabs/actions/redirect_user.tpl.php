1cbd3433c79a644cd15a4ec9cc3c0e729446b943
Remigijus Kiminas <remdex@gmail.com>
2021-05-06 05:50:23 -0400

    Make shorter sentence

--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/actions/redirect_user.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/actions/redirect_user.tpl.php
@@ -1,7 +1,7 @@
 <?php if (erLhcoreClassUser::instance()->hasAccessTo('lhchat','allowredirect')) : ?>
 <div class="col-6 pb-1">
 <a class="text-muted" onclick="lhinst.redirectToURL('<?php echo $chat->id?>','<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Please enter a URL');?>')">
-    <span class="material-icons">link</span><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Redirect user to another url');?>
+    <span class="material-icons">link</span><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Redirect to another url');?>
 </a>
 </div>
 <?php endif;?>
