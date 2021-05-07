70ac105e4b1ea3881756334335d3193a4e28199b
Remigijus Kiminas <remdex@gmail.com>
2021-05-06 23:51:43 -0400

    Use nick for user left messages

--- lhc_web/modules/lhchat/chatwidgetclosed.php
+++ lhc_web/modules/lhchat/chatwidgetclosed.php
@@ -81,7 +81,7 @@ if ($Params['user_parameters_unordered']['hash'] != '') {
             if ($chat->user_typing < (time() - 12)) {
                 $chat->user_typing = time() - 5;// Show for shorter period these status messages
                 $chat->is_user_typing = 1;
-                $chat->user_typing_txt = htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'Visitor has left the chat!'), ENT_QUOTES);
+                $chat->user_typing_txt = ($chat->nick != 'Visitor' ? $chat->nick : htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'Visitor'), ENT_QUOTES)) .' '.htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'has left the chat!'), ENT_QUOTES);
             }
 
             // User Closed Chat
@@ -107,7 +107,7 @@ if ($Params['user_parameters_unordered']['hash'] != '') {
 
                     if ($informVisitorLeft == true) {
                         $msg = new erLhcoreClassModelmsg();
-                        $msg->msg = '[level=system-warning exit-visitor]'.htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'Visitor has closed the chat explicitly!'), ENT_QUOTES).'[/level]';
+                        $msg->msg = '[level=system-warning exit-visitor]' . ($chat->nick != 'Visitor' ? $chat->nick : htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'Visitor'), ENT_QUOTES)) .' '.htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'has closed the chat explicitly!'), ENT_QUOTES).'[/level]';
                         $msg->chat_id = $chat->id;
                         $msg->user_id = -1;
                         $msg->time = time();
@@ -186,7 +186,7 @@ if ($Params['user_parameters_unordered']['hash'] != '') {
                 }
 
                 $msg = new erLhcoreClassModelmsg();
-                $msg->msg = '[level=system-warning exit-visitor]'.htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'Visitor has closed the chat explicitly!'), ENT_QUOTES).'[/level]';
+                $msg->msg = '[level=system-warning exit-visitor]'.($chat->nick != 'Visitor' ? $chat->nick : htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'Visitor'), ENT_QUOTES)) .' '.htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/userleftchat', 'has closed the chat explicitly!'), ENT_QUOTES).'[/level]';
                 $msg->chat_id = $chat->id;
                 $msg->user_id = -1;
                 $msg->time = time();
