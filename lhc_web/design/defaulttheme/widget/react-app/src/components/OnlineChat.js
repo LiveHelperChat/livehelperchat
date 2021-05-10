34e42af57c06a26c2faf5a3365504301be072f81
Remigijus Kiminas <remdex@gmail.com>
2021-05-10 08:21:15 -0400

    Remove spacing

--- lhc_web/design/defaulttheme/widget/react-app/src/components/OnlineChat.js
+++ lhc_web/design/defaulttheme/widget/react-app/src/components/OnlineChat.js
@@ -765,7 +765,7 @@ class OnlineChat extends Component {
                 bottom_messages += " position-relative";
             }
 
-            var message_send_style = "mx-auto pb-1 w-100";
+            var message_send_style = "mx-auto w-100";
 
             if (this.props.chatwidget.getIn(['chatLiveData','closed']) == true) {
                 message_send_style += (this.props.chatwidget.get('mode') == 'embed' ? ' pr-2' : ' pr-1');
--- lhc_web/modules/lhwidgetrestapi/settings.php
+++ lhc_web/modules/lhwidgetrestapi/settings.php
@@ -365,7 +365,7 @@ $ts = time();
 $outputResponse['wv'] = 171;
 
 // React APP versions
-$outputResponse['v'] = 196;
+$outputResponse['v'] = 197;
 
 $outputResponse['hash'] = sha1(erLhcoreClassIPDetect::getIP() . $ts . erConfigClassLhConfig::getInstance()->getSetting( 'site', 'secrethash' ));
 $outputResponse['hash_ts'] = $ts;
