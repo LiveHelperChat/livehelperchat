4d2a183f7fa66b0aa085fd59b639c8892b36b432
Remigijus Kiminas <remdex@gmail.com>
2021-05-21 12:05:39 -0400

    Add support for replaceable variables

--- lhc_web/lib/core/lhgenericbot/actionTypes/lhgenericbotactionexecute_js.php
+++ lhc_web/lib/core/lhgenericbot/actionTypes/lhgenericbotactionexecute_js.php
@@ -11,14 +11,23 @@ class erLhcoreClassGenericBotActionExecute_js {
         if ((isset($action['content']['payload']) && !empty($action['content']['payload'])) || (isset($action['content']['ext_execute']) && !empty($action['content']['ext_execute'])))
         {
             if (isset($action['content']['payload'])) {
+                if (isset($params['replace_array'])) {
+                    $action['content']['payload'] = @str_replace(array_keys($params['replace_array']),array_values($params['replace_array']),$action['content']['payload']);
+                }
                 $action['content']['payload'] = erLhcoreClassGenericBotWorkflow::translateMessage($action['content']['payload'], array('chat' => $chat, 'args' => $params));
             }
 
             if (isset($action['content']['ext_args'])) {
+                if (isset($params['replace_array'])) {
+                    $action['content']['ext_args'] = @str_replace(array_keys($params['replace_array']),array_values($params['replace_array']),$action['content']['ext_args']);
+                }
                 $action['content']['ext_args'] = erLhcoreClassGenericBotWorkflow::translateMessage($action['content']['ext_args'], array('chat' => $chat, 'args' => $params));
             }
 
             if (isset($action['content']['ext_execute'])) {
+                if (isset($params['replace_array'])) {
+                    $action['content']['ext_execute'] = @str_replace(array_keys($params['replace_array']),array_values($params['replace_array']),$action['content']['ext_execute']);
+                }
                 $action['content']['ext_execute'] = erLhcoreClassGenericBotWorkflow::translateMessage($action['content']['ext_execute'], array('chat' => $chat, 'args' => $params));
             }
 
