8ca5a6e7a281c49f0ba6c8105f564473fd08bf14
Remigijus Kiminas <remdex@gmail.com>
2021-05-31 05:41:09 -0400

    Make smaller fields

--- lhc_web/design/defaulttheme/tpl/lhchat/cannedmsg/search_panel.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/cannedmsg/search_panel.tpl.php
@@ -6,19 +6,19 @@
         <div class="col-md-3">
             <div class="form-group">
                 <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Title');?></label>
-                <input type="text" class="form-control" name="title" value="<?php echo htmlspecialchars($input->title)?>" />
+                <input type="text" class="form-control form-control-sm" name="title" value="<?php echo htmlspecialchars($input->title)?>" />
             </div>
         </div>
         <div class="col-md-3">
             <div class="form-group">
                 <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Message');?></label>
-                <input type="text" class="form-control" name="message" value="<?php echo htmlspecialchars($input->message)?>" />
+                <input type="text" class="form-control form-control-sm" name="message" value="<?php echo htmlspecialchars($input->message)?>" />
             </div>
         </div>
         <div class="col-md-3">
             <div class="form-group">
                 <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Fallback message');?></label>
-                <input type="text" class="form-control" name="fmsg" value="<?php echo htmlspecialchars($input->fmsg)?>" />
+                <input type="text" class="form-control form-control-sm" name="fmsg" value="<?php echo htmlspecialchars($input->fmsg)?>" />
             </div>
         </div>
         <div class="col-md-3">
@@ -28,7 +28,7 @@
                     'input_name'     => 'department_id',
                     'optional_field' => erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Select department'),
                     'selected_id'    => $input->department_id,
-                    'css_class'      => 'form-control',
+                    'css_class'      => 'form-control form-control-sm',
                     'list_function_params' => erLhcoreClassUserDep::conditionalDepartmentFilter(),
                     'list_function'  => 'erLhcoreClassModelDepartament::getList'
                 )); ?>
@@ -37,13 +37,13 @@
     </div>
 
     <div class="btn-group" role="group" aria-label="...">
-        <input type="submit" name="doSearch" class="btn btn-secondary" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Search');?>" />
+        <input type="submit" name="doSearch" class="btn btn-secondary btn-sm" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Search');?>" />
         <?php if ($pages->items_total > 0) : ?>
-            <a target="_blank" class="btn btn-secondary" href="<?php echo $pages->serverURL?>?export=1"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','CSV');?></a>
+            <a target="_blank" class="btn btn-secondary btn-sm" href="<?php echo $pages->serverURL?>?export=1"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','CSV');?></a>
         <?php endif; ?>
         
         <?php if (erLhcoreClassUser::instance()->hasAccessTo('lhcannedmsg','use_import'))  : ?>
-            <a target="_blank" class="btn btn-secondary text-white" onclick="return lhc.revealModal({'title' : '<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Import');?>', 'iframe':true, 'height':500, 'url':WWW_DIR_JAVASCRIPT +'cannedmsg/import'})"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Import');?></a>
+            <a target="_blank" class="btn btn-secondary btn-sm text-white" onclick="return lhc.revealModal({'title' : '<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Import');?>', 'iframe':true, 'height':500, 'url':WWW_DIR_JAVASCRIPT +'cannedmsg/import'})"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Import');?></a>
         <?php endif; ?>
         
     </div>
