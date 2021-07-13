d96e3cd5405a42a5b4402163c98fea5edd1b8aed
Remigijus Kiminas <remdex@gmail.com>
2021-07-12 23:03:56 -0400

    Add spaces

--- lhc_web/design/defaulttheme/tpl/lhchat/transfer/transferchatrefilter.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/transfer/transferchatrefilter.tpl.php
@@ -4,14 +4,12 @@
 
     <?php foreach (erLhcoreClassModelDepartamentGroup::getList() as $depGroupItem) : $idsUnique = [];?>
         <h6>
-            <input type="button" class="btn btn-xs btn-secondary" value="Show/Hide" onclick="$('#dep-group-filter-id-<?php echo $depGroupItem->id?>').toggle()">
-            <?php echo htmlspecialchars($depGroupItem->name)?>
+            <input type="button" class="btn btn-xs btn-secondary" value="Show/Hide" onclick="$('#dep-group-filter-id-<?php echo $depGroupItem->id?>').toggle()">&nbsp;<?php echo htmlspecialchars($depGroupItem->name)?>
         </h6>
         <div id="dep-group-filter-id-<?php echo $depGroupItem->id?>" style="display: none">
             <?php $membersDeps = erLhcoreClassModelDepartamentGroupMember::getList(array('filter' => array('dep_group_id' => $depGroupItem->id))); ?>
             <?php foreach ($membersDeps as $memberDep) {$idsUnique[] = $memberDep->dep_id; } ?>
             <?php if (!empty($idsUnique)) : ?>
-                <label><b><?php echo htmlspecialchars($depGroupItem->name)?></b></label>
                 <?php foreach (erLhcoreClassModelDepartament::getList(array('sort' => 'sort_priority ASC, name ASC','filterin' => array('id' => $idsUnique))) as $depGroup) : ?>
                     <div class="checkbox"><label><input type="radio" name="DepartamentID<?php echo $departments_filter['chat_id']?>" value="<?php echo $depGroup->id?>"/> <?php echo htmlspecialchars($depGroup->name)?><?php if ($depGroup->id == $departments_filter['dep_id']) : ?> <b>[<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','current');?>]</b><?php endif;?></label></div>
                 <?php endforeach; ?>
@@ -22,8 +20,7 @@
     <?php $memberOf = erLhcoreClassModelDepartamentGroupMember::getList(array('filter' => array('dep_id' => $departments_filter['dep_id'])));$hasMembers = !empty($memberOf); ?>
     <?php if (!empty($memberOf)) : ?>
         <h6 class="border-top pt-2">
-            <input type="button" class="btn btn-xs btn-secondary" value="Show/Hide" onclick="$('.member-of-dep-filter').toggle()">
-            <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Member of these departments groups')?>
+            <input type="button" class="btn btn-xs btn-secondary" value="Show/Hide" onclick="$('.member-of-dep-filter').toggle()">&nbsp;<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Member of these departments groups')?>
             </h6>
         </h6>
     <?php endif; ?>
@@ -42,8 +39,7 @@
     <?php endforeach; ?>
 
     <h6 class="border-top pt-2">
-        <input type="button" class="btn btn-xs btn-secondary" value="Show/Hide" onclick="$('#online-transfer-dep').toggle()">
-        <span class="label label-success"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Online');?></span>
+        <input type="button" class="btn btn-xs btn-secondary" value="Show/Hide" onclick="$('#online-transfer-dep').toggle()">&nbsp;<span class="label label-success"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Online');?></span>
         </h6>
     <div id="online-transfer-dep" style="display: <?php $hasMembers == true ? print 'none' : 'block'?>">
     <?php
@@ -55,9 +51,7 @@
     </div>
 
     <h6 class="border-top pt-2">
-        <input type="button" class="btn btn-xs btn-secondary" value="Show/Hide" onclick="$('#offline-transfer-dep').toggle()">
-        <span class="label label-default"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Offline');?></span>
-
+        <input type="button" class="btn btn-xs btn-secondary" value="Show/Hide" onclick="$('#offline-transfer-dep').toggle()">&nbsp;<span class="label label-default"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Offline');?></span>
     </h6>
     <div id="offline-transfer-dep" style="display: none">
     <?php foreach ($departments as $departament) : if (!in_array($departament->id, $onlineDepartments)) : ?>
