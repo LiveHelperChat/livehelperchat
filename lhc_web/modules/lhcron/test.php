c958a290fa75767156a121641652378ea03ce680
Remigijus Kiminas <remdex@gmail.com>
2021-07-21 12:35:16 -0400

    Changes

--- lhc_web/modules/lhcron/test.php
+++ lhc_web/modules/lhcron/test.php
@@ -13,7 +13,7 @@ erLhcoreClassChatEventDispatcher::getInstance()->dispatch('mail.conversation_sta
     'mail' => & $message,
     'conversation' => & $conversation
 ));*/
-include 'lib/vendor/autoload.php';
+/*include 'lib/vendor/autoload.php';
 
 $mailboxHandler = new PhpImap\Mailbox(
     'host', // IMAP server incl. flags and optional mailbox folder
@@ -26,10 +26,16 @@ $mail = $mailboxHandler->getMail(23295, false);
 echo erLhcoreClassMailconvEncoding::toUTF8($mail->textPlain),"\n";
 
 $mail = $mailboxHandler->getMail(23325, false);
-echo erLhcoreClassMailconvEncoding::toUTF8($mail->textPlain),"\n";
+echo erLhcoreClassMailconvEncoding::toUTF8($mail->textPlain),"\n";*/
 
 //echo mb_convert_encoding($mail->textPlain,'UTF-8','ISO-8859-1');
 
+
+
+
+// php cron.php72 -s site_admin -c cron/test
+$mailbox = erLhcoreClassModelMailconvMailbox::fetch(23);
+erLhcoreClassMailconvParser::syncMailbox($mailbox, ['live' => true]);
 
 
 ?>
