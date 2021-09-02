c704e64ca94c4b9cdfc255f842fb8df1ed9ac51e
Remigijus Kiminas <remdex@gmail.com>
2021-09-02 02:56:19 -0400

    Chat variables

--- lhc_web/lib/models/lhmailconv/erlhcoreclassmodelmailconvconversation.php
+++ lhc_web/lib/models/lhmailconv/erlhcoreclassmodelmailconvconversation.php
@@ -164,6 +164,19 @@ class erLhcoreClassModelMailconvConversation
                 }
                 return $this->customer_email;
 
+            case 'mail_variables_array':
+                if (!empty($this->mail_variables)) {
+                    $jsonData = json_decode($this->mail_variables,true);
+                    if ($jsonData !== null) {
+                        $this->mail_variables_array = $jsonData;
+                    } else {
+                        $this->mail_variables_array = $this->mail_variables;
+                    }
+                } else {
+                    $this->mail_variables_array = array();
+                }
+                return $this->mail_variables_array;
+
             default:
                 ;
                 break;
