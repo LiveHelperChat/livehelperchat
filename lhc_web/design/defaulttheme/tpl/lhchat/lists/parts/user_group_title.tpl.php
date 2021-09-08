3a67dd0e93d65f3efd501369be914f9ce3bce80d
Remigijus Kiminas <remdex@gmail.com>
2021-09-08 02:24:23 -0400

    Allow extensions to have custom title

---
+++ lhc_web/design/defaulttheme/tpl/lhchat/lists/parts/user_group_title.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+$userGroupTitle = [
+    'user_group' => erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','User group'),
+    'user_group_select' => erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Select group'),
+];
+
+?>
