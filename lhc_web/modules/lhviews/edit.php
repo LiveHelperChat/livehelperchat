63bf711659a5c2294afc6c39ab808e3c85913fbe
Remigijus Kiminas <remdex@gmail.com>
2021-09-13 01:04:26 -0400

    Updates

--- lhc_web/modules/lhviews/edit.php
+++ lhc_web/modules/lhviews/edit.php
@@ -6,7 +6,10 @@ erLhcoreClassModelUser::fetch((int)$Params['user_parameters']['user_id']);
 
 $append = erLhcoreClassSearchHandler::getURLAppendFromInput($search->params_array['input_form']);
 
-erLhcoreClassModule::redirect('chat/list', '/(view)/'.$search->id.$append);
+if ($search->scope == 'chat'){
+    erLhcoreClassModule::redirect('chat/list', '/(view)/'.$search->id.$append);
+}
+
 exit;
 
 ?>
