03fb4261d08e59ebc660194ee8247704b2d89a85
Remigijus Kiminas <remdex@gmail.com>
2021-09-16 06:04:57 -0400

    Change titles

--- lhc_web/design/defaulttheme/tpl/pagelayouts/parts/top_menu_chat_actions.tpl.php
+++ lhc_web/design/defaulttheme/tpl/pagelayouts/parts/top_menu_chat_actions.tpl.php
@@ -5,10 +5,10 @@ if ($currentUser->hasAccessTo('lhchat','allowchattabs')) {
     $menuItems[] = array('href' => '#', 'onclick' => 'javascript:lhinst.chatTabsOpen()' ,'iclass' => 'chat', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Chat tabs'));
 } 
 
-$menuItems[] = array('href' => erLhcoreClassDesign::baseurl('chat/list'),'iclass' => 'list', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Chats list'));
+$menuItems[] = array('href' => erLhcoreClassDesign::baseurl('chat/list'),'iclass' => 'list', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Chats List'));
 
 if ($currentUser->hasAccessTo('lhmailconv','use_admin')) {
-    $menuItems[] = array('href' => erLhcoreClassDesign::baseurl('mailconv/conversations'), 'iclass' => 'mail_outline', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout', 'Mails'));
+    $menuItems[] = array('href' => erLhcoreClassDesign::baseurl('mailconv/conversations'), 'iclass' => 'mail_outline', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout', 'Mails List'));
 }
 
 if ($currentUser->hasAccessTo('lhviews','use')) {
