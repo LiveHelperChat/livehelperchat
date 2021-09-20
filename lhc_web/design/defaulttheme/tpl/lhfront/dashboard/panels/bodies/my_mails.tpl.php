3a72deaedbea91414aa3b336f6f084a36f0c9c5e
Remigijus Kiminas <remdex@gmail.com>
2021-09-20 04:40:12 -0400

    No need owner

--- lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/bodies/my_mails.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/bodies/my_mails.tpl.php
@@ -9,7 +9,7 @@
     <tr ng-repeat="chat in my_mails.list track by chat.id">
         <td>
             <div class="abbr-list" >
-                <i ng-if="chat.status != 1" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Pending chat');?>" class="material-icons chat-unread">&#xE80E;</i><a title="{{chat.id}}" ng-click="lhc.previewMail(chat.id)" class="material-icons">info_outline</a><a ng-click="lhc.startMailChat(chat.id,chat.subject_front)" title="{{chat.from_address}}">{{chat.from_name}} | {{chat.subject_front}}</a><small>{{chat.plain_user_name !== undefined ? ' | ' + chat.plain_user_name : ''}}</small></div>
+                <i ng-if="chat.status != 1" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Pending chat');?>" class="material-icons chat-unread">&#xE80E;</i><a title="{{chat.id}}" ng-click="lhc.previewMail(chat.id)" class="material-icons">info_outline</a><a ng-click="lhc.startMailChat(chat.id,chat.subject_front)" title="{{chat.from_address}}">{{chat.from_name}} | {{chat.subject_front}}</a></div>
         </td>
         <td>
             <div class="abbr-list" title="{{chat.wait_time_pending}}">{{chat.wait_time_pending}}</div>
