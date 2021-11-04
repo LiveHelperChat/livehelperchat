66891f3ad2b8015e661578bc21fe06dd583e9c7a
Remigijus Kiminas <remdex@gmail.com>
2021-11-04 04:32:42 -0400

    Auto preload previous messages on scroll

--- lhc_web/design/defaulttheme/tpl/lhchat/loadpreviousmessages.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/loadpreviousmessages.tpl.php
@@ -1,6 +1,34 @@
 <?php include(erLhcoreClassDesign::designtpl('lhchat/syncadmin.tpl.php'));?>
 <?php if ($initial == true) : ?>
-    <div class="alert alert-info" role="alert" style="margin:10px 10px 30px 10px;padding:5px">
-        <p align="center"><b><i class="material-icons">&#xE316;</i><i class="material-icons">&#xE316;</i><i class="material-icons">&#xE316;</i> <i class="material-icons fs24">&#xE889;</i> <i class="material-icons">&#xE316;</i><i class="material-icons">&#xE316;</i><i class="material-icons">&#xE316;</i></b></p>
+    <div class="alert alert-info" role="alert" style="margin:10px 10px 30px 10px;padding:5px" id="scroll-to-chat-<?php echo $chat->id?>-<?php echo $message_start?>">
+        <p class="mb-0 text-center"><b><i class="material-icons">&#xE316;</i><i class="material-icons">&#xE316;</i><i class="material-icons">&#xE316;</i> <i class="material-icons fs24">&#xE889;</i> End of the chat - <?php echo $chat->id?> <i class="material-icons">&#xE316;</i><i class="material-icons">&#xE316;</i><i class="material-icons">&#xE316;</i></b></p>
+    </div>
+<?php elseif (isset($chat) && isset($chat_history) && is_object($chat) && is_object($chat_history) && $chat->id != $chat_history->id ) : ?>
+    <?php if ($message_start == 0) : ?>
+    <div class="alert alert-info" role="alert" style="margin:10px 10px 30px 10px;padding:5px" id="scroll-to-chat-<?php echo $chat->id?>-<?php echo $message_start?>">
+        <p class="mb-0 text-center"><b>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons fs24">&#xE889;</i> End of the chat - <?php echo $chat->id?>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons">&#xE316;</i>
+            </b>
+        </p>
+    </div>
+    <?php endif; ?>
+<?php elseif (isset($chat) && is_object($chat) && $message_start == 0) : ?>
+    <div class="alert alert-info" role="alert" style="margin:10px 10px 30px 10px;padding:5px" id="scroll-to-chat-<?php echo $chat->id?>-<?php echo $message_start?>">
+        <p class="mb-0 text-center"><b>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons fs24">&#xE889;</i> End of the chat -  <?php echo $chat->id?>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons">&#xE316;</i>
+                <i class="material-icons">&#xE316;</i>
+            </b>
+        </p>
     </div>
 <?php endif; ?>
