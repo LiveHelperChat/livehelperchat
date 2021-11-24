ff2cb826d1b42d037e5986cff75c2c11023a0e39
Remigijus Kiminas <remdex@gmail.com>
2021-11-24 01:00:28 -0500

    If json is invalid save it as string

--- lhc_web/lib/core/lhgenericbot/actionTypes/lhgenericbotactionlaction.php
+++ lhc_web/lib/core/lhgenericbot/actionTypes/lhgenericbotactionlaction.php
@@ -19,8 +19,16 @@ class erLhcoreClassGenericBotActionLaction {
             $chatAction = new erLhcoreClassModelChatAction();
             $chatAction->chat_id = $chat->id;
             $chatAction->action = (string)$action['content']['action_options']['action'];
-            $chatAction->body_array = json_decode(erLhcoreClassGenericBotWorkflow::translateMessage($action['content']['text'], array('as_json' => true, 'chat' => $chat, 'args' => $params)), true);
+
+            $bodyAction = erLhcoreClassGenericBotWorkflow::translateMessage($action['content']['text'], array('as_json' => true, 'chat' => $chat, 'args' => $params));
+
+            $chatAction->body_array = json_decode($bodyAction, true);
             $chatAction->body = json_encode($chatAction->body_array);
+
+            if ($chatAction->body_array == null) {
+                $chatAction->body = json_encode($bodyAction);
+            }
+
             $chatAction->saveThis();
         }
     }
