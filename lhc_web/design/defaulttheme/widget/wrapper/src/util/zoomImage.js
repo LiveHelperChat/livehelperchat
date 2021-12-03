2c8d6e94a0fafb0ff5d09b9a4d2666e6dcf729cc
Remigijus Kiminas <remdex@gmail.com>
2021-12-03 10:13:54 -0500

    Update

--- lhc_web/design/defaulttheme/widget/wrapper/src/util/zoomImage.js
+++ lhc_web/design/defaulttheme/widget/wrapper/src/util/zoomImage.js
@@ -18,7 +18,7 @@ class _zoomImage {
         this.attributes = attributes;
         this.chatEvents = chatEvents;
 
-        this.addCss('body{overflow:hidden;}.lhc-modal {display: none; position: fixed; z-index: 2147483641 !important;padding-top: ' + (attributes.isMobile == true ? 0 : 20)  +'px;left: 0;top: 0;  width: 100%;height: 100%; overflow: auto; background-color: rgb(0,0,0);  background-color: rgba(0,0,0,0.4); }'+
+        this.addCss('body{overflow:hidden;}.lhc-modal *{box-sizing: border-box;}.lhc-modal { display: none; position: fixed; z-index: 2147483641 !important;padding-top: ' + (attributes.isMobile == true ? 0 : 20)  +'px;left: 0;top: 0;  width: 100%;height: 100%; overflow: auto; background-color: rgb(0,0,0);  background-color: rgba(0,0,0,0.4); }'+
             '.lhc-modal-content {background-color: #fefefe; margin: auto; border: 1px solid #888; width: ' + (attributes.isMobile == true ? 100 : 80)  +'%;border-radius:5px; }'+
             '#lhc-close { color: #aaaaaa;    font-size: 34px;    font-weight: bold;  }'+
             '#lhc-close:hover,#lhc-close:focus {color: #000; text-decoration: none; cursor: pointer;}');
@@ -26,7 +26,7 @@ class _zoomImage {
         this.appendHTML('<div id="lhc-co-browsing-modal" style="display: block" class="lhc-modal">'+
             '<div class="lhc-modal-content">'+
             '<div style="padding:5px 20px;clear: both; text-align: right"><span id="lhc-close">&times;</span></div>'+
-            '<div style="text-align: center;background-color:#cecece"><img id="lhc-zoom-image" src="'+params.src+'" style="max-width:100%;max-height: '+(window.innerHeight - 140) +'px;" title="" /></div><div><div style="height: 33px; padding:10px;" ><span style="font-family: Arial; font-size: 18px;font-weight: bold;">'+params.title+'&nbsp;</span> <a target="_blank" href="'+params.src+'" style="font-family: Arial; text-decoration: none;background-color: #dddddd; display: inline-block; padding:5px; border:1px solid #CECECE; color: #6d6d6d; float: right">Download</a></div></div></div></div>');
+            '<div style="text-align: center;background-color:#cecece"><img id="lhc-zoom-image" src="'+params.src+'" style="max-width:100%;max-height: '+(window.innerHeight - 140) +'px;" title="" /></div><div><div style="height: 49px; padding:10px;" ><span style="font-family: Arial; font-size: 18px;font-weight: bold;">'+params.title+'&nbsp;</span> <a target="_blank" href="'+params.src+'" style="font-family: Arial; text-decoration: none;background-color: #dddddd; display: inline-block; padding:5px; border:1px solid #CECECE; color: #6d6d6d; float: right">Download</a></div></div></div></div>');
 
         var btn = document.getElementById("lhc-close");
         var modal = document.getElementById("lhc-co-browsing-modal");
