b5d21d2830560a76a2602e41fdb5ecc6f7182e9f
Remigijus Kiminas <remdex@gmail.com>
2021-12-14 06:26:13 -0500

    Let extensions override default explain text

--- lhc_web/design/defaulttheme/tpl/lhabstract/custom/subject.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhabstract/custom/subject.tpl.php
@@ -11,7 +11,7 @@
 
 <div class="form-group">
     <label class="mb-0"><?php echo erLhcoreClassAbstract::renderInput('internal', $fields['internal'], $object)?>&nbsp;<?php echo $fields['internal']['trans'];?></label>
-    <p><small><i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/subject','Internal subjects will not be shown in a subject choosing modal window for the operators. They are good for setting them in the bot, extensions etc.');?></i></small></p>
+    <?php include(erLhcoreClassDesign::designtpl('lhabstract/custom/subject/internal_explain.tpl.php'));?>
 </div>
 
 <div class="form-group">
