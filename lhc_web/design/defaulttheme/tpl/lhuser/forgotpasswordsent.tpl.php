fbed8728be59040a7218610e72f6eceb5f8bc152
Remigijus Kiminas <remdex@gmail.com>
2022-01-03 01:11:45 -0500

    Show same message in all cases

--- lhc_web/design/defaulttheme/tpl/lhuser/forgotpasswordsent.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhuser/forgotpasswordsent.tpl.php
@@ -1,2 +1,2 @@
 <h1><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/forgotpasswordsent','Password reminder');?></h1>
-<p><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/forgotpasswordsent','E-mail sent. Click the link in the email and You will be able to set a new password.')?></p>
+<p><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('user/forgotpasswordsent','If this e-mail exists we have send password remind link. Click the link in the email and You will be able to set a new password.')?></p>
