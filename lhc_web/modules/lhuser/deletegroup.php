cc1122aed0d1ad9f05757eaea2ab9e6a924776bd
Remigijus Kiminas <remdex@gmail.com>
2022-01-18 00:18:36 -0500

    Display logic should match backand logic

--- lhc_web/modules/lhuser/deletegroup.php
+++ lhc_web/modules/lhuser/deletegroup.php
@@ -5,6 +5,11 @@ if (!$currentUser->validateCSFRToken($Params['user_parameters_unordered']['csfr'
 	exit;
 }
 
+if ((int)$Params['user_parameters']['group_id'] == 1) {
+    die('admin account never can be deleted!');
+    exit;
+}
+
 erLhcoreClassGroup::deleteGroup((int)$Params['user_parameters']['group_id']);
 
 erLhcoreClassAdminChatValidatorHelper::clearUsersCache();
