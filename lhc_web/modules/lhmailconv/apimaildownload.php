7b430f2e8f96fabbf5addf8a773aa469b28441cd
Remigijus Kiminas <remdex@gmail.com>
2022-01-23 08:18:01 -0500

    Fix mail download

--- lhc_web/modules/lhmailconv/apimaildownload.php
+++ lhc_web/modules/lhmailconv/apimaildownload.php
@@ -7,19 +7,34 @@ $mail = erLhcoreClassModelMailconvMessage::fetch($Params['user_parameters']['id'
 $mailbox = erLhcoreClassModelMailconvMailbox::fetch($mail->mailbox_id);
 
 $mailboxHandler = new PhpImap\Mailbox(
-    $mail->mb_folder, // IMAP server incl. flags and optional mailbox folder
+    $mailbox->imap, // IMAP server incl. flags and optional mailbox folder
     $mailbox->username, // Username for the before configured mailbox
     $mailbox->password, // Password for the before configured username
     false
 );
 
-$bodyRaw = $mailboxHandler->getRawMail($mail->uid);
+try {
+    $bodyRaw = $mailboxHandler->getRawMail($mail->uid);
 
-header('Content-Disposition: attachment; filename="'.$mail->id.'.eml"');
-header('Content-type: text/plain');
+    header('Content-Disposition: attachment; filename="'.$mail->id.'.eml"');
+    header('Content-type: text/plain');
 
-echo $bodyRaw;
+    echo $bodyRaw;
 
-exit;
+    exit;
+
+} catch (Exception $e) {
+
+    $tpl = erLhcoreClassTemplate::getInstance('lhkernel/validation_error.tpl.php');
+
+    $tpl->set('errors',[
+        erTranslationClassLhTranslation::getInstance()->getTranslation('module/mailconv','Message with specified ID could not be found anymore in provided IMAP folder'),
+        htmlspecialchars($e->getMessage()),
+        erTranslationClassLhTranslation::getInstance()->getTranslation('module/mailconv','Message ID').' - '.$mail->uid.' '.$mail->message_id,
+        $mail->mb_folder
+    ]);
+
+    $Result['content'] = $tpl->fetch();
+}
 
 ?>
