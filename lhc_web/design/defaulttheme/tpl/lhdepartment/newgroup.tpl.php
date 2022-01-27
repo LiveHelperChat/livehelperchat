c061f91c43aa617dd4221f02de95d17da2e80e2f
Remigijus Kiminas <remdex@gmail.com>
2022-01-27 00:17:11 -0500

    fix

--- lhc_web/design/defaulttheme/tpl/lhdepartment/newgroup.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhdepartment/newgroup.tpl.php
@@ -4,7 +4,7 @@
 	<?php include(erLhcoreClassDesign::designtpl('lhkernel/validation_error.tpl.php'));?>
 <?php endif; ?>
 
-<form action="<?php echo erLhcoreClassDesign::baseurl('department/newgroup')?>" method="post">
+<form action="<?php echo erLhcoreClassDesign::baseurl('department/newgroup')?>" method="post" ng-non-bindable>
 
     <?php include(erLhcoreClassDesign::designtpl('lhkernel/csfr_token.tpl.php'));?>
 
