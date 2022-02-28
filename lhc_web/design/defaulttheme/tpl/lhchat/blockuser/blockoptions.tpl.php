8f15f9ecc3eaea07aec94f52e0f2504b4a77ab49
Remigijus Kiminas <remdex@gmail.com>
2022-02-28 05:40:19 -0500

    Block options

--- lhc_web/design/defaulttheme/tpl/lhchat/blockuser/blockoptions.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/blockuser/blockoptions.tpl.php
@@ -1,10 +1,10 @@
-<label><input type="checkbox" name="btype[]" checked="checked" value="<?php echo erLhcoreClassModelChatBlockedUser::BLOCK_IP?>"> IP</label></br>
+<label><input type="checkbox" name="btype[]" checked="checked" value="<?php echo erLhcoreClassModelChatBlockedUser::BLOCK_IP?>"> IP<?php if (isset($chat) && isset($chat->ip)) : ?>&nbsp;<small ng-non-bindable>(<?php echo htmlspecialchars($chat->ip)?>)</small><?php endif;?></label></br>
 <?php if (!isset($chat) || $chat->nick != 'Visitor') : ?>
-<label><input type="checkbox" name="btype[]" value="<?php echo erLhcoreClassModelChatBlockedUser::BLOCK_NICK?>"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/blockedusers','Nick')?>&nbsp;<?php if (isset($chat)) : ?><small>(<?php echo htmlspecialchars($chat->nick)?>)</small><?php endif; ?></label>
+<label ng-non-bindable><input type="checkbox" name="btype[]" value="<?php echo erLhcoreClassModelChatBlockedUser::BLOCK_NICK?>"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/blockedusers','Nick')?>&nbsp;<?php if (isset($chat)) : ?><small>(<?php echo htmlspecialchars($chat->nick)?>)</small><?php endif; ?></label>
 </br>
-<label><input type="checkbox" name="btype[]" value="<?php echo erLhcoreClassModelChatBlockedUser::BLOCK_NICK_DEP?>"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/blockedusers','Nick and Department')?>&nbsp;<?php if (isset($chat)) : ?><small>(<?php echo htmlspecialchars($chat->nick)?> && <?php echo htmlspecialchars($chat->department_name)?>)</small><?php endif; ?></label></br>
+<label ng-non-bindable><input type="checkbox" name="btype[]" value="<?php echo erLhcoreClassModelChatBlockedUser::BLOCK_NICK_DEP?>"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/blockedusers','Nick and Department')?>&nbsp;<?php if (isset($chat)) : ?><small>(<?php echo htmlspecialchars($chat->nick)?> && <?php echo htmlspecialchars($chat->department_name)?>)</small><?php endif; ?></label></br>
 <?php endif; ?>
-<?php if (isset($chat->online_user_id)) : ?>
+<?php if (isset($chat) && isset($chat->online_user_id)) : ?>
 <label><input type="checkbox" name="btype_online_user" value="on"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/blockedusers','Online user (cookie)')?></label></br>
 <?php endif; ?>
 <label><input type="checkbox" name="btype_email" value="on"> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/blockedusers','E-mail')?></label>
