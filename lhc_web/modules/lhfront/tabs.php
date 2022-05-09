a88901ebe13400ecfb515153bc88855112dcdceb
Remigijus Kiminas <remdex@gmail.com>
2022-05-09 05:21:40 -0400

    Merge fix

--- lhc_web/modules/lhfront/tabs.php
+++ lhc_web/modules/lhfront/tabs.php
@@ -4,20 +4,13 @@ erLhcoreClassRestAPIHandler::setHeaders();
 
 $id = $Params['user_parameters_unordered']['id'];
 
-if (is_array($id)) {
-    erLhcoreClassChat::validateFilterIn($id);
+erLhcoreClassChat::validateFilterIn($id);
 
-<<<<<<< HEAD
-    $response = array();
+$response = array();
 
-    if (!empty($id)) {
-        $chats = erLhcoreClassModelChat::getList(array('sort' => 'id DESC', 'filterin' => array('id' => $id)));
-        foreach ($chats as $chat) {
-            $item = array(
-=======
 if (!empty($id)) {
 
-    $icons_additional = erLhAbstractModelChatColumn::getList(array('ignore_fields' => array('position','conditions','column_identifier','enabled'), 'sort' => false, 'filter' => array('icon_mode' => 1, 'enabled' => 1, 'chat_enabled' => 1)));
+    $icons_additional = erLhAbstractModelChatColumn::getList(array('ignore_fields' => array('position', 'conditions', 'column_identifier', 'enabled'), 'sort' => false, 'filter' => array('icon_mode' => 1, 'enabled' => 1, 'chat_enabled' => 1)));
 
     $chats = erLhcoreClassModelChat::getList(array('sort' => 'id DESC', 'filterin' => array('id' => $id)));
 
@@ -27,34 +20,25 @@ if (!empty($id)) {
 
     foreach ($chats as $chat) {
         $item = array(
->>>>>>> master
-                'id' => $chat->id,
-                'nick' => $chat->nick,
-                'cs' => $chat->status,
-                'co' => $chat->user_id,
-                'dep' => (string)$chat->department,
-                'cn' => (string)$chat->country_name,
-                'us' => $chat->user_status_front,
-                'pnd_rsp' => $chat->pnd_rsp,
-                'um' => $chat->has_unread_op_messages,
-                'lmsg' => erLhcoreClassChat::formatSeconds(time() - ($chat->last_user_msg_time > 0 ? $chat->last_user_msg_time : $chat->time)),
-                'lmsg_id' => $chat->last_msg_id,
-                'cc' => ($chat->country_code != '' ? erLhcoreClassDesign::design('images/flags') . '/' . (string)$chat->country_code . '.png' : ''),
-                'msg' => erLhcoreClassChat::getGetLastChatMessagePending($chat->id, true, 3, ' » '),
-                'vwa' => ($chat->status != erLhcoreClassModelChat::STATUS_CLOSED_CHAT && $chat->last_user_msg_time > ($chat->last_op_msg_time > 0 ? $chat->last_op_msg_time : $chat->pnd_time) && (time() - $chat->last_user_msg_time > (int)erLhcoreClassModelChatConfig::fetchCache('vwait_to_long')->current_value) ? erLhcoreClassChat::formatSeconds(time() - $chat->last_user_msg_time) : null)
-            );
+            'id' => $chat->id,
+            'nick' => $chat->nick,
+            'cs' => $chat->status,
+            'co' => $chat->user_id,
+            'dep' => (string)$chat->department,
+            'cn' => (string)$chat->country_name,
+            'us' => $chat->user_status_front,
+            'pnd_rsp' => $chat->pnd_rsp,
+            'um' => $chat->has_unread_op_messages,
+            'lmsg' => erLhcoreClassChat::formatSeconds(time() - ($chat->last_user_msg_time > 0 ? $chat->last_user_msg_time : $chat->time)),
+            'lmsg_id' => $chat->last_msg_id,
+            'cc' => ($chat->country_code != '' ? erLhcoreClassDesign::design('images/flags') . '/' . (string)$chat->country_code . '.png' : ''),
+            'msg' => erLhcoreClassChat::getGetLastChatMessagePending($chat->id, true, 3, ' » '),
+            'vwa' => ($chat->status != erLhcoreClassModelChat::STATUS_CLOSED_CHAT && $chat->last_user_msg_time > ($chat->last_op_msg_time > 0 ? $chat->last_op_msg_time : $chat->pnd_time) && (time() - $chat->last_user_msg_time > (int)erLhcoreClassModelChatConfig::fetchCache('vwait_to_long')->current_value) ? erLhcoreClassChat::formatSeconds(time() - $chat->last_user_msg_time) : null)
+        );
 
-            $aicons = $chat->aicons;
-            if (!empty($aicons)) {
-                $item['aicons'] = $aicons;
-            }
-
-<<<<<<< HEAD
-            $response[] = $item;
-=======
         $chatIcons = [];
         foreach ($icons_additional as $iconAdditional) {
-            $columnIconData = json_decode($iconAdditional->column_icon,true);
+            $columnIconData = json_decode($iconAdditional->column_icon, true);
             if (isset($chat->{'cc_' . $iconAdditional->id})) {
                 $chatIcons[] = [
                     'has_popup' => $iconAdditional->has_popup,
@@ -70,8 +54,9 @@ if (!empty($id)) {
         $aicons = $chat->aicons;
         if (!empty($aicons)) {
             $item['aicons'] = $aicons;
->>>>>>> master
         }
+
+        $response[] = $item;
     }
 }
 
