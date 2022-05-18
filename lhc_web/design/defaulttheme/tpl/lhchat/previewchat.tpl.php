9e9b816a74ea81dadb35d0feeed0a50e59fcee3d
Remigijus Kiminas <remdex@gmail.com>
2022-05-18 01:08:24 -0400

    Escape keyword for quote

--- lhc_web/design/defaulttheme/tpl/lhchat/previewchat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/previewchat.tpl.php
@@ -30,7 +30,7 @@
     <?php $messages = array_reverse(erLhcoreClassModelmsg::getList(array('limit' => 100,'sort' => 'id DESC','filter' => array('chat_id' => $chat->id)))); ?>
     <?php if (isset($keyword) && !empty($keyword)) : ?>
         <?php foreach ($messages as $message) : ?>
-            <?php $message->msg = preg_replace('/\b(' . $keyword . ')\b/i','[level=bg-warning text-dark rounded p-1 d-inline-block][i][b][fs14]' . $keyword . '[/fs][/b][/i][/level]' ,$message->msg); ?>
+            <?php $message->msg = preg_replace('/\b(' . preg_quote($keyword,'/') . ')\b/is','[level=bg-warning text-dark rounded p-1 d-inline-block][i][b][fs14]' . $keyword . '[/fs][/b][/i][/level]', $message->msg); ?>
         <?php endforeach; ?>
     <?php endif; ?>
     <?php include(erLhcoreClassDesign::designtpl('lhchat/lists/msg_obj_list_admin.tpl.php'));?>
