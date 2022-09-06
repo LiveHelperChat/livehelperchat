e354e9ed976ad9cb8fb3afcb5952dadd0af049a4
Remigijus Kiminas <remdex@gmail.com>
2022-09-06 03:27:06 -0400

    Mail export support

--- lhc_web/modules/lhviews/exportview.php
+++ lhc_web/modules/lhviews/exportview.php
@@ -18,9 +18,15 @@ if ($search->scope == 'chat') {
     $tpl->set('action_url', erLhcoreClassDesign::baseurl('chat/list') . $append);
     echo $tpl->fetch();
     exit;
+} else if ($search->scope == 'mail') {
+    $tpl = erLhcoreClassTemplate::getInstance('lhmailconv/export_config.tpl.php');
+    $tpl->set('action_url', erLhcoreClassDesign::baseurl('mailconv/conversations') . $append);
+    echo $tpl->fetch();
+    exit;
 } else {
     erLhcoreClassChatEventDispatcher::getInstance()->dispatch('views.export', array(
-        'search' => $search
+        'search' => $search,
+        'append' => $append
     ));
 }
 
