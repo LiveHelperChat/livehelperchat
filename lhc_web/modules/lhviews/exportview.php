ca93db1db922b36c18a1096ba7fa9f38271d2e93
Remigijus Kiminas <remdex@gmail.com>
2022-09-06 03:56:51 -0400

    Apply same date filter on export

--- lhc_web/modules/lhviews/exportview.php
+++ lhc_web/modules/lhviews/exportview.php
@@ -11,6 +11,10 @@ if (isset($search->params_array['input_form']['view'])) {
     unset($search->params_array['input_form']['view']);
 }
 
+if ($search->days > 0) {
+    $search->params_array['input_form']['timefromts'] = time() - $search->days * 24 * 3600;
+}
+
 $append = erLhcoreClassSearchHandler::getURLAppendFromInput($search->params_array['input_form']);
 
 if ($search->scope == 'chat') {
@@ -20,7 +24,8 @@ if ($search->scope == 'chat') {
     exit;
 } else {
     erLhcoreClassChatEventDispatcher::getInstance()->dispatch('views.export', array(
-        'search' => $search
+        'search' => $search,
+        'append' => $append
     ));
 }
 
