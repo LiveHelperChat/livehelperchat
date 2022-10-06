0a016d1e195af2b267e68b55e08615ab9f4e87a6
Remigijus Kiminas <remdex@gmail.com>
2022-10-06 07:37:29 -0400

    Redirect back to referrer if exists

--- lhc_web/modules/lhchat/cannedmsg.php
+++ lhc_web/modules/lhchat/cannedmsg.php
@@ -59,7 +59,12 @@ if ($tab == 'cannedmsg') {
             // Do nothing
         }
 
-        erLhcoreClassModule::redirect('chat/cannedmsg');
+        if (isset($_SERVER['HTTP_REFERER'])){
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+        } else {
+            erLhcoreClassModule::redirect('chat/cannedmsg');
+        }
+
         exit;
     }
 
