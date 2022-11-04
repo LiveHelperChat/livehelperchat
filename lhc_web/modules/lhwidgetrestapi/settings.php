f7a8cda12bfdb976f56de74df77a667457e49b92
Remigijus Kiminas <remdex@gmail.com>
2022-11-04 09:08:02 -0400

    Put quotes

--- lhc_web/modules/lhwidgetrestapi/settings.php
+++ lhc_web/modules/lhwidgetrestapi/settings.php
@@ -286,7 +286,7 @@ $startDataDepartment = false;
 
 if (is_array($department) && !empty($department) && count($department) == 1) {
     $dep_id = $department[0];
-    $startDataDepartment = erLhcoreClassModelChatStartSettings::findOne(array('customfilter' => array("(JSON_CONTAINS(`dep_ids`," . (int)$dep_id . ",'$') OR department_id = " . (int)$dep_id . ")" )));
+    $startDataDepartment = erLhcoreClassModelChatStartSettings::findOne(array('customfilter' => array("(JSON_CONTAINS(`dep_ids`,'" . (int)$dep_id . "','$') OR department_id = " . (int)$dep_id . ")" )));
     if ($startDataDepartment instanceof erLhcoreClassModelChatStartSettings) {
         $startDataFields = $startDataDepartment->data_array;
     }
