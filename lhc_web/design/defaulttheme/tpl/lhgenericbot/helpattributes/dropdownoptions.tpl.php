a4efd3f74bde9fcd8cc6ae1e5b9298856997ca1f
Remigijus Kiminas <remdex@gmail.com>
2022-11-08 15:57:28 -0500

    More manual

--- lhc_web/design/defaulttheme/tpl/lhgenericbot/helpattributes/dropdownoptions.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhgenericbot/helpattributes/dropdownoptions.tpl.php
@@ -2,7 +2,7 @@
     <ul>
         <li>Option name || &lt;department_id OR department_alias&gt;</li>
         <li>Internal Value => Option name || &lt;department_id OR department_alias&gt;</li>
-        <li>If you are passing department make sure
+        <li>If you are passing department make sure. This is highly recommended as chat might not be able to start if you have custom required fields by department.
             <ul>
                 <li>You pass it for all options</li>
                 <li>All options have to be assigned to same start chat configuration</li>
