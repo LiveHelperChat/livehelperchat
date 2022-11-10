0acfd281e3c69345d542822eafe53c22122840de
Remigijus Kiminas <remdex@gmail.com>
2022-11-10 06:01:59 -0500

    Improve stats

--- lhc_web/design/defaulttheme/tpl/lhstatistic/campaignmodal.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhstatistic/campaignmodal.tpl.php
@@ -14,8 +14,14 @@ $modalSize = 'xl';
     <thead>
         <tr>
             <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('statistic/campaign','Parameter');?></th>
-            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('statistic/campaign','Value');?></th>
-            <?php foreach (erLhAbstractModelProactiveChatInvitation::getList(['filter' => ['parent_id' => $invitation->id]]) as $childInvitation) : $statsChild[$childInvitation->id] = erLhcoreClassChatStatistic::getProactiveStatistic(array('filter' => array('filter' => array('invitation_id' => $invitation->id, 'variation_id' => $childInvitation->id))))?>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('statistic/campaign','Total value');?></th>
+            <th>
+                <span class="material-icons">supervisor_account</span><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('statistic/campaign','Parent');?>
+            </th>
+            <?php $statsChild = []; foreach (erLhAbstractModelProactiveChatInvitation::getList(['filter' => ['parent_id' => $invitation->id]]) as $childInvitation) :
+                $statsChild[$childInvitation->id] = erLhcoreClassChatStatistic::getProactiveStatistic(array('filter' => array('filter' => array('invitation_id' => $invitation->id, 'variation_id' => $childInvitation->id))));
+                $statsChild[$childInvitation->id]['EXECUTED_TIMES'] = $childInvitation->executed_times;
+                ?>
                 <th><span class="material-icons">insert_invitation</span><?php echo htmlspecialchars($childInvitation->name);?></th>
             <?php endforeach; ?>
             <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('statistic/campaign','Explanation');?></th>
@@ -27,6 +33,9 @@ $modalSize = 'xl';
         </td>
         <td>
             <?php echo $stats['INV_SEND'];?>
+        </td>
+        <td>
+            <?php $total = 0; foreach ($statsChild as $statChild) {$total+=$statChild['INV_SEND'];} echo $stats['INV_SEND'] - $total; ?>
         </td>
         <?php foreach (erLhAbstractModelProactiveChatInvitation::getList(['filter' => ['parent_id' => $invitation->id]]) as $childInvitation) : ?>
         <td>
@@ -44,6 +53,9 @@ $modalSize = 'xl';
         <td>
             <?php echo $stats['INV_SHOWN'];?>
         </td>
+        <td>
+            <?php $total = 0; foreach ($statsChild as $statChild) {$total+=$statChild['INV_SHOWN'];} echo $stats['INV_SHOWN'] - $total; ?>
+        </td>
         <?php foreach (erLhAbstractModelProactiveChatInvitation::getList(['filter' => ['parent_id' => $invitation->id]]) as $childInvitation) : ?>
         <td>
             <?php echo $statsChild[$childInvitation->id]['INV_SHOWN'];?>
@@ -59,6 +71,9 @@ $modalSize = 'xl';
         </td>
         <td>
             <?php echo $stats['INV_SEEN'];?>
+        </td>
+        <td>
+            <?php $total = 0; foreach ($statsChild as $statChild) {$total+=$statChild['INV_SEEN'];} echo $stats['INV_SEEN'] - $total; ?>
         </td>
         <?php foreach (erLhAbstractModelProactiveChatInvitation::getList(['filter' => ['parent_id' => $invitation->id]]) as $childInvitation) : ?>
             <td>
@@ -76,6 +91,9 @@ $modalSize = 'xl';
         <td>
             <?php echo $stats['INV_CHAT_STARTED'];?>
         </td>
+        <td>
+            <?php $total = 0; foreach ($statsChild as $statChild) {$total+=$statChild['INV_CHAT_STARTED'];} echo $stats['INV_CHAT_STARTED'] - $total; ?>
+        </td>
         <?php foreach (erLhAbstractModelProactiveChatInvitation::getList(['filter' => ['parent_id' => $invitation->id]]) as $childInvitation) : ?>
             <td>
                 <?php echo $statsChild[$childInvitation->id]['INV_CHAT_STARTED'];?>
@@ -83,6 +101,25 @@ $modalSize = 'xl';
         <?php endforeach; ?>
         <td>
             <small><i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('statistic/campaign','Visitor started chat by online invitation');?></i></small>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <strong><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('statistic/campaign','Matched');?></strong>
+        </td>
+        <td>
+            <?php echo $invitation->executed_times;?>
+        </td>
+        <td>
+            <?php $total = 0; foreach ($statsChild as $statChild) {$total+=$statChild['EXECUTED_TIMES'];} echo $invitation->executed_times - $total; ?>
+        </td>
+        <?php foreach (erLhAbstractModelProactiveChatInvitation::getList(['filter' => ['parent_id' => $invitation->id]]) as $childInvitation) : ?>
+            <td>
+                <?php echo $childInvitation->executed_times;?>
+            </td>
+        <?php endforeach; ?>
+        <td>
+            <small><i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('statistic/campaign','Total times invitation was matched');?></i></small>
         </td>
     </tr>
 </table>
