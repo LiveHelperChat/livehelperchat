944d4ee2175167fd52efbdba5364683618ee9495
Remigijus Kiminas <remdex@gmail.com>
2022-11-12 09:10:24 -0500

    Do not rely on referrer if there is no other way

--- lhc_web/lib/models/lhchat/erlhcoreclassmodelchatonlineuser.php
+++ lhc_web/lib/models/lhchat/erlhcoreclassmodelchatonlineuser.php
@@ -927,7 +927,10 @@ class erLhcoreClassModelChatOnlineUser
             // Update variables only if it's not JS to check for operator message
             if (!isset($paramsHandle['check_message_operator']) || (isset($paramsHandle['pages_count']) && $paramsHandle['pages_count'] == true)) {
                 $item->user_agent = isset($_POST['ua']) ? $_POST['ua'] : (isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '');
-                $item->current_page = isset($_POST['l']) ? $_POST['l'] : (isset($_GET['l']) ? rawurldecode($_GET['l']) : self::getReferer());
+                $location = isset($_POST['l']) ? $_POST['l'] : (isset($_GET['l']) ? rawurldecode($_GET['l']) : ($item->current_page == '' ? self::getReferer() : null));
+                if ($location !== null) {
+                    $item->current_page = $location;
+                }
                 $item->page_title = isset($_POST['dt']) ? $_POST['dt'] : (isset($_GET['dt']) ? substr((string)rawurldecode($_GET['dt']),0,250) : '');
                 $item->last_visit = time();
                 $item->store_chat = true;
