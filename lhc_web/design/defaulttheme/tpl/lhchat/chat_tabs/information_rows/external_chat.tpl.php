ad191323f2eef7b4fae9399147845a9a5708c788
Remigijus Kiminas <remdex@gmail.com>
2022-12-14 07:08:37 -0500

    Allow extensions to have custom content for incoming webhooks

--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/information_rows/external_chat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/information_rows/external_chat.tpl.php
@@ -2,7 +2,11 @@
         (!empty($chat_variables_array) && isset($chat_variables_array['iwh_id']) && $iwh_id = $chat_variables_array['iwh_id']) ||
         ($chat->iwh_id > 0 && $iwh_id = $chat->iwh_id)
 ) : ?>
-    <?php if (($incomingWebhook = erLhcoreClassModelChatIncomingWebhook::fetch($iwh_id)) instanceof erLhcoreClassModelChatIncomingWebhook) : ?>
+    <?php if (($incomingWebhook = erLhcoreClassModelChatIncomingWebhook::fetch($iwh_id)) instanceof erLhcoreClassModelChatIncomingWebhook) : $customExternalRendered = false;?>
+
+            <?php include(erLhcoreClassDesign::designtpl('lhchat/chat_tabs/information_rows/external_chat_render_multiinclude.tpl.php'));?>
+
+            <?php if ($customExternalRendered == false) : ?>
             <div class="col-6 pb-1">
                 <span class="material-icons" <?php if ($incomingWebhook->icon_color != '') : ?>style="color: <?php echo htmlspecialchars($incomingWebhook->icon_color)?>"<?php endif;?> ><?php if ($incomingWebhook->icon != '') : ?><?php echo htmlspecialchars($incomingWebhook->icon)?><?php else : ?>extension<?php endif; ?></span>
                 <?php echo htmlspecialchars($incomingWebhook->name)?>
@@ -10,5 +14,7 @@
                 &nbsp;|&nbsp;<?php echo htmlspecialchars($chat_variables_array['iwh_field'])?>
                 <?php endif; ?>
             </div>
+            <?php endif; ?>
+
     <?php endif; ?>
 <?php endif; ?>
