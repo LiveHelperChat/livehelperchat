cd11d92db1d5bda2c02ce56e22233e74bb996f9f
Remigijus Kiminas <remdex@gmail.com>
2023-01-26 05:15:11 -0500

    Add missing index

--- lhc_web/doc/update_db/update_289.sql
+++ lhc_web/doc/update_db/update_289.sql
@@ -1,4 +1,5 @@
 ALTER TABLE `lh_chat` ADD INDEX `time` (`time`);
+ALTER TABLE `lh_audits` ADD INDEX `time` (`time`);
 ALTER TABLE `lh_departament_group_user` ADD `assign_priority` int(11) NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_departament_group_user` ADD `chat_min_priority` int(11) NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_departament_group_user` ADD `chat_max_priority` int(11) NOT NULL DEFAULT '0', COMMENT='';
