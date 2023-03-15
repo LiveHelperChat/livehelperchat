6762ce6f3f657126aa53a58f78e68beb8b73d599
Remigijus Kiminas <remdex@gmail.com>
2023-03-15 01:39:32 -0400

    Unique survey per chat session

--- lhc_web/lib/core/lhgenericbot/actionTypes/lhgenericbotactionsurvey.php
+++ lhc_web/lib/core/lhgenericbot/actionTypes/lhgenericbotactionsurvey.php
@@ -32,6 +32,17 @@ class erLhcoreClassGenericBotActionSurvey {
                 $surveyIds = array_diff($surveyIds, $surveyFilledIds);
             }
 
+            if (!empty($surveyIds) && isset($action['content']['survey_options']['unique_per_chat']) && $action['content']['survey_options']['unique_per_chat'] == true) {
+                $surveyFilledIds = [];
+                foreach (erLhcoreClassModelmsg::getList(['filterlike' => ['meta_msg' => 'survey_id'], 'filter' => ['chat_id' => $chat->id, 'user_id' => -2]]) as $msgPrevious) {
+                    $metaData = $msgPrevious->meta_msg_array;
+                    if (isset($metaData['content']['survey']['survey_id'])){
+                        $surveyFilledIds[] = $metaData['content']['survey']['survey_id'];
+                    }
+                }
+                $surveyIds = array_diff($surveyIds, $surveyFilledIds);
+            }
+
             $msg->msg = "";
 
             // No survey to fill
