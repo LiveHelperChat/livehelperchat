647b29094d4491c4ff6ebea85cfc305336d6c7e8
Remigijus Kiminas <remdex@gmail.com>
2023-04-03 05:07:51 -0400

    Convert to string always

--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/information_rows/chat_owner.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/information_rows/chat_owner.tpl.php
@@ -3,7 +3,7 @@
         <h6 class="fw-bold"><i class="material-icons">account_box</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Chat owner')?></h6>
         <div class="row">
             <div class="col-6 pb-1">
-                <div class="text-muted" id="chat-owner-<?php echo $chat->id?>" user-id="<?php echo $chat->user_id?>" title="<?php echo htmlspecialchars(\LiveHelperChat\Models\Departments\UserDepAlias::getAlias(array('scope' => 'as_string', 'chat' => $chat)));?>">
+                <div class="text-muted" id="chat-owner-<?php echo $chat->id?>" user-id="<?php echo $chat->user_id?>" title="<?php echo htmlspecialchars((string)\LiveHelperChat\Models\Departments\UserDepAlias::getAlias(array('scope' => 'as_string', 'chat' => $chat)));?>">
                     <?php $user = $chat->getChatOwner();  if ($user !== false) : ?>
                         <span class="material-icons action-image" onclick='lhc.revealModal({"iframe" : true, "title" : <?php echo json_encode(htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Concurrent chats.')), JSON_HEX_QUOT)?>, height: "600", "url":WWW_DIR_JAVASCRIPT+"chat/chathistory/<?php echo $chat->id?>"})' title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Operator chats during this conversations.')?>">history</span><?php echo htmlspecialchars($user->name.' '.$user->surname)?>
                     <?php endif; ?>
