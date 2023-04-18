0e2e7e91b5e91b77fa440a409862582f5fad7106
Remigijus Kiminas <remdex@gmail.com>
2023-04-18 03:07:52 -0400

    Chat archive support next/previous chats quick navigation

--- lhc_web/design/defaulttheme/js/lh/lh.js
+++ lhc_web/design/defaulttheme/js/lh/lh.js
@@ -10,13 +10,13 @@ __webpack_public_path__ = window.WWW_DIR_LHC_WEBPACK;
      function showPreviewClickListener(evt) {
 
         if (evt.altKey && (evt.which == 38 || evt.which == 40)) {
-
-            if (evt.which == 40 && nextChatId > 0) {
-               $('#preview-item-'+nextChatId).click();
+            if (evt.which == 40) {
+                if (nextChatId > 0) {
+                    $('#preview-item-'+nextChatId).click();
+                }
             } else if (prevChatId > 0) {
                 $('#preview-item-'+prevChatId).click();
             }
-
             return;
         }
      }
@@ -24,8 +24,7 @@ __webpack_public_path__ = window.WWW_DIR_LHC_WEBPACK;
 	 global.lhc = {
 			previewChat : function(chat_id, event) {
 
-                var keyword = '';
-                var navigatorList = '';
+                var keyword = '',navigatorList = '';
 
                 prevChatId = 0, nextChatId = 0;
 
@@ -37,28 +36,11 @@ __webpack_public_path__ = window.WWW_DIR_LHC_WEBPACK;
                         $(event).addClass('bg-current');
                     }
 
-                    if (typeof event.getAttribute('data-list-navigate') !== 'undefined') {
-
-                        $('.chat-row-tr.bg-light').removeClass('bg-light');
-                        $('#chat-row-tr-'+chat_id).addClass('bg-light');
-
-                        prevChatId = $(event).parent().parent().prev().attr('data-chat-id');
-                        nextChatId = $(event).parent().parent().next().attr('data-chat-id');
-
-                        if (prevChatId) {
-                            navigatorList = '&prevId=' + prevChatId;
-                            document.addEventListener("keyup", showPreviewClickListener);
-                        }
-
-                        if (nextChatId) {
-                            navigatorList = navigatorList + '&nextId=' + nextChatId;
-                            document.addEventListener("keyup", showPreviewClickListener);
-                        }
-                    }
+                    navigatorList = this.attachNavigator(chat_id, event);
 
                 }
 
-				this.revealModal({'url':WWW_DIR_JAVASCRIPT+'chat/previewchat/'+chat_id + '?keyword=' + (keyword || '') + navigatorList ,
+				this.revealModal({'url':WWW_DIR_JAVASCRIPT+'chat/previewchat/'+chat_id + '?keyword=' + (keyword || '') + navigatorList,
                     'showcallback' : function() {
                         document.addEventListener("keyup", showPreviewClickListener);
                     },
@@ -67,16 +49,53 @@ __webpack_public_path__ = window.WWW_DIR_LHC_WEBPACK;
                 }});
 			},
 
+            attachNavigator : function(chat_id, event) {
+                var navigatorLink = '';
+
+                if (typeof event.getAttribute('data-list-navigate') !== 'undefined') {
+
+                    $('.chat-row-tr.bg-light').removeClass('bg-light');
+                    $('#chat-row-tr-'+chat_id).addClass('bg-light');
+
+                    prevChatId = $(event).parent().parent().prev().attr('data-chat-id');
+                    nextChatId = $(event).parent().parent().next().attr('data-chat-id');
+
+                    if (prevChatId) {
+                        navigatorLink = '&prevId=' + prevChatId;
+                        document.addEventListener("keyup", showPreviewClickListener);
+                    }
+
+                    if (nextChatId) {
+                        navigatorLink = navigatorLink + '&nextId=' + nextChatId;
+                        document.addEventListener("keyup", showPreviewClickListener);
+                    }
+                }
+
+                return navigatorLink;
+            },
+
           	previewChatArchive : function(archive_id, chat_id, event) {
-                var keyword = '';
+                var keyword = '',navigatorList = '';
+                prevChatId = 0, nextChatId = 0;
+
                 if (event) {
                     keyword = typeof event.getAttribute('data-keyword') !== 'undefined' ? event.getAttribute('data-keyword') : '';
                     if (event.classList.contains('preview-list')){
                         $('.preview-list').removeClass('bg-current');
                         $(event).addClass('bg-current');
                     }
+
+                    navigatorList = this.attachNavigator(chat_id, event);
                 }
-				this.revealModal({'url':WWW_DIR_JAVASCRIPT+'chatarchive/previewchat/'+archive_id+'/'+chat_id + '?keyword=' + (keyword || '')});
+
+				this.revealModal({'url':WWW_DIR_JAVASCRIPT+'chatarchive/previewchat/'+archive_id+'/'+chat_id + '?keyword=' + (keyword || '') + navigatorList,
+                    'showcallback' : function() {
+                        document.addEventListener("keyup", showPreviewClickListener);
+                    },
+                    'hidecallback' : function() {
+                        document.removeEventListener("keyup", showPreviewClickListener);
+                    }
+                });
 			},
 
 			revealModal : function(params) {				
--- lhc_web/design/defaulttheme/tpl/lhchat/previewchat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/previewchat.tpl.php
@@ -92,16 +92,5 @@
       })(<?php echo $chat->id,',',$msg['id']?>);
   </script>
 
-          <?php if (erConfigClassLhConfig::getInstance()->getSetting( 'site', 'debug_output' ) == true) {
-              $debug = ezcDebug::getInstance();
-              echo "<div><pre class='bg-light text-dark m-2 p-2 border'>" . json_encode(erLhcoreClassUser::$permissionsChecks, JSON_PRETTY_PRINT) . "</pre></div>";
-              echo $debug->generateOutput();
-          } ?>
-      </div>
+          <?php include(erLhcoreClassDesign::designtpl('lhkernel/modal_footer.tpl.php'));?>
 
-
-
-        
-        
-    </div>
-</div>
