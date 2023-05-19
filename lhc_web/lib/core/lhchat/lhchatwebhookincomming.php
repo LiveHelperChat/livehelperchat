06821407d81f665449abd9391ddbd35a569c53bf
Remigijus Kiminas <remdex@gmail.com>
2023-05-19 03:10:11 -0400

    xlsx mimetype

--- lhc_web/lib/core/lhchat/lhchatwebhookincomming.php
+++ lhc_web/lib/core/lhchat/lhchatwebhookincomming.php
@@ -1479,6 +1479,7 @@ class erLhcoreClassChatWebhookIncoming {
             // open office
             'odt' => 'application/vnd.oasis.opendocument.text',
             'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+            'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
         );
 
         if ($getMime == false) {
