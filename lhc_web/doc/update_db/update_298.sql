134d675416d7ab6b49fb2f77db1a6162ba77eadf
Remigijus Kiminas <remdex@gmail.com>
2023-05-31 08:41:22 -0400

    Updates

--- lhc_web/doc/update_db/update_298.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `lh_abstract_chat_priority` ADD `role_destination` varchar(50) NOT NULL, COMMENT='';
-ALTER TABLE `lh_abstract_chat_priority` ADD `present_role_is` varchar(50) NOT NULL, COMMENT='';
