4202dc80baccad10528e11461dcc4146fd3a054a
Remigijus Kiminas <remdex@gmail.com>
2023-08-07 02:22:20 -0400

    Mailbox status filter

--- lhc_web/lib/core/lhmailconv/filter/mailbox.php
+++ lhc_web/lib/core/lhmailconv/filter/mailbox.php
@@ -38,6 +38,18 @@ $fieldsSearch['sync_status'] = array (
     )
 );
 
+$fieldsSearch['active'] = array (
+    'type' => 'checkbox',
+    'trans' => 'Failed',
+    'required' => false,
+    'valid_if_filled' => false,
+    'filter_type' => 'filter',
+    'filter_table_field' => 'active',
+    'validation_definition' => new ezcInputFormDefinitionElement (
+        ezcInputFormDefinitionElement::OPTIONAL, 'int', array('min_range' => 0, 'max_range' => 1)
+    )
+);
+
 $fieldSortAttr = array (
     'field'      => false,
     'default'    => false,
