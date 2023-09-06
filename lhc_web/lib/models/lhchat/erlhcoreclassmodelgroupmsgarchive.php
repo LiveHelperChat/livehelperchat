602353511ef7b2e2a3ae3710019af6f96eee74d2
Remigijus Kiminas <remdex@gmail.com>
2023-09-06 08:28:29 -0400

    Move place

--- lhc_web/lib/models/lhchat/erlhcoreclassmodelgroupmsgarchive.php
+++ lhc_web/lib/models/lhchat/erlhcoreclassmodelgroupmsgarchive.php
@@ -1,8 +1,8 @@
 <?php
-#[\AllowDynamicProperties]
 /**
  * Just extends class to support proper class loading
  * */
+#[\AllowDynamicProperties]
 class erLhcoreClassModelGroupMsgArchive extends erLhcoreClassModelGroupMsg {
 
     public static $dbTable = null;
