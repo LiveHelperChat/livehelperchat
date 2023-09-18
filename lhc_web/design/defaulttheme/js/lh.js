ef7deda2f0cfd315fe198d53c3e35c525c3b8e20
Remigijus Kiminas <remdex@gmail.com>
2023-09-18 07:28:10 -0400

    Hide right column on smaller res
    This was feedback from one of the customers as they use chattabs

--- lhc_web/design/defaulttheme/js/lh.js
+++ lhc_web/design/defaulttheme/js/lh.js
@@ -1081,7 +1081,7 @@ function lh(){
     {
     	if ($('#chat-main-column-'+chat_id+' .collapse-right').text() == 'chevron_right'){
 	    	$('#chat-right-column-'+chat_id).hide();
-	    	$('#chat-main-column-'+chat_id).removeClass('col-xl-8').addClass('col-xl-12');
+	    	$('#chat-main-column-'+chat_id).removeClass('col-md-8').addClass('col-md-12');
 	    	$('#chat-main-column-'+chat_id+' .collapse-right').text('chevron_left');
 	    	try {
 		    	if (localStorage) {
@@ -1090,7 +1090,7 @@ function lh(){
 	    	} catch(e) {}
     	} else {
     		$('#chat-right-column-'+chat_id).show();
-	    	$('#chat-main-column-'+chat_id).removeClass('col-xl-12').addClass('col-xl-8');
+	    	$('#chat-main-column-'+chat_id).removeClass('col-md-12').addClass('col-md-8');
 	    	$('#chat-main-column-'+chat_id+' .collapse-right').text('chevron_right');
 	    	
 	    	try {
