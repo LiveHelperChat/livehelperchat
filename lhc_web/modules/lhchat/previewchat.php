ef0003768201152aba20c01886821e3f9e505320
Remigijus Kiminas <remdex@gmail.com>
2023-10-17 09:03:24 -0400

    Support field name in keyword highlight

--- lhc_web/modules/lhchat/previewchat.php
+++ lhc_web/modules/lhchat/previewchat.php
@@ -6,16 +6,27 @@ $chat = erLhcoreClassChat::getSession()->load( 'erLhcoreClassModelChat', $Params
 
 if ( erLhcoreClassChat::hasAccessToRead($chat) ) {
 
+    // Support older PHP versions
+    if (!function_exists('str_ends_with')) {
+        function str_ends_with(string $haystack, string $needle): bool
+        {
+            $needle_len = strlen($needle);
+            return ($needle_len === 0 || 0 === substr_compare($haystack, $needle, - $needle_len));
+        }
+    }
+
     $keyword = isset($_GET['keyword']) ? (string)$_GET['keyword'] : '';
+    $keyword = str_replace(":",': ',$keyword);
     preg_match_all('~(?|"([^"]+)"|(\S+))~', $keyword, $matches);
     $keywords = [];
     if (isset($matches[1]) && !empty($matches[1])){
         foreach ($matches[1] as $potentionalKeyword) {
-            if (trim(str_ireplace(['and','or'],'',$potentionalKeyword)) != '') {
+            if (trim(str_ireplace(['and','or'],'',$potentionalKeyword)) != '' && str_ends_with($potentionalKeyword,":") === false) {
                 $keywords[] = $potentionalKeyword;
             }
         }
     }
+
     $tpl->set('keyword',$keywords);
     $tpl->set('chat',$chat);
     $tpl->set('see_sensitive_information', $currentUser->hasAccessTo('lhchat','see_sensitive_information'));
