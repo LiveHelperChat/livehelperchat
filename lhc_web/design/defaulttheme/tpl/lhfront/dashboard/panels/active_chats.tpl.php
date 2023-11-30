20a314fc2075842b308362b5ea8b6d9b36f44b5f
Remigijus Kiminas <remdex@gmail.com>
2023-11-30 09:03:49 -0500

    PRogress

--- lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/active_chats.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/active_chats.tpl.php
@@ -6,7 +6,7 @@
         $permissionsWidget[] = 'lhstatistic_statisticdep';
     }
     ?>
-    <lhc-widget permissions='<?php echo json_encode($permissionsWidget);?>' <?php if (isset($rightPanelMode)) : ?>right_panel_mode="true"<?php endif; ?> <?php if (isset($hideCardHeader)) : ?>hide_header="true"<?php endif;?> type="active_chats" sort_identifier="active_chats_sort" icon_class="chat-active" type="active_chats" status_id="1" expand_identifier="activec_widget_exp" list_identifier="active-chats" panel_list_identifier="actived-panel-list" optionsPanel={"panelid":"actived","limitid":"limita","userid":"activeu"} www_dir_flags="<?php echo erLhcoreClassDesign::design('images/flags');?>"></lhc-widget>
+    <lhc-widget <?php if (isset($customCardNoDuration)) : ?>no_duration="<?php echo $customCardNoDuration?>"<?php endif; ?> <?php if (isset($customCardTitleClass)) : ?>custom_title_class="<?php echo $customCardTitleClass?>"<?php endif; ?> permissions='<?php echo json_encode($permissionsWidget);?>' <?php if (isset($rightPanelMode)) : ?>right_panel_mode="true"<?php endif; ?> <?php if (isset($hideCardHeader)) : ?>hide_header="true"<?php endif;?> type="active_chats" sort_identifier="active_chats_sort" icon_class="chat-active" type="active_chats" status_id="1" expand_identifier="activec_widget_exp" list_identifier="active-chats" panel_list_identifier="actived-panel-list" optionsPanel={"panelid":"actived","limitid":"limita","userid":"activeu"} www_dir_flags="<?php echo erLhcoreClassDesign::design('images/flags');?>"></lhc-widget>
 
     <?php /*
 	<div class="card card-dashboard card-active-chats" data-panel-id="active_chats" ng-init="lhc.getToggleWidget('activec_widget_exp');lhc.getToggleWidgetSort('active_chats_sort');">
