a92e028b345d93c067fd1001596454433924310f
Remigijus Kiminas <remdex@gmail.com>
2024-02-09 07:33:18 -0500

    fix #2051

--- lhc_web/modules/lhwidgetrestapi/settings.php
+++ lhc_web/modules/lhwidgetrestapi/settings.php
@@ -537,7 +537,7 @@ $ts = time();
 $outputResponse['wv'] = 224;
  
 // React APP versions
-$outputResponse['v'] = 323;
+$outputResponse['v'] = 324;
 
 $outputResponse['hash'] = sha1(erLhcoreClassIPDetect::getIP() . $ts . erConfigClassLhConfig::getInstance()->getSetting( 'site', 'secrethash' ));
 $outputResponse['hash_ts'] = $ts;
