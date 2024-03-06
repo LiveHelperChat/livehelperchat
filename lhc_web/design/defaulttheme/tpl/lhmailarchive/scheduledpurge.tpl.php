4d43ebce0397bf37c14c4b85253527521a23ac25
Remigijus Kiminas <remdex@gmail.com>
2024-03-06 08:23:41 -0500

    Change text

--- lhc_web/design/defaulttheme/tpl/lhmailarchive/scheduledpurge.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhmailarchive/scheduledpurge.tpl.php
@@ -27,7 +27,11 @@
                 <?php elseif ($item->status == \LiveHelperChat\Models\mailConv\Delete\DeleteFilter::STATUS_IN_PROGRESS) : ?>
                     <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatarchive/list','In progress');?>
                 <?php else : ?>
-                    <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatarchive/list','Finished');?>
+                    <?php if ($item->records_count == 0) : ?>
+                        <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatarchive/list','Finished');?>
+                    <?php else : ?>
+                        <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatarchive/list','Finished collecting');?>
+                    <?php endif; ?>
                 <?php endif; ?>
             </td>
             <td>
@@ -46,7 +50,7 @@
                 <a title="<?php echo htmlspecialchars($item->filter)?>" class="btn btn-secondary btn-xs" href="<?php echo erLhcoreClassDesign::baseurl('mailconv/conversations')?><?php echo $item->filter_input_url?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatarchive/list','List mails');?></a>
             </td>
             <td>
-                <?php echo $item->records_count?>, <?php echo $item->records_count_progress?> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatarchive/list','of them in progress');?>
+                <?php echo $item->records_count?> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatarchive/list','found');?>, <?php echo $item->records_count_progress?> <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chatarchive/list','of them in progress');?>
             </td>
             <td>
                 <?php echo $item->last_id?>
