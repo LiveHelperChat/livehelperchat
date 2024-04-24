e6e77f996253f6e5e6f3204739fa7c8e8b979de3
Remigijus Kiminas <remdex@gmail.com>
2024-04-24 04:59:17 -0400

    Add sort by offline time

--- lhc_web/modules/lhchat/syncadmininterface.php
+++ lhc_web/modules/lhchat/syncadmininterface.php
@@ -747,7 +747,7 @@ if ($canListOnlineUsers == true || $canListOnlineUsersAll == true) {
     $validSort = array(
         'onn_dsc' => 'name DESC',
         'onn_asc' => 'name ASC',
-        'onl_dsc' => 'hide_online DESC, name ASC',
+        'onl_dsc' => 'hide_online DESC, hide_online_ts DESC, name ASC',
         'onl_asc' => 'hide_online ASC, name ASC',
         'ac_dsc' => 'active_chats DESC, name ASC',
         'ac_asc' => 'active_chats ASC, name ASC',
