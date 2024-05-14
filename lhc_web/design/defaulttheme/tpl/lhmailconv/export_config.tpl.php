6cdf2c57acd4263af48c40d879118f95352fef01
Remigijus Kiminas <remdex@gmail.com>
2024-05-14 04:37:20 -0400

    Prepare for keyword as request parameter

--- lhc_web/design/defaulttheme/tpl/lhmailconv/export_config.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhmailconv/export_config.tpl.php
@@ -9,7 +9,7 @@ $appendPrintExportURL = '';
 
 <?php include(erLhcoreClassDesign::designtpl('lhchat/lists/search_panel_append_print_multiinclude.tpl.php'));?>
 
-<form action="<?php echo htmlspecialchars($action_url)?>/(export)/1?<?php echo $appendPrintExportURL?>" method="post" target="_blank">
+<form action="<?php echo htmlspecialchars($action_url)?>/(export)/1?<?php echo htmlspecialchars(isset($query_url) ? $query_url : '')?><?php echo !empty($appendPrintExportURL) ? '&amp;' . $appendPrintExportURL : ''?>" method="post" target="_blank">
     <div class="modal-body">
         <div class="row">
             <div class="col-6">
