ec5cc1fab0392765b396806268640834ca3c0758
Remigijus Kiminas <remdex@gmail.com>
2024-05-23 02:30:18 -0400

    Fix widget background if color-scheme tag is used in website

--- lhc_web/design/defaulttheme/widget/wrapper/src/index.js
+++ lhc_web/design/defaulttheme/widget/wrapper/src/index.js
@@ -55,7 +55,7 @@
             lhc.loaded = false;
             lhc.connected = false;
             lhc.ready = false;
-            lhc.version = 225;
+            lhc.version = 226;
 
             const isMobileItem = require('ismobilejs');
             var isMobile = isMobileItem.default(global.navigator.userAgent).phone;
