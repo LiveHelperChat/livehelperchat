79e8c742e7a368d18a50d56ce72b263e3dd2f9c5
Remigijus Kiminas <remdex@gmail.com>
2024-08-12 02:54:47 -0400

    Hide canned messages block if no permission to use it

--- lhc_web/design/defaulttheme/tpl/lhchat/part/action_block.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/part/action_block.tpl.php
@@ -1,3 +1,3 @@
-<?php if ($canEditChat == true && (!isset($writeRemoteDisabled) || $writeRemoteDisabled == false)) :  ?>
+<?php if ($canEditChat == true && (!isset($writeRemoteDisabled) || $writeRemoteDisabled == false) && erLhcoreClassUser::instance()->hasAccessTo('lhcannedmsg','use')) :  ?>
 	<?php include(erLhcoreClassDesign::designtpl('lhchat/part/canned_messages_action.tpl.php')); ?>
 <?php endif; ?>
