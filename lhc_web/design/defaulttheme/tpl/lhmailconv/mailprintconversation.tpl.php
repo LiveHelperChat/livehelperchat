ac96cb792fbe6988ba913f85dab6ec6c465ff895
Remigijus Kiminas <remdex@gmail.com>
2024-09-02 16:03:21 -0400

    Sensitive information for print

--- lhc_web/design/defaulttheme/tpl/lhmailconv/mailprintconversation.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhmailconv/mailprintconversation.tpl.php
@@ -1,14 +1,24 @@
-<?php foreach (erLhcoreClassModelMailconvMessage::getList(['filter' => ['conversation_id' => $chat->id]]) as $message) : ?>
-<h5><?php echo htmlspecialchars($message->subject)?></h5>
+<?php foreach (erLhcoreClassModelMailconvMessage::getList(['filter' => ['conversation_id' => $chat->id]]) as $message) :
 
-<ul>
-    <li><b><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/mailconvprint','From');?></b> <?php echo htmlspecialchars($message->from_name)?> &lt;<?php echo htmlspecialchars($message->from_address)?>&gt; <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/mailconvprint','at');?> <?php echo htmlspecialchars($message->udate_front)?></li>
-    <li><b><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/mailconvprint','To');?></b> <?php echo htmlspecialchars($message->to_data_front)?></li>
-</ul>
+    if (!erLhcoreClassUser::instance()->hasAccessTo('lhmailconv','mail_see_unhidden_email')) {
+        $message->setSensitive(true);
 
-<hr>
+        if ($message->response_type !== erLhcoreClassModelMailconvMessage::RESPONSE_INTERNAL) {
+            $message->from_address = \LiveHelperChat\Helpers\Anonymizer::maskEmail($message->from_address);
+        }
+    }
 
-<?php echo $message->body_front ?>
+    ?>
+    <h5><?php echo htmlspecialchars($message->subject)?></h5>
 
-<hr>
+    <ul>
+        <li><b><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/mailconvprint','From');?></b> <?php echo htmlspecialchars($message->from_name)?> &lt;<?php echo htmlspecialchars($message->from_address)?>&gt; <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/mailconvprint','at');?> <?php echo htmlspecialchars($message->udate_front)?></li>
+        <li><b><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/mailconvprint','To');?></b> <?php echo htmlspecialchars($message->to_data_front)?></li>
+    </ul>
+
+    <hr>
+
+    <?php echo $message->body_front ?>
+
+    <hr>
 <?php endforeach; ?>
--- lhc_web/modules/lhmailconv/mailprintcovnersation.php
+++ lhc_web/modules/lhmailconv/mailprintcovnersation.php
@@ -1,9 +1,24 @@
 <?php
 
-$mail = erLhcoreClassModelMailconvConversation::fetch($Params['user_parameters']['id']);
+$conv = erLhcoreClassModelMailconvConversation::fetch($Params['user_parameters']['id']);
+
+if (!erLhcoreClassUser::instance()->hasAccessTo('lhmailconv','mail_see_unhidden_email')) {
+    $conv->from_address = \LiveHelperChat\Helpers\Anonymizer::maskEmail($conv->from_address);
+}
+
+if (isset($conv->phone)) {
+    $conv->phone_front = $conv->phone;
+
+    if ($conv->phone != '' && !erLhcoreClassUser::instance()->hasAccessTo('lhmailconv','phone_see_unhidden')) {
+        $conv->phone_front = \LiveHelperChat\Helpers\Anonymizer::maskPhone($conv->phone);
+        if (!erLhcoreClassUser::instance()->hasAccessTo('lhmailconv','have_phone_link')) {
+            $conv->phone = '';
+        }
+    }
+}
 
 $tpl = erLhcoreClassTemplate::getInstance('lhmailconv/mailprintconversation.tpl.php');
-$tpl->set('chat',$mail);
+$tpl->set('chat',$conv);
 
 $Result['content'] = $tpl->fetch();
 $Result['pagelayout'] = 'print';
