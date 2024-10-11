0fbb96e1eb33942a64aa369c0c230fa4182922e3
Remigijus Kiminas <remdex@gmail.com>
2024-10-11 06:44:39 -0400

    Use loop to set table properties

--- lhc_web/pos/lhdepartament/erlhcoreclassmodeldepartamentavailability.php
+++ lhc_web/pos/lhdepartament/erlhcoreclassmodeldepartamentavailability.php
@@ -9,45 +9,12 @@ $def->idProperty->columnName = 'id';
 $def->idProperty->propertyName = 'id';
 $def->idProperty->generator = new ezcPersistentGeneratorDefinition(  'ezcPersistentNativeGenerator' );
 
-$def->properties['dep_id'] = new ezcPersistentObjectProperty();
-$def->properties['dep_id']->columnName   = 'dep_id';
-$def->properties['dep_id']->propertyName = 'dep_id';
-$def->properties['dep_id']->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
-
-$def->properties['hourminute'] = new ezcPersistentObjectProperty();
-$def->properties['hourminute']->columnName   = 'hourminute';
-$def->properties['hourminute']->propertyName = 'hourminute';
-$def->properties['hourminute']->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
-
-$def->properties['hour'] = new ezcPersistentObjectProperty();
-$def->properties['hour']->columnName   = 'hour';
-$def->properties['hour']->propertyName = 'hour';
-$def->properties['hour']->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
-
-$def->properties['minute'] = new ezcPersistentObjectProperty();
-$def->properties['minute']->columnName   = 'minute';
-$def->properties['minute']->propertyName = 'minute';
-$def->properties['minute']->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
-
-$def->properties['time'] = new ezcPersistentObjectProperty();
-$def->properties['time']->columnName   = 'time';
-$def->properties['time']->propertyName = 'time';
-$def->properties['time']->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
-
-$def->properties['status'] = new ezcPersistentObjectProperty();
-$def->properties['status']->columnName   = 'status';
-$def->properties['status']->propertyName = 'status';
-$def->properties['status']->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
-
-$def->properties['ymdhi'] = new ezcPersistentObjectProperty();
-$def->properties['ymdhi']->columnName   = 'ymdhi';
-$def->properties['ymdhi']->propertyName = 'ymdhi';
-$def->properties['ymdhi']->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
-
-$def->properties['ymd'] = new ezcPersistentObjectProperty();
-$def->properties['ymd']->columnName   = 'ymd';
-$def->properties['ymd']->propertyName = 'ymd';
-$def->properties['ymd']->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
+foreach (['dep_id','hourminute','hour','minute','time','status','ymdhi','ymd'] as $posAttr) {
+    $def->properties[$posAttr] = new ezcPersistentObjectProperty();
+    $def->properties[$posAttr]->columnName   = $posAttr;
+    $def->properties[$posAttr]->propertyName = $posAttr;
+    $def->properties[$posAttr]->propertyType = ezcPersistentObjectProperty::PHP_TYPE_INT;
+}
 
 return $def;
 
