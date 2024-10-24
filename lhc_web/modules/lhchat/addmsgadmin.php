17ba833321ce75854120518f194085448d02aba0
Remigijus Kiminas <remdex@gmail.com>
2024-10-24 09:36:23 -0400

    few fixes
    * We should use same header sizes for back office also
    * If operator takes over a chat we should unlock send area

--- lhc_web/modules/lhchat/addmsgadmin.php
+++ lhc_web/modules/lhchat/addmsgadmin.php
@@ -252,6 +252,20 @@ if (trim($form->msg) != '')
 
                         $Chat->user_id = $currentUser->getUserID();
 
+                        // If operator takes over and task is not finished we want to unlock text field for visitor
+                        if (isset($Chat->chat_variables_array['bot_lock_msg'])) {
+                            $chatVariables = $Chat->chat_variables_array;
+                            unset($chatVariables['bot_lock_msg']);
+                            $Chat->chat_variables_array = $chatVariables;
+                            $Chat->chat_variables = json_encode($chatVariables);
+                        }
+
+                        if (isset($chat->chat_variables_array['bot_lock_msg'])) {
+                            $r = erTranslationClassLhTranslation::getInstance()->getTranslation('chat/startchat','We are still working on your previous request!');
+                            echo erLhcoreClassChat::safe_json_encode(array('error' => true, 'r' => $r));
+                            exit;
+                        }
+
                         // User status in event of chat acceptance
                         $Chat->usaccept = $userData->hide_online;
                         $Chat->operation_admin .= "lhinst.updateVoteStatus(".$Chat->id.");";
