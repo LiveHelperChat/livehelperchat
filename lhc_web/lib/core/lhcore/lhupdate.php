5ab1dcf69e59bfdc03de0c44b362599197d1c069
Remigijus Kiminas <remdex@gmail.com>
2024-12-16 02:58:02 -0500

    Change log for 4.52v

--- lhc_web/lib/core/lhcore/lhupdate.php
+++ lhc_web/lib/core/lhcore/lhupdate.php
@@ -4,7 +4,7 @@ class erLhcoreClassUpdate
 {
 
     const DB_VERSION = 321;
-    const LHC_RELEASE = 451;
+    const LHC_RELEASE = 452;
 
 	public static function doTablesUpdate($definition){
 		$updateInformation = self::getTablesStatus($definition);
