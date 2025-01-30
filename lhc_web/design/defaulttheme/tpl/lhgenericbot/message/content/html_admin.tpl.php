0cf17313d32d8654870db3430f940382ba3ca5ca
Remigijus Kiminas <remdex@gmail.com>
2025-01-30 03:13:46 -0500

    In html rendering we should remove new line characters and use HTML

--- lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/html_admin.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/html_admin.tpl.php
@@ -5,7 +5,7 @@
             <pre class="blockquote blockquote-code" style="resize: vertical; height: 100px; min-height: 100px"><?php echo htmlspecialchars($metaMessage['content']);?></pre>
         </div>
     <?php else : ?>
-        <?php $msgBody = '[html]'.$metaMessage['content'].'[/html]'; $paramsMessageRender = array('sender' => (is_object($msg) ? $msg->user_id : $msg['user_id']));?>
+        <?php $msgBody = '[html]'.str_replace(["\r","\n"],["",""],$metaMessage['content']).'[/html]'; $paramsMessageRender = array('sender' => (is_object($msg) ? $msg->user_id : $msg['user_id']));?>
         <?php include(erLhcoreClassDesign::designtpl('lhchat/lists/msg_body.tpl.php'));?>
     <?php endif; ?>
 
