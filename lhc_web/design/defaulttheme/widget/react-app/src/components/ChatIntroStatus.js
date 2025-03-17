48a73a41aaa87a73cf807026704dd89db52e7547
Remigijus Kiminas <remdex@gmail.com>
2025-03-17 04:43:14 -0400

    Avoid 1px shift for settings icon

--- lhc_web/design/defaulttheme/widget/react-app/src/components/ChatIntroStatus.js
+++ lhc_web/design/defaulttheme/widget/react-app/src/components/ChatIntroStatus.js
@@ -19,7 +19,7 @@ class ChatIntroStatus extends PureComponent {
             {this.props.profileBefore !== null && <div dangerouslySetInnerHTML={{__html:this.props.profileBefore}}></div>}
             <div className={this.props.msg_expand} id="messagesBlock" dangerouslySetInnerHTML={{__html:this.props.messagesBefore}}></div>
             {!this.props.hideMessageField && <div className="d-flex flex-row border-top position-relative message-send-area">
-                <div className="btn-group dropup disable-select ps-1 pt-2"><i className="material-icons settings text-muted" id="chat-dropdown-options" aria-haspopup="true" aria-expanded="false">&#xf100;</i></div>
+                <div className="btn-group dropup disable-select ps-1 pt-2"><button type="button" class="border-0 p-0 material-icons settings text-muted" id="chat-dropdown-options" role="button" data-bs-toggle="dropdown" tabindex="0" aria-haspopup="true" aria-expanded="false">&#xf100;</button></div>
                 <div className="mx-auto w-100">
                     {/* <textarea aria-label="Type your message here..." defaultValue={this.props.value} placeholder={this.props.placeholderMessage} readOnly="readOnly" id="CSChatMessage" rows="1" className="ps-0 no-outline form-control rounded-0 form-control rounded-start-0 rounded-end-0 border-0" /> */}
                     <SharedTextarea
