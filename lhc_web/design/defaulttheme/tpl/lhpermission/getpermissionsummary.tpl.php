2f57377f2c9dc30a9764685946b9ba260fedd7db
Remigijus Kiminas <remdex@gmail.com>
2025-04-01 04:43:12 -0400

    Fix permissions window UI

--- lhc_web/design/defaulttheme/tpl/lhpermission/getpermissionsummary.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhpermission/getpermissionsummary.tpl.php
@@ -1,24 +1,24 @@
 <br/>
-<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+<div class="accordion" id="accordion" role="tablist" aria-multiselectable="true">
   <?php foreach (erLhcoreClassModules::getModuleList() as $key => $Module) : ?>
   <?php $moduleFunctions = erLhcoreClassModules::getModuleFunctions($key); ?>
-  <div class="panel panel-default">
-    <div class="card-header" role="tab" id="heading<?php echo $key?>One">     
-        <a data-bs-toggle="collapse" data-parent="#accordion" href="#<?php echo $key?>One" aria-expanded="true" aria-controls="collapseOne">
+  <div class="accordion-item">
+    <h2 class="accordion-header" role="tab" id="heading<?php echo $key?>One">
+        <button class="accordion-button collapsed" data-bs-toggle="collapse" data-parent="#accordion" href="#<?php echo $key?>One" aria-expanded="true" aria-controls="collapseOne">
             <?php if (count($moduleFunctions) > 0) : $hasFunctions = true;?>
             <?php include(erLhcoreClassDesign::designtpl('lhpermission/gerpermissionsummary_module.tpl.php'));?>                      
             <?php else : $hasFunctions = false; // There is no custom functions that means user can use all module functions ?>                       
             <?php include(erLhcoreClassDesign::designtpl('lhpermission/gerpermissionsummary_module.tpl.php'));?>            
             <label class="float-end label label-success">Y</label>            
             <?php endif;?>
-        </a>
-    </div>    
+        </button>
+    </h2>
     <?php if ($hasFunctions == true) : ?>
-    <div id="<?php echo $key?>One" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?php echo $key?>One">
-      <div class="card-body">
-        <ul class="list-unstyled">
+    <div id="<?php echo $key?>One" class="accordion-collapse collapse" role="tabpanel" aria-labelledby="heading<?php echo $key?>One">
+      <div class="accordion-body">
+        <ul class="list-unstyled mb-0">
         <?php foreach ($moduleFunctions as $keyFunction => $function) : $canUse = erLhcoreClassRole::canUseByModuleAndFunction($permissions,$key,$keyFunction)?>
-            <li><label><input class="<?php if ($canUse == false) : ?>PermissionSelector<?php endif;?>" <?php if ($canUse == true) : ?>disabled="disabled" checked="checked"<?php endif;?> type="checkbox" name="RequestPermission" value="<?php echo $key?>_f_<?php echo $keyFunction?>"> [<?php echo $keyFunction?>] <?php echo htmlspecialchars($function['explain'])?></label> <label class="label label-<?php if ($canUse == true) : ?>success<?php else : ?>danger<?php endif;?>"><?php if ($canUse == true) : ?>Y<?php else : ?>N<?php endif;?></label></li>        
+            <li><label><input class="<?php if ($canUse == false) : ?>PermissionSelector<?php endif;?>" <?php if ($canUse == true) : ?>disabled="disabled" checked="checked"<?php endif;?> type="checkbox" name="RequestPermission" value="<?php echo $key?>_f_<?php echo $keyFunction?>"> [<?php echo $keyFunction?>] <?php echo htmlspecialchars($function['explain'])?></label> <label class="badge bg-<?php if ($canUse == true) : ?>success<?php else : ?>danger<?php endif;?>"><?php if ($canUse == true) : ?>Y<?php else : ?>N<?php endif;?></label></li>
         <?php endforeach;?>
         </ul>
       </div>
