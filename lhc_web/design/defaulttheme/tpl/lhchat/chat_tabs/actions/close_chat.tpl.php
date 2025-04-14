ab11d7ff8d5b0d00a26e52b12bd8c5afa06ee1cb
Remigijus Kiminas <remdex@gmail.com>
2025-04-14 03:03:23 -0400

    Add status attribute

--- lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/actions/close_chat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/chat_tabs/actions/close_chat.tpl.php
@@ -1,6 +1,6 @@
 <?php if ($chat->user_id == erLhcoreClassUser::instance()->getUserID() || erLhcoreClassUser::instance()->hasAccessTo('lhchat','allowcloseremote')) : ?>
 <div class="col-6 pb-1">
-    <a class="text-muted" id="chat-close-action-<?php echo $chat->id?>" data-loading="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Closing...')?>" onclick="lhinst.closeActiveChatDialog(<?php echo $chat->id?>,$('#tabs'),true)" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Close chat')?>">
+    <a class="text-muted" id="chat-close-action-<?php echo $chat->id?>" data-status="<?php echo $chat->status;?>" data-loading="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Closing...')?>" onclick="lhinst.closeActiveChatDialog(<?php echo $chat->id?>,$('#tabs'),true)" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Close chat')?>">
         <span class="material-icons">close</span><span data-original="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Close chat')?>" class="close-text"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Close chat')?></span></a>
 </div>
 <?php endif;?>
