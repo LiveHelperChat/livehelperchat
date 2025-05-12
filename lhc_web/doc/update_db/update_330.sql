f0c6478f69956da8e54b5755a9c0082ebfc8c388
Remigijus Kiminas <remdex@gmail.com>
2025-05-12 04:30:49 -0400

    Updates

--- lhc_web/doc/update_db/update_330.sql
+++ lhc_web/doc/update_db/update_330.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `lh_abstract_chat_column` ADD `chat_window_enabled` tinyint(1) NOT NULL DEFAULT '0', COMMENT='';
 ALTER TABLE `lh_abstract_chat_column` ADD INDEX `chat_window_enabled` (`chat_window_enabled`);
+UPDATE `lh_chat_config` SET value = '330' WHERE `identifier` = 'version_updates' LIMIT 1;
