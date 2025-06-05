4146b3ad7cb31b025d78c84f3e18984b438c37a3
Remigijus Kiminas <remdex@gmail.com>
2025-06-05 06:44:53 -0400

    Show embed code in file edit

--- lhc_web/design/defaulttheme/tpl/lhfile/edit.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhfile/edit.tpl.php
@@ -8,18 +8,33 @@
     <?php include(erLhcoreClassDesign::designtpl('lhkernel/alert_success.tpl.php'));?>
 <?php endif; ?>
 
-<form action="" method="post" enctype="multipart/form-data" ng-non-bindable>
-    <div class="form-group">
-        <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','File name');?></label>
-        <p><?php echo htmlspecialchars($item->upload_name)?></p>
+<div class="row">
+    <div class="col-md-6">
+        <form action="" method="post" enctype="multipart/form-data" ng-non-bindable>
+            <div class="form-group">
+                <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','File name');?></label>
+                <p><?php echo htmlspecialchars($item->upload_name)?></p>
+            </div>
+
+            <div class="form-group">
+                <label><input type="checkbox" name="persistent" value="on" <?php echo $item->persistent == 1 ? print 'checked="checked"' : print ''?> > <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','Persistent');?></label>
+                <span class="d-block text-muted fs13"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','Files maintenance jobs will not be run on this file.');?></span>
+            </div>
+
+            <?php include(erLhcoreClassDesign::designtpl('lhkernel/csfr_token.tpl.php'));?>
+
+            <input type="submit" class="btn btn-secondary" name="UploadFileAction" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','Update');?>" />
+        </form>
     </div>
-
-    <div class="form-group">
-        <label><input type="checkbox" name="persistent" value="on" <?php echo $item->persistent == 1 ? print 'checked="checked"' : print ''?> > <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','Persistent');?></label>
-        <span class="d-block text-muted fs13"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','Files maintenance jobs will not be run on this file.');?></span>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','BB code as a file');?></label>
+            <input type="text" readonly="readonly" class="form-control form-control-sm" value="[file=<?php echo $item->id,'_',$item->security_hash?>]">
+        </div>
+        <div class="form-group">
+            <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','BB code as an image');?></label>
+            <input type="text" readonly="readonly" class="form-control form-control-sm" value="[file=<?php echo $item->id,'_',$item->security_hash,'_img'?>]">
+        </div>
     </div>
+</div>
 
-    <?php include(erLhcoreClassDesign::designtpl('lhkernel/csfr_token.tpl.php'));?>
-
-    <input type="submit" class="btn btn-secondary" name="UploadFileAction" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('file/new','Update');?>" />
-</form>
