614eaabedcae850c3b9cb2f3337e34e05ccdfb8e
Remigijus Kiminas <remdex@gmail.com>
2025-06-10 07:44:24 -0400

    Escape template

--- lhc_web/design/defaulttheme/tpl/lhmailconv/searchtemplate.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhmailconv/searchtemplate.tpl.php
@@ -1,7 +1,7 @@
 <?php foreach ($items as $item) : ?>
 <div>
     <h5 class="pb-0 mb-0"><a class="use-template" data-id="<?php echo $item->id?>" href="#"><?php echo htmlspecialchars($item->name)?></a></h5>
-    <p class="pt-1 mt-0"><?php echo mb_substr($item->template_plain,0,250)?>...</p>
+    <p class="pt-1 mt-0"><?php echo htmlspecialchars(mb_substr($item->template_plain,0,250))?>...</p>
     <input type="hidden" id="use-template-value-<?php echo $item->id?>" value="<?php echo htmlspecialchars($item->template_html)?>" /></input>
 </div>
 <?php endforeach; ?>
