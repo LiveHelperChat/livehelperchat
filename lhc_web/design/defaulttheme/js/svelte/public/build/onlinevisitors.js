5fdb71695eaf1b262dc4a534022e6656daa8c5b7
Remigijus Kiminas <remdex@gmail.com>
2025-06-17 15:19:05 +0300

    Option to suspend/resume online visitors updating in main window.

--- lhc_web/design/defaulttheme/js/svelte/public/build/onlinevisitors.js
+++ lhc_web/design/defaulttheme/js/svelte/public/build/onlinevisitors.js
@@ -1,2 +1,2 @@
-export{L as LHCOnlineVisitors}from"./onlinevisitors.DCkG4PqW.js";import"./i18n.B4Bqyi2p.js";
+export{L as LHCOnlineVisitors}from"./onlinevisitors.Fc3E8hGs.js";import"./i18n.B4Bqyi2p.js";
 //# sourceMappingURL=onlinevisitors.js.map
