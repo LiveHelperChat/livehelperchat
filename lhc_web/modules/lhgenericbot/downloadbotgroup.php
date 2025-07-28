f1bbb973d4d215a4abb06d34dc4e8d089fd7f292
Remigijus Kiminas <remdex@gmail.com>
2025-07-28 03:35:37 -0400

    Add nice export for bot trigger group

--- lhc_web/modules/lhgenericbot/downloadbotgroup.php
+++ lhc_web/modules/lhgenericbot/downloadbotgroup.php
@@ -48,7 +48,7 @@ foreach ($groups as $group) {
 
 header('Content-Disposition: attachment; filename="lhc-bot-group-'.$group->id.'.json"');
 header('Content-Type: application/json');
-echo json_encode($exportData);
+echo json_encode($exportData, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 
 exit;
 ?>
