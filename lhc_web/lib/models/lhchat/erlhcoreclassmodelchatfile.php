5c3c831c7db3a04beb63c6153f5d7f2d944bd03a
Remigijus Kiminas <remdex@gmail.com>
2025-08-26 08:22:32 -0400

    Raw file embed syntax

--- lhc_web/lib/models/lhchat/erlhcoreclassmodelchatfile.php
+++ lhc_web/lib/models/lhchat/erlhcoreclassmodelchatfile.php
@@ -115,6 +115,9 @@ class erLhcoreClassModelChatFile
             case 'file_body':
                 return 'data:'.$this->type.';base64,'.base64_encode(file_get_contents($this->file_path_server));
 
+            case 'file_body_raw':
+                return base64_encode(file_get_contents($this->file_path_server));
+
             case 'file_body_embed':
                 return '[chatfilebody='.$this->id . '_' . $this->security_hash . ']';
 
