ed84ee834710a29327ab60037dd2ad6142edaec8
Remigijus Kiminas <remdex@gmail.com>
2025-12-08 03:37:40 -0500

    Updates

--- lhc_web/modules/lhmailconv/deletependingimport.php
+++ lhc_web/modules/lhmailconv/deletependingimport.php
@@ -1,5 +1,10 @@
 <?php
 
+if (!$currentUser->validateCSFRToken($Params['user_parameters_unordered']['csfr'])) {
+    die('Invalid CSFR Token');
+    exit;
+}
+
 $item = \LiveHelperChat\Models\mailConv\PendingImport::fetch($Params['user_parameters']['id']);
 
 if ($item instanceof \LiveHelperChat\Models\mailConv\PendingImport) {
