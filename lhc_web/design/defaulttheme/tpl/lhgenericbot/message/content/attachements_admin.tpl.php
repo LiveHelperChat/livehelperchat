b7fdbe1f88a8515c3940e2c550bcaef0ff6923d0
Remigijus Kiminas <remdex@gmail.com>
2026-03-25 03:18:29 -0400

    Files uploaded via file prefiew also should be verified

--- lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/attachements_admin.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhgenericbot/message/content/attachements_admin.tpl.php
@@ -1,7 +1,16 @@
 <div class="d-flex flex-wrap gap-2">
     <?php foreach($metaMessage as $messageAttachement) : ?>
         <?php if (isset($messageAttachement['id']) && isset($messageAttachement['security_hash'])) : ?>
-                <?php $msgBody = '[file='.$messageAttachement['id'].'_'.$messageAttachement['security_hash'] .']'; $paramsMessageRender = array('no_reactions' => true, 'sender' => 0);?>
+                <?php
+                $msgBody = '[file='.$messageAttachement['id'].'_'.$messageAttachement['security_hash'] .']';
+                $paramsMessageRender = array(
+                        'img_verify_min_dim' => (isset($data['img_verify_min_dim']) ? $data['img_verify_min_dim'] : 100),
+                        'print_admin' => (isset($print_admin) && $print_admin === true),
+                        'download_policy' => $download_policy,
+                        'operator_render' => true,
+                        'no_reactions' => true,
+                        'sender' => 0)
+                ?>
                 <div class="attachment-item">
                     <?php include(erLhcoreClassDesign::designtpl('lhchat/lists/msg_body.tpl.php'));?>
                 </div>
