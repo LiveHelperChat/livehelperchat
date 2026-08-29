f7fa94d302abef5361905530ca52b025ef37fa6f
root <root@vpn.com>
2026-08-29 08:34:36 +0300

    Change widget theme custom CSS fields from text to mediumtext to prevent CSS truncation

---
+++ lhc_web/doc/update_db/update_357.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `lh_abstract_widget_theme` CHANGE `custom_status_css` `custom_status_css` mediumtext NOT NULL, CHANGE `custom_container_css` `custom_container_css` mediumtext NOT NULL, CHANGE `custom_widget_css` `custom_widget_css` mediumtext NOT NULL, CHANGE `custom_popup_css` `custom_popup_css` mediumtext NOT NULL;
